955ff1947ac72aaa4090173487f1f122456f7d9b
Daniel M. Duley <daniel.duley@verizon.net>
2000-03-25 22:23:09 +0000

    Reduced titlebar size by 2 pixels ;-)
    svn path=/trunk/kdebase/kwin/; revision=44705

--- clients/system/systemclient.cpp
+++ clients/system/systemclient.cpp
@@ -237,7 +237,7 @@ SystemClient::SystemClient( Workspace *ws, WId w, QWidget *parent,
     g->addLayout( hb, 0, 1 );
     hb->addSpacing(2);
     hb->addWidget( button[0]);
-    titlebar = new QSpacerItem(10, 16, QSizePolicy::Expanding,
+    titlebar = new QSpacerItem(10, 14, QSizePolicy::Expanding,
                                QSizePolicy::Minimum);
     hb->addItem(titlebar);
     hb->addSpacing(3);
@@ -302,7 +302,7 @@ void SystemClient::paintEvent( QPaintEvent* )
             p.drawTiledPixmap(0, 0, width(), 18, *iUpperGradient);
         }
     }
-    t.setTop( 2 );
+    t.setTop( 1 );
     if(isActive())
         p.drawTiledPixmap(t, *titlePix);
 
