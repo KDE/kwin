427941efea8e9bfa798c755929723529cee3d11d
Luboš Luňák <l.lunak@kde.org>
2002-09-03 16:47:41 +0000

    Keep BC.
    svn path=/trunk/kdebase/kwin/; revision=175540

--- workspace.h
+++ workspace.h
@@ -395,7 +395,7 @@ private:
     int number_of_desktops;
     QMemArray<int> desktop_focus_chain;
 
-    Client* popup_client;
+    QGuardedPtr<Client> popup_client;
 
     void loadSessionInfo();
 
