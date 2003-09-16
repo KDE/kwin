f8b2ceee53e747946d61f590eef2b4b7ca17b49e
Luboš Luňák <l.lunak@kde.org>
2003-09-16 19:30:05 +0000

    Merging old headers from kwin_iii back to HEAD.
    svn path=/trunk/kdebase/kwin/; revision=251612

---
+++ oldheaders/options.h
@@ -0,0 +1,4 @@
+#error The KWin decorations API provided by this header file is obsolete.
+#error All code using it will have to be ported to the new KDecoration API.
+
+// See kdebase/kwin/clients/PORTING .
