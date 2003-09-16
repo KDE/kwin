0ef8354c30fd741e55dd5fc3583c3ce4c8542973
Luboš Luňák <l.lunak@kde.org>
2003-09-16 19:32:34 +0000

    Merging back the process killing helper from kwin_iii to HEAD.
    svn path=/trunk/kdebase/kwin/; revision=251615

---
+++ killer/killer.cpp
@@ -0,0 +1,108 @@
+/****************************************************************************
+
+ Copyright (C) 2003 Lubos Lunak        <l.lunak@kde.org>
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+****************************************************************************/
+
+#include <kcmdlineargs.h>
+#include <kapplication.h>
+#include <kmessagebox.h>
+#include <kprocess.h>
+#include <klocale.h>
+#include <unistd.h>
+#include <kwin.h>
+#include <X11/Xlib.h>
+
+static const KCmdLineOptions options[] =
+{
+    { "pid <pid>", I18N_NOOP("PID of the application to terminate."), 0 },
+    { "hostname <hostname>", I18N_NOOP("Hostname on which the application is running."), 0 },
+    { "windowname <caption>", I18N_NOOP("Caption of the window to be terminated."), 0 },
+    { "applicationname <name>", I18N_NOOP("Name of the application to be terminated."), 0 },
+    { "wid <id>", I18N_NOOP("ID of resource belonging to the application."), 0 },
+    { "timestamp <time>", I18N_NOOP("Time of user action causing killing."), 0 },
+    KCmdLineLastOption
+};
+
+class ShowFilter
+    : public QObject
+    {
+    // no need for QObject
+    protected:
+        virtual bool eventFilter( QObject*, QEvent* );
+    };
+    
+Time timestamp = CurrentTime;
+
+// unfortunately KMessageBox doesn't allow accessing the dialog directly
+bool ShowFilter::eventFilter( QObject* o, QEvent* e )
+{
+    if( e->type() == QEvent::Show && o->inherits( "KDialogBase" ))
+    {
+        QWidget* w = static_cast< QWidget* >( o );
+        KWin::setUserTime( w->winId(), timestamp );
+    }
+    return false;
+}
+
+int main( int argc, char* argv[] )
+{
+    KCmdLineArgs::init( argc, argv, "kwin_killer_helper", I18N_NOOP( "KWin" ),
+	I18N_NOOP( "KWin utility" ), "1.0" );
+    KCmdLineArgs::addCmdLineOptions( options );
+    KApplication app;
+    KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
+    QCString hostname = args->getOption( "hostname" );
+    bool pid_ok = false;
+    pid_t pid = args->getOption( "pid" ).toULong( &pid_ok );
+    QString caption = QString::fromUtf8( args->getOption( "windowname" ));
+    QString appname = QString::fromLatin1( args->getOption( "applicationname" ));
+    bool id_ok = false;
+    Window id = args->getOption( "wid" ).toULong( &id_ok );
+    bool time_ok = false;
+    timestamp =args->getOption( "timestamp" ).toULong( &time_ok );
+    args->clear();
+    if( !pid_ok || pid == 0 || !id_ok || id == None || !time_ok || timestamp == CurrentTime
+	|| hostname.isEmpty() || caption.isEmpty() || appname.isEmpty())
+    {
+	KCmdLineArgs::usage( i18n( "This helper utility is not supposed to be called directly!" ));
+	return 1;
+    }
+    QString question = i18n(
+	"<qt>Window with title \"<b>%2</b>\" doesn't respond. "
+	"This window belongs to application <b>%1</b> (PID=%3, hostname=%4).<p>"
+	"Do you wish to terminate this application? (All unsaved data in this application will be lost.)</qt>" )
+	.arg( appname ).arg( caption ).arg( pid ).arg( hostname );
+    ShowFilter filter;
+    app.installEventFilter( &filter );
+    if( KMessageBox::warningYesNo( NULL, question, "" ) == KMessageBox::Yes )
+    {    
+	if( hostname != "localhost" )
+	{
+    	    KProcess proc;
+	    proc << "xon" << hostname << "kill" << pid;
+    	    proc.start( KProcess::DontCare );
+	}
+	else
+	    ::kill( pid, SIGTERM );
+	XKillClient( qt_xdisplay(), id );
+    }
+}
