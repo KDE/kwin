a77da4851ec96eb3b016e8d32cc231177ad0bd60
Luboš Luňák <l.lunak@kde.org>
2003-10-13 16:18:24 +0000

    Use KApplication::updateUserTimestamp() instead of trying to set it directly on the dialog.
    svn path=/trunk/kdebase/kwin/; revision=258581

--- killer/killer.cpp
+++ killer/killer.cpp
@@ -32,41 +32,21 @@ DEALINGS IN THE SOFTWARE.
 #include <X11/Xlib.h>
 
 static const KCmdLineOptions options[] =
-{
-    { "pid <pid>", I18N_NOOP("PID of the application to terminate."), 0 },
-    { "hostname <hostname>", I18N_NOOP("Hostname on which the application is running."), 0 },
-    { "windowname <caption>", I18N_NOOP("Caption of the window to be terminated."), 0 },
-    { "applicationname <name>", I18N_NOOP("Name of the application to be terminated."), 0 },
-    { "wid <id>", I18N_NOOP("ID of resource belonging to the application."), 0 },
-    { "timestamp <time>", I18N_NOOP("Time of user action causing killing."), 0 },
-    KCmdLineLastOption
-};
-
-class ShowFilter
-    : public QObject
     {
-    // no need for QObject
-    protected:
-        virtual bool eventFilter( QObject*, QEvent* );
+    // no need for I18N_NOOP(), this is not supposed to be used directly
+        { "pid <pid>", "PID of the application to terminate.", 0 },
+        { "hostname <hostname>", "Hostname on which the application is running.", 0 },
+        { "windowname <caption>", "Caption of the window to be terminated.", 0 },
+        { "applicationname <name>", "Name of the application to be terminated.", 0 },
+        { "wid <id>", "ID of resource belonging to the application.", 0 },
+        { "timestamp <time>", "Time of user action causing killing.", 0 },
+        KCmdLineLastOption
     };
-    
-Time timestamp = CurrentTime;
-
-// unfortunately KMessageBox doesn't allow accessing the dialog directly
-bool ShowFilter::eventFilter( QObject* o, QEvent* e )
-{
-    if( e->type() == QEvent::Show && o->inherits( "KDialogBase" ))
-    {
-        QWidget* w = static_cast< QWidget* >( o );
-        KWin::setUserTime( w->winId(), timestamp );
-    }
-    return false;
-}
 
 int main( int argc, char* argv[] )
-{
+    {
     KCmdLineArgs::init( argc, argv, "kwin_killer_helper", I18N_NOOP( "KWin" ),
-	I18N_NOOP( "KWin utility" ), "1.0" );
+	I18N_NOOP( "KWin helper utility" ), "1.0" );
     KCmdLineArgs::addCmdLineOptions( options );
     KApplication app;
     KGlobal::locale()->insertCatalogue( "kwin" ); // the messages are in kwin's .po file
@@ -79,31 +59,30 @@ int main( int argc, char* argv[] )
     bool id_ok = false;
     Window id = args->getOption( "wid" ).toULong( &id_ok );
     bool time_ok = false;
-    timestamp =args->getOption( "timestamp" ).toULong( &time_ok );
+    Time timestamp =args->getOption( "timestamp" ).toULong( &time_ok );
     args->clear();
     if( !pid_ok || pid == 0 || !id_ok || id == None || !time_ok || timestamp == CurrentTime
 	|| hostname.isEmpty() || caption.isEmpty() || appname.isEmpty())
-    {
+        {
 	KCmdLineArgs::usage( i18n( "This helper utility is not supposed to be called directly!" ));
 	return 1;
-    }
+        }
     QString question = i18n(
 	"<qt>Window with title \"<b>%2</b>\" doesn't respond. "
 	"This window belongs to application <b>%1</b> (PID=%3, hostname=%4).<p>"
 	"Do you wish to terminate this application? (All unsaved data in this application will be lost.)</qt>" )
 	.arg( appname ).arg( caption ).arg( pid ).arg( hostname );
-    ShowFilter filter;
-    app.installEventFilter( &filter );
+    app.updateUserTimestamp( timestamp );
     if( KMessageBox::warningYesNo( NULL, question, "" ) == KMessageBox::Yes )
-    {    
+        {    
 	if( hostname != "localhost" )
-	{
+            {
     	    KProcess proc;
 	    proc << "xon" << hostname << "kill" << pid;
     	    proc.start( KProcess::DontCare );
-	}
+	    }
 	else
 	    ::kill( pid, SIGTERM );
 	XKillClient( qt_xdisplay(), id );
+        }
     }
-}
