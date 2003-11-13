e7c55a0c892313e561c8e8c123fc83f15b597197
Luboš Luňák <l.lunak@kde.org>
2003-11-13 18:27:31 +0000

    Ouch ... thanks to Ossi for noticing.
    svn path=/trunk/kdebase/kwin/; revision=266865

--- group.cpp
+++ group.cpp
@@ -213,6 +213,7 @@ bool Client::belongToSameApplication( const Client* c1, const Client* c2, bool a
         ; // "different" apps
     else if( c1->pid() == 0 || c2->pid() == 0 )
         ; // old apps that don't have _NET_WM_PID, consider them different
+    else
         same_app = true; // looks like it's the same app
     return same_app;
     }
