d160269cdedc5b0d0a543a5379ef188fc1169da1
Luboš Luňák <l.lunak@kde.org>
2004-03-23 16:52:12 +0000

    I guess I didn't intend to commit this file with that.
    svn path=/trunk/kdebase/kwin/; revision=298096

--- kwin_focus1.sh
+++ kwin_focus1.sh
@@ -1,17 +1,13 @@
 #! /bin/sh
 was=
 while read line; do
-    echo LINE:$line >>/tmp/qwe.txt
     if echo "$line" | grep '^IgnoreFocusStealingClasses=' >/dev/null 2>/dev/null; then
-        echo REPLACE >>/tmp/qwe.txt
         echo "$line" | sed 's/\(^IgnoreFocusStealingClasses=.*$\)/\1,kded,kio_uiserver,kget/'
-        echo "$line" | sed 's/\(^IgnoreFocusStealingClasses=.*$\)/\1,kded,kio_uiserver,kget/' >>/tmp/qwe.txt
         was=1
     else
         echo "$line"
     fi
 done
 if test -z "$was"; then
-    echo APPEND >>/tmp/qwe.txt
     echo "IgnoreFocusStealingClasses=kded,kio_uiserver,kget"
 fi
