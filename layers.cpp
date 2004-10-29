d421b5c7e791bd2e3a0856da6c1a24da4f892da6
Luboš Luňák <l.lunak@kde.org>
2004-10-29 12:59:29 +0000

    I wonder if mplayer's default fullscreen type will be simply 'fullscreen' instead of various hacks one day. BUG: 92302
    svn path=/trunk/kdebase/kwin/; revision=358750

--- layers.cpp
+++ layers.cpp
@@ -712,8 +712,6 @@ Layer Client::belongsToLayer() const
         return DockLayer;
     if( isTopMenu())
         return DockLayer;
-    if( keepAbove())
-        return AboveLayer;
     // only raise fullscreen above docks if it's the topmost window in unconstrained stacking order,
     // i.e. the window set to be topmost by the user (also includes transients of the fullscreen window)
     const Client* ac = workspace()->mostRecentlyActivatedClient(); // instead of activeClient() - avoids flicker
@@ -721,6 +719,8 @@ Layer Client::belongsToLayer() const
         && workspace()->topClientOnDesktop( desktop(), true ) == ac
         && ( ac == this || this->hasTransient( ac, true )))
         return ActiveLayer;
+    if( keepAbove())
+        return AboveLayer;
     return NormalLayer;
     }
 
