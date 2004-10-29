d9bc9b09897c6e366d021d34e204cd4142772c01
Luboš Luňák <l.lunak@kde.org>
2004-10-29 16:16:30 +0000

    Create startup notification even for apps that shouldn't have any, just don't create any visual feedback. This will make sure the apps will also have windows shown on the virtual desktop where they were launched (#68851), and they'll also be able to get user timestamp from the startup notification. KGet no longer needs exception in focus stealing prevention, KWallet either (as the openWallet() call can get a mainwindow id). If I'm lucky I even haven't broken anything.
    svn path=/trunk/kdebase/kwin/; revision=358798

--- data/kwin_focus1.sh
+++ data/kwin_focus1.sh
@@ -2,12 +2,12 @@
 was=
 while read line; do
     if echo "$line" | grep '^IgnoreFocusStealingClasses=' >/dev/null 2>/dev/null; then
-        echo "$line" | sed 's/\(^IgnoreFocusStealingClasses=.*$\)/\1,kded,kio_uiserver,kget/'
+        echo "$line" | sed 's/\(^IgnoreFocusStealingClasses=.*$\)/\1,kio_uiserver/'
         was=1
     else
         echo "$line"
     fi
 done
 if test -z "$was"; then
-    echo "IgnoreFocusStealingClasses=kded,kio_uiserver,kget"
+    echo "IgnoreFocusStealingClasses=kio_uiserver"
 fi
