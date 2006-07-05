66f535ac804cb82d1004c9729674404de2e5abe8
Luboš Luňák <l.lunak@kde.org>
2006-07-05 20:54:15 +0000

    No need to support <=KDE3.1 plugins.
    svn path=/trunk/KDE/kdebase/workspace/; revision=558691

--- oldheaders/client.h
+++
@@ -1,4 +0,0 @@
-#error The KWin decorations API provided by this header file is obsolete.
-#error All code using it will have to be ported to the new KDecoration API.
-
-// See kdebase/kwin/clients/PORTING .
