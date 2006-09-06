1b9baf8447e8ea0e082ee850357b918638f1878b
Luboš Luňák <l.lunak@kde.org>
2006-09-06 10:37:46 +0000

    Not used.
    svn path=/trunk/KDE/kdebase/workspace/; revision=581402

--- kcmkwin/kwindecoration/kwindecoration.cpp
+++ kcmkwin/kwindecoration/kwindecoration.cpp
@@ -62,8 +62,6 @@
 #include <kdecoration_plugins_p.h>
 #include <kdecorationfactory.h>
 #include <kvbox.h>
-#include "kwindecorationadaptor.h"
-#include <QtDBus/QtDBus>
 
 // KCModule plugin interface
 // =========================
@@ -75,8 +73,6 @@ KWinDecorationModule::KWinDecorationModule(QWidget* parent, const QStringList &)
           kwinConfig("kwinrc"),
           pluginObject(0)
 {
-    new DecorationAdaptor(this);
-    QDBusConnection::sessionBus().registerObject("/KWinClientDecoration", this);
     kwinConfig.setGroup("Style");
         plugins = new KDecorationPreviewPlugins( &kwinConfig );
 
