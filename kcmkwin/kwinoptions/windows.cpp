69bfbbcba8f18d2466ea3e16691682be004d264b
Daniel Molkentin <molkentin@kde.org>
2006-12-12 13:21:44 +0000

    s/KActiveLabel/QLabel/
    svn path=/branches/work/kwin_composite/; revision=612724

--- kcmkwin/kwinoptions/windows.cpp
+++ kcmkwin/kwinoptions/windows.cpp
@@ -39,7 +39,6 @@
 #include <QVBoxLayout>
 #include <kmessagebox.h>
 
-#include <kactivelabel.h>
 #include <klocale.h>
 #include <kcolorbutton.h>
 #include <kconfig.h>
@@ -1244,7 +1243,7 @@ KTranslucencyConfig::KTranslucencyConfig (bool _standAlone, KConfig *_config, KI
   QVBoxLayout *lay = new QVBoxLayout (this);
   kompmgrAvailable_ = kompmgrAvailable();
   if (!kompmgrAvailable_){
-  KActiveLabel *label = new KActiveLabel(i18n("<qt><b>It seems that alpha channel support is not available.</b><br><br>"
+  QLabel *label = new QLabel(i18n("<qt><b>It seems that alpha channel support is not available.</b><br><br>"
                                  "Please make sure you have "
                                  "<a href=\"http://www.freedesktop.org/\">Xorg &ge; 6.8</a>,"
                                  " and have installed the kompmgr that came with kwin.<br>"
