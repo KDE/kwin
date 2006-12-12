7433a0d2fbab2b84f0f4378fdea7111b90c137d7
Volker Krause <vkrause@kde.org>
2006-12-12 10:36:00 +0000

    compile
    svn path=/branches/work/kwin_composite/; revision=612696

--- kcmkwin/kwinoptions/windows.cpp
+++ kcmkwin/kwinoptions/windows.cpp
@@ -39,7 +39,7 @@
 #include <QVBoxLayout>
 #include <kmessagebox.h>
 
-#include <qlabel.h>
+#include <k3activelabel.h>
 #include <klocale.h>
 #include <kcolorbutton.h>
 #include <kconfig.h>
@@ -689,7 +689,7 @@ void KAdvancedConfig::load( void )
     setShadeHover(config->readEntry(KWIN_SHADEHOVER, QVariant(false)).toBool());
     setShadeHoverInterval(config->readEntry(KWIN_SHADEHOVER_INTERVAL, 250));
 
-    setElectricBorders(config->readEntry(KWM_ELECTRIC_BORDER, 0));
+    setElectricBorders(config->readEntry(KWM_ELECTRIC_BORDER, false));
     setElectricBorderDelay(config->readEntry(KWM_ELECTRIC_BORDER_DELAY, 150));
 
 //    setFocusStealing( config->readEntry(KWIN_FOCUS_STEALING, 2 ));
@@ -1244,7 +1244,7 @@ KTranslucencyConfig::KTranslucencyConfig (bool _standAlone, KConfig *_config, KI
   QVBoxLayout *lay = new QVBoxLayout (this);
   kompmgrAvailable_ = kompmgrAvailable();
   if (!kompmgrAvailable_){
-  QLabel *label = new QLabel(i18n("<qt><b>It seems that alpha channel support is not available.</b><br><br>"
+  K3ActiveLabel *label = new K3ActiveLabel(i18n("<qt><b>It seems that alpha channel support is not available.</b><br><br>"
                                  "Please make sure you have "
                                  "<a href=\"http://www.freedesktop.org/\">Xorg &ge; 6.8</a>,"
                                  " and have installed the kompmgr that came with kwin.<br>"
@@ -1255,8 +1255,6 @@ KTranslucencyConfig::KTranslucencyConfig (bool _standAlone, KConfig *_config, KI
                                  "And if your GPU provides hardware-accelerated Xrender support (mainly nVidia cards):<br><br>"
                                  "<i>Option     \"RenderAccel\" \"true\"</i><br>"
                                  "In <i>Section \"Device\"</i></qt>"), this);
-  label->setOpenExternalLinks(true);
-  label->setTextInteractionFlags(Qt::LinksAccessibleByMouse);
   lay->addWidget(label);
   }
   else
@@ -1477,10 +1475,8 @@ void KTranslucencyConfig::load( void )
 
     if (!kompmgrAvailable_)
         return;
-  config->setGroup( "Notification Messages" );
-  useTranslucency->setChecked(config->readEntry("UseTranslucency", QVariant(false)).toBool());
-
   config->setGroup( "Translucency" );
+  useTranslucency->setChecked(config->readEntry("UseTranslucency", QVariant(false)).toBool());
   activeWindowTransparency->setChecked(config->readEntry("TranslucentActiveWindows", QVariant(false)).toBool());
   inactiveWindowTransparency->setChecked(config->readEntry("TranslucentInactiveWindows", QVariant(true)).toBool());
   movingWindowTransparency->setChecked(config->readEntry("TranslucentMovingWindows", QVariant(false)).toBool());
@@ -1540,10 +1536,8 @@ void KTranslucencyConfig::save( void )
 {
     if (!kompmgrAvailable_)
         return;
-  config->setGroup( "Notification Messages" );
-  config->writeEntry("UseTranslucency",useTranslucency->isChecked());
-
   config->setGroup( "Translucency" );
+  config->writeEntry("UseTranslucency",useTranslucency->isChecked());
   config->writeEntry("TranslucentActiveWindows",activeWindowTransparency->isChecked());
   config->writeEntry("TranslucentInactiveWindows",inactiveWindowTransparency->isChecked());
   config->writeEntry("TranslucentMovingWindows",movingWindowTransparency->isChecked());
