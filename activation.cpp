12478151a28f50b47719fd66fd94a1a6ca20d4f8
Luboš Luňák <l.lunak@kde.org>
2007-02-06 13:55:12 +0000

    Don't accidentally set _NET_ACTIVE_WINDOW to 0 when there is an active window. And r612728 from 3.5 branch.
    svn path=/trunk/KDE/kdebase/workspace/; revision=630817

--- activation.cpp
+++ activation.cpp
@@ -29,7 +29,6 @@ License. See the file "COPYING" for the exact licensing terms.
 #include "atoms.h"
 #include "group.h"
 #include "rules.h"
-#include "effects.h"
 #include <QX11Info>
 
 namespace KWinInternal
@@ -222,8 +221,8 @@ void Workspace::setActiveClient( Client* c, allowed_t )
     ++set_active_client_recursion;
     if( active_client != NULL )
         { // note that this may call setActiveClient( NULL ), therefore the recursion counter
-            active_client->setActive( false );
-    }
+        active_client->setActive( false );
+        }
     active_client = c;
     Q_ASSERT( c == NULL || c->isActive());
     if( active_client != NULL )
@@ -246,8 +245,6 @@ void Workspace::setActiveClient( Client* c, allowed_t )
 
     rootInfo->setActiveWindow( active_client? active_client->window() : 0 );
     updateColormap();
-    if( effects )
-        effects->windowActivated( active_client ? active_client->effectWindow() : NULL );
     --set_active_client_recursion;
     }
 
@@ -266,6 +263,7 @@ void Workspace::activateClient( Client* c, bool force )
     {
     if( c == NULL )
         {
+        focusToNull();
         setActiveClient( NULL, Allowed );
         return;
         }
@@ -700,7 +698,7 @@ void Client::demandAttentionKNotify()
     }
 
 // TODO I probably shouldn't be lazy here and do it without the macro, so that people can read it
-KWIN_COMPARE_PREDICATE( SameApplicationActiveHackPredicate, Client, const Client*,
+KWIN_COMPARE_PREDICATE( SameApplicationActiveHackPredicate, const Client*,
     // ignore already existing splashes, toolbars, utilities, menus and topmenus,
     // as the app may show those before the main window
     !cl->isSplash() && !cl->isToolbar() && !cl->isTopMenu() && !cl->isUtility() && !cl->isMenu()
