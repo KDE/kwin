629166134a56f7951336df1471d0b0c10bf9f332
Luboš Luňák <l.lunak@kde.org>
2007-04-29 22:46:05 +0000

    Fix compile.
    svn path=/trunk/KDE/kdebase/workspace/; revision=659314

--- client.cpp
+++ client.cpp
@@ -473,7 +473,7 @@ void Client::updateShape()
     // !shape() mask setting is done in setMask() when the decoration
     // calls it or when the decoration is created/destroyed
 
-    if( Shape::version() >= 0x11 ) // 1.1, has input shape support
+    if( Extensions::shapeVersion() >= 0x11 ) // 1.1, has input shape support
         { // There appears to be no way to find out if a window has input
           // shape set or not, so always propagate the input shape
           // (it's the same like the bounding shape by default).
