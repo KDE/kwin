1940ac37775fbcf30fb562caf3f9327f09a82050
Stephan Kulow <coolo@kde.org>
2007-05-14 10:10:40 +0000

    revert Laurent's commit
    svn path=/trunk/KDE/kdebase/workspace/; revision=664556

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.rc *.ui *.kcfg > rc.cpp
 $XGETTEXT *.h *.cpp killer/*.cpp lib/*.cpp -o $podir/kwin.pot
--- kcmkwin/kwinrules/Messages.sh
+++ kcmkwin/kwinrules/Messages.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.rc *.ui *.kcfg > rc.cpp
 $XGETTEXT *.cpp -o $podir/kcmkwinrules.pot
