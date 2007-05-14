ed4548ae66614e0372b405b8d46416ac977b443f
Laurent Montel <montel@kde.org>
2007-05-14 09:59:11 +0000

    It will be a good idea to extract string from ui file, rc file, kcfg file
    svn path=/trunk/KDE/kdebase/workspace/; revision=664548

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/env bash
+$EXTRACTRC *.rc *.ui *.kcfg > rc.cpp
 $XGETTEXT *.h *.cpp killer/*.cpp lib/*.cpp -o $podir/kwin.pot
--- kcmkwin/kwinrules/Messages.sh
+++ kcmkwin/kwinrules/Messages.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/env bash
+$EXTRACTRC *.rc *.ui *.kcfg > rc.cpp
 $XGETTEXT *.cpp -o $podir/kcmkwinrules.pot
