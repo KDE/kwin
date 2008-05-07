15b807933413c14c26a7e0a6cab3f564a6c98cf7
Luboš Luňák <l.lunak@kde.org>
2008-05-07 17:11:00 +0000

    Follow the color option.
    svn path=/trunk/KDE/kdebase/workspace/; revision=805136

--- clients/ozone/oxygenbutton.cpp
+++ clients/ozone/oxygenbutton.cpp
@@ -168,7 +168,14 @@ void OxygenButton::paintEvent(QPaintEvent *)
 
 //    widget->window()setPalette(pal);
 
-    helper_.renderWindowBackground(&painter, this->rect(), this, pal);
+    if( OxygenFactory::blendTitlebarColors())
+        helper_.renderWindowBackground(&painter, this->rect(), this, pal);
+    else {
+        QPalette pal2( pal );
+        pal2.setColor( window()->backgroundRole(), client_.options()->color(
+            KDecorationDefines::ColorTitleBar, client_.isActive()));
+        helper_.renderWindowBackground(&painter, this->rect(), this, pal2);
+    }
 
     if (type_ == ButtonMenu) {
         // we paint the mini icon (which is 16 pixels high)
