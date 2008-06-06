be22824e0239fab926f88a06e3599ec02c88a662
Luboš Luňák <l.lunak@kde.org>
2008-06-06 14:19:53 +0000

    Show tooltips. BUG: 162648
    svn path=/trunk/KDE/kdebase/workspace/; revision=817651

--- lib/kdecoration.cpp
+++ lib/kdecoration.cpp
@@ -82,6 +82,7 @@ void KDecoration::createMainWidget( Qt::WFlags flags )
     QWidget *w = new QWidget( initialParentWidget(), initialWFlags() | flags );
     w->setObjectName("decoration widget");
     w->setAttribute( Qt::WA_PaintOnScreen );
+    w->setAttribute( Qt::WA_AlwaysShowToolTips );
     setMainWidget(w);
     }
 
