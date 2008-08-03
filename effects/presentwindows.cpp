0bfb2ccafef4a4b350e4490ac50bbd6d3763de38
Lucas Murray <lmurray@undefinedfire.com>
2008-08-03 08:21:03 +0000

    Make the present windows animation last a little bit longer
    svn path=/trunk/KDE/kdebase/workspace/; revision=841336

--- effects/presentwindows.cpp
+++ effects/presentwindows.cpp
@@ -81,7 +81,7 @@ PresentWindowsEffect::~PresentWindowsEffect()
 void PresentWindowsEffect::prePaintScreen( ScreenPrePaintData& data, int time )
     {
     // How long does it take for the effect to get it's full strength (in ms)
-    const double changeTime = 125;
+    const double changeTime = 250;
     if(mActivated)
         {
         mActiveness = qMin(1.0, mActiveness + time/changeTime);
