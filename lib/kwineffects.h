d0c1f5dd62896bbd9071b924a7cfb1d1d4c280a0
Luboš Luňák <l.lunak@kde.org>
2008-09-18 15:26:17 +0000

    Disable shadow with effects that transform windows, such as wobbly, since shadow currently can't handle that. CCBUG: 161330
    svn path=/trunk/KDE/kdebase/workspace/; revision=862309

--- lib/kwineffects.h
+++ lib/kwineffects.h
@@ -862,6 +862,7 @@ class KWIN_EXPORT WindowQuadList
         WindowQuadList filterOut( WindowQuadType type ) const;
         bool smoothNeeded() const;
         void makeArrays( float** vertices, float** texcoords ) const;
+        bool isTransformed() const;
     };
 
 class KWIN_EXPORT WindowPrePaintData
