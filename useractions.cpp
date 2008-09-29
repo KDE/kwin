30f293c01fdde41707f62fbd7c0ab515fac8c360
Michael Jansen <kde@michael-jansen.biz>
2008-09-29 18:42:45 +0000

    Use caption instead of windowId.
    svn path=/trunk/KDE/kdebase/workspace/; revision=865997

--- useractions.cpp
+++ useractions.cpp
@@ -417,7 +417,7 @@ void Workspace::clientShortcutUpdated( Client* c )
         if( action == NULL ) // new shortcut
             {
             action = client_keys->addAction(QString("session:clientShortcut %1").arg(key));
-            action->setText(i18n("Activate Window (%1)", key));  // TODO: Add window title if possible?
+            action->setText(i18n("Activate Window (%1)", c->caption()));
             connect( action, SIGNAL(triggered(bool)), c, SLOT(shortcutActivated()));
             }
         // no autoloading, since it's configured explicitly here and is not meant to be reused
