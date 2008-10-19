c6de48bd9a0cdfc818d0f1fe3ccd8fd6d278021d
Huynh Huu Long <long.upcase@googlemail.com>
2008-10-19 00:39:37 +0000

    Backport of r835483 redraw buttons when redrawing the whole decoration BUG: 167886
    svn path=/branches/KDE/4.1/kdebase/workspace/; revision=873188

--- clients/oxygen/oxygenclient.cpp
+++ clients/oxygen/oxygenclient.cpp
@@ -256,6 +256,8 @@ void OxygenClient::paintEvent(QPaintEvent *e)
     Q_UNUSED(e)
     if (!OxygenFactory::initialized()) return;
 
+    updateButtons();
+
     QPalette palette = widget()->palette();
     QPainter painter(widget());
 
