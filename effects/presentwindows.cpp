KDE/kwin

commit 848b2a936c21d0f36bbb9395cee89dfe68c7a771

Fix bug where if the user filters to only a single single in the natural layout there is no motion dynamics applied to it.
svn path=/trunk/KDE/kdebase/workspace/; revision=889761

effects/presentwindows.cpp
@@ -740,9 +740,9 @@ void PresentWindowsEffect::calculateWindowTransformationsKompose( EffectWindowLi
 void PresentWindowsEffect::calculateWindowTransformationsNatural( EffectWindowList windowlist, int screen )
     {
     if( windowlist.count() == 1 )
-        { // No idea why this is needed but if there is only one window on a Xinerama screen
-          // then the window is scaled into nothingness at (0,0) if its position isn't reset.
-        m_motionManager.reset( windowlist[0] );
+        { // We can't use the algorithm as it scales the window into nothingness
+          // Instead just move the window to its original location
+        m_motionManager.moveWindow( windowlist[0], windowlist[0]->geometry() );
         return;
         }
 

commit 1efa1b0376a11830e71e86229b74accdc51180f0

Fix regression caused by r889761. Also fix windows scaling off into nothingness when they do not overlap another window.
svn path=/trunk/KDE/kdebase/workspace/; revision=889766

effects/presentwindows.cpp
@@ -739,9 +739,16 @@ void PresentWindowsEffect::calculateWindowTransformationsKompose( EffectWindowLi
 
 void PresentWindowsEffect::calculateWindowTransformationsNatural( EffectWindowList windowlist, int screen )
     {
+    // If windows do not overlap they scale into nothingness, fix by resetting. To reproduce
+    // just have a single window on a Xinerama screen or have two windows that do not touch.
+    // TODO: Work out why this happens, is most likely a bug in the manager.
+    foreach( EffectWindow *w, windowlist )
+        if( m_motionManager.transformedGeometry( w ) == w->geometry() )
+            m_motionManager.reset( w );
+
     if( windowlist.count() == 1 )
-        { // We can't use the algorithm as it scales the window into nothingness
-          // Instead just move the window to its original location
+        {
+        // Just move the window to its original location to save time
         m_motionManager.moveWindow( windowlist[0], windowlist[0]->geometry() );
         return;
         }