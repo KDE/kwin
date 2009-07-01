71095bbf38db5d439c65d1abc2b7f82684fb44bc
Laurent Montel <montel@kde.org>
2009-07-01 07:01:58 +0000

    Fix include
    svn path=/trunk/KDE/kdebase/workspace/; revision=989833

--- clients/aurorae/src/config/config.cpp
+++ clients/aurorae/src/config/config.cpp
@@ -33,7 +33,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <KUrlRequesterDialog>
 #include <KDE/Plasma/FrameSvg>
 
-#include <QAbstractItemView>
 #include <QFile>
 #include <QPainter>
 
