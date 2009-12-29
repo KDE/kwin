2f05afa1503a6409553658e48292679110973abd
Jan Gerrit Marker <jangerrit@weiler-marker.com>
2009-12-29 14:35:53 +0000

    Add an icon to the specific window settings dialog. It takes the same icon as the contextual menu uses.
    svn path=/trunk/KDE/kdebase/workspace/; revision=1067398

--- kcmkwin/kwinrules/ruleswidget.cpp
+++ kcmkwin/kwinrules/ruleswidget.cpp
@@ -710,6 +710,7 @@ RulesDialog::RulesDialog( QWidget* parent, const char* name )
       setModal( true );
       setCaption( i18n( "Edit Window-Specific Settings" ) );
       setButtons( Ok | Cancel );
+      setWindowIcon(KIcon("preferences-system-windows-actions"));
 
       widget = new RulesWidget( this );
       setMainWidget( widget );
