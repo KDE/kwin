f3dbf99a9d4608b25da6e3541e3f9827d48dbeca
Martin Gräßlin <kde@martin-graesslin.com>
2010-01-03 15:43:18 +0000

    List screen edges kcm in "Configure window behaviour".
    svn path=/trunk/KDE/kdebase/workspace/; revision=1069509

--- workspace.cpp
+++ workspace.cpp
@@ -1238,7 +1238,7 @@ QStringList Workspace::configModules( bool controlCenter )
         args << "kwinoptions";
     else if( KAuthorized::authorizeControlModule( "kde-kwinoptions.desktop" ))
         args << "kwinactions" << "kwinfocus" <<  "kwinmoving" << "kwinadvanced"
-             << "kwinrules" << "kwincompositing" << "kwintabbox";
+             << "kwinrules" << "kwincompositing" << "kwintabbox" << "kwinscreenedges";
     return args;
     }
 
