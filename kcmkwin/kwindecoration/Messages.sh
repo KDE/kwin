334c865bf005e3ea52a30e19c90c6b2c3ae6b5de
Martin Gräßlin <kde@martin-graesslin.com>
2010-04-16 21:43:13 +0000

    Extract messages from ui files. SVN_SILENT
    svn path=/trunk/KDE/kdebase/workspace/; revision=1115577

--- kcmkwin/kwindecoration/Messages.sh
+++ kcmkwin/kwindecoration/Messages.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
+$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
 $XGETTEXT *.cpp -o $podir/kcmkwindecoration.pot
+rm -f rc.cpp
--- kcmkwin/kwindesktop/Messages.sh
+++ kcmkwin/kwindesktop/Messages.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
+$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
 $XGETTEXT *.cpp -o $podir/kcm_kwindesktop.pot
+rm -f rc.cpp
