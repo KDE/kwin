fc54bdfe8995a1298c0350f3b003b1ebc071a14c
Martin Gräßlin <kde@martin-graesslin.com>
2011-02-06 09:27:30 +0100

    Unbind shader before copying pixels to back buffer.
    This solves the regression with NVIDIA drivers resulting in a black screen.
    Apparently NVIDIA does not support glXCopySubBuffer and the copying of pixels
    fails with a shader bound.
    
    So unbinding the shader before copying the pixels and rebinding the shader
    afterwards resolves the issue.
    
    This allows to remove the temporary hack to make NVIDIA work, again.

--- scene_opengl_glx.cpp
+++ scene_opengl_glx.cpp
@@ -587,6 +587,13 @@ void SceneOpenGL::flushBuffer(int mask, QRegion damage)
                     glXCopySubBuffer(display(), glxbuffer, r.x(), y, r.width(), r.height());
                 }
             } else {
+                // if a shader is bound, copy pixels results in a black screen
+                // therefore unbind the shader and restore after copying the pixels
+                GLint shader = 0;
+                if (ShaderManager::instance()->isShaderBound()) {
+                   glGetIntegerv(GL_CURRENT_PROGRAM, &shader);
+                   glUseProgram(0);
+                }
                 // no idea why glScissor() is used, but Compiz has it and it doesn't seem to hurt
                 glEnable(GL_SCISSOR_TEST);
                 glDrawBuffer(GL_FRONT);
@@ -608,6 +615,10 @@ void SceneOpenGL::flushBuffer(int mask, QRegion damage)
                 glBitmap(0, 0, 0, 0, -xpos, -ypos, NULL);   // move position back to 0,0
                 glDrawBuffer(GL_BACK);
                 glDisable(GL_SCISSOR_TEST);
+                // rebind previously bound shader
+                if (ShaderManager::instance()->isShaderBound()) {
+                   glUseProgram(shader);
+                }
             }
         } else {
             waitSync();
