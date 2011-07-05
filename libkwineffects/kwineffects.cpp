eae91e189ca8dd49ae1e1c6da87a2225f593bc46
Martin Gräßlin <mgraesslin@kde.org>
2011-07-05 20:40:27 +0200

    Remove include to kwinglutils.h from kwineffects
    No more OpenGL is used in the effects library itself.

--- libkwineffects/kwineffects.cpp
+++ libkwineffects/kwineffects.cpp
@@ -21,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "kwineffects.h"
 
-#include "kwinglutils.h"
 #include "kwinxrenderutils.h"
 
 #include <QtDBus/QtDBus>
