7e4e43d203f1caf8a2075ea192768dcac89e0425
Martin Gräßlin <mgraesslin@kde.org>
2011-07-17 17:09:18 +0200

    Make ShowFpsEffectConfig build without GL dep
    Forward declarations to the help!

--- effects/showfps/showfps_config.cpp
+++ effects/showfps/showfps_config.cpp
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 
 #include "showfps_config.h"
+#include "showfps.h"
 
 #include <kwineffects.h>
 
