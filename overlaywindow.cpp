055b4d032f8132347dfc3959dbaeae8ac3c4ae99
Martin Gräßlin <mgraesslin@kde.org>
2011-08-03 08:12:50 +0200

    Include own header first line
    Krazy--
    SVN_SILENT

--- overlaywindow.cpp
+++ overlaywindow.cpp
@@ -18,9 +18,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 
-#include <config-X11.h>
-
 #include "overlaywindow.h"
+
+#include <config-X11.h>
 
 #include "kwinglobals.h"
 
