f931cd22f9bb78f3d24dc49431f17e00ee59893f
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-09-20 14:08:34 +0200

    Use Client::layoutMetric to access shadowSize instead of shadowCache. This fixes some painting issues for maximized windows.

--- clients/oxygen/oxygenclient.cpp
+++ clients/oxygen/oxygenclient.cpp
@@ -650,9 +650,7 @@ namespace Oxygen
         }
 
         QRect r = (isPreview()) ? this->widget()->rect():window->rect();
-
-        const qreal shadowSize( shadowCache().shadowSize() );
-        r.adjust( shadowSize, shadowSize, -shadowSize, -shadowSize );
+        r.adjust( layoutMetric( LM_OuterPaddingLeft ), layoutMetric( LM_OuterPaddingTop ), -layoutMetric( LM_OuterPaddingRight ), -layoutMetric( LM_OuterPaddingBottom ) );
         r.adjust(0,0, 1, 1);
 
         // base color
@@ -794,8 +792,7 @@ namespace Oxygen
         }
 
         QRect r = (isPreview()) ? this->widget()->rect():window->rect();
-        qreal shadowSize( shadowCache().shadowSize() );
-        r.adjust( shadowSize, shadowSize, -shadowSize, -shadowSize );
+        r.adjust( layoutMetric( LM_OuterPaddingLeft ), layoutMetric( LM_OuterPaddingTop ), -layoutMetric( LM_OuterPaddingRight ), -layoutMetric( LM_OuterPaddingBottom ) );
 
         // dimensions
         const int titleHeight = layoutMetric(LM_TitleHeight);
