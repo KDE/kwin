5c6febf147d53db2ad668a53b896d74edea52cea
Martin Gräßlin <mgraesslin@kde.org>
2012-01-01 09:23:48 +0100

    Use Q_SLOTS instead of slots
    Krazy wants it that way.
    SVN_SILENT

--- libkwineffects/kwinanimationeffect.h
+++ libkwineffects/kwinanimationeffect.h
@@ -164,7 +164,7 @@ protected:
 private:
     float interpolated( const AniData&, int i = 0 ) const;
     float progress( const AniData& ) const;
-private slots:
+private Q_SLOTS:
     void init();
     void triggerRepaint();
     void _windowClosed( EffectWindow* w );
