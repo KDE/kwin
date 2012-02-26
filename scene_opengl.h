83affb0d813135f675e093fdc633f6ce4e7ce93c
Martin Gräßlin <mgraesslin@kde.org>
2012-02-26 11:47:13 +0100

    Droped unused XSHM import

--- scene_opengl.h
+++ scene_opengl.h
@@ -28,10 +28,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "kwinglutils.h"
 #include "kwingltexture_p.h"
 
-#ifdef HAVE_XSHM
-#include <X11/extensions/XShm.h>
-#endif
-
 namespace KWin
 {
 
