e71364a13e3c561048a893dfff8edc869de0c777
Martin Gräßlin <mgraesslin@kde.org>
2012-02-27 21:10:14 +0100

    Script to synchronize skip switcher with skip taskbar
    REVIEW: 104099

---
+++ scripts/synchronizeskipswitcher/contents/code/main.js
@@ -0,0 +1,34 @@
+/********************************************************************
+ KWin - the KDE window manager
+ This file is part of the KDE project.
+
+Copyright (C) 2012 Martin Gräßlin <mgraesslin@kde.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+function synchronizeSwitcher(client) {
+    client.skipSwitcher = client.skipTaskbar;
+}
+
+function setupConnection(client) {
+    synchronizeSwitcher(client);
+    client.skipTaskbarChanged.connect(client, synchronizeSwitcher);
+}
+
+workspace.clientAdded.connect(setupConnection);
+// connect all existing clients
+var clients = workspace.clientList();
+for (var i=0; i<clients.length; i++) {
+    setupConnection(clients[i]);
+}
