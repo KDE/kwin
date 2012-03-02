b8478624aad61ee2f6e782f3fd917edf14286cac
Montel Laurent <montel@kde.org>
2012-03-02 13:58:39 +0100

    Fix warning

--- libkwineffects/kwineffects.h
+++ libkwineffects/kwineffects.h
@@ -315,7 +315,7 @@ public:
         /**
          * Window will be painted with a lanczos filter.
          **/
-        PAINT_WINDOW_LANCZOS = 1 << 8,
+        PAINT_WINDOW_LANCZOS = 1 << 8
         // PAINT_SCREEN_WITH_TRANSFORMED_WINDOWS_WITHOUT_FULL_REPAINTS = 1 << 9 has been removed
     };
 
