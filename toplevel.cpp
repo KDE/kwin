1cedbe6c2332b0948645ea1357bc30c1b09a6df6
Thomas Lübking <thomas.luebking@gmail.com>
2012-03-16 18:53:19 +0100

    fix new shadow update repaints
    resetting is no longer needed with philip layer changes and actually
    breaks damages on simultaneous resizes
    also use addLayerRepaint for the shadow update
    
    REVIEW: 104306

--- toplevel.cpp
+++ toplevel.cpp
@@ -381,9 +381,8 @@ void Toplevel::getShadow()
     if (hasShadow())
         dirtyRect |= shadow()->shadowRegion().boundingRect();
     if (dirtyRect.isValid()) {
-        resetRepaints();
         dirtyRect.translate(pos());
-        workspace()->addRepaint(dirtyRect);
+        addLayerRepaint(dirtyRect);
     }
 }
 
