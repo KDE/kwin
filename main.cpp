40970a32f13d7c839d935ca7f812b44551495dba
Martin Gräßlin <mgraesslin@kde.org>
2012-03-16 10:46:51 +0100

    Drop workaround for BUG 157659
    Workaround for a Qt4.4RC1 redrawing bug is no longer needed.
    CCBUG: 157659
    REVIEW: 104300

--- main.cpp
+++ main.cpp
@@ -496,9 +496,6 @@ KDE_EXPORT int kdemain(int argc, char * argv[])
     if (KDE_signal(SIGHUP, KWin::sighandler) == SIG_IGN)
         KDE_signal(SIGHUP, SIG_IGN);
 
-    // HACK: this is needed to work around a Qt4.4.0RC1 bug (#157659)
-    setenv("QT_SLOW_TOPLEVEL_RESIZE", "1", true);
-
     // Disable the glib event loop integration, since it seems to be responsible
     // for several bug reports about high CPU usage (bug #239963)
     setenv("QT_NO_GLIB", "1", true);
