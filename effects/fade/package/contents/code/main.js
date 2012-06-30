34ba9411c9bc0443f543659cf3a1e0a59647ed44
Thomas Lübking <thomas.luebking@gmail.com>
2012-06-30 13:50:02 +0200

    use proper onCurrentDesktop instead of isOnCurrentDesktop
    BUG: 302784
    FIXED-IN: 4.9
    REVIEW: 105394

--- effects/fade/package/contents/code/main.js
+++ effects/fade/package/contents/code/main.js
@@ -24,7 +24,7 @@ function isFadeWindow(w) {
     } else if (!w.deleted && effect.isGrabbed(w, Effect.WindowAddedGrabRole)) {
         return false;
     }
-    return w.isOnCurrentDesktop && !isLoginWindow(w) && !w.desktopWindow && !w.utility;
+    return w.onCurrentDesktop && !isLoginWindow(w) && !w.desktopWindow && !w.utility;
 }
 
 function isLoginWindow(w) {
