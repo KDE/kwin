0ee5c40faad86806cebfbd7bc713edfb3bd9d7d2
Guy Maurel <guy-kde@maurel.de>
2012-07-12 19:22:26 +0200

    missing include

--- scripting/workspace_wrapper.h
+++ scripting/workspace_wrapper.h
@@ -24,6 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <QtCore/QObject>
 #include <QtCore/QSize>
+#include <QtCore/QStringList>
 #include <kwinglobals.h>
 
 namespace KWin
