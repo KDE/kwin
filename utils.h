cefaa756e79b159ad62dae00389e95af0d6bbaa6
Martin Gräßlin <mgraesslin@kde.org>
2013-05-02 10:10:36 +0200

    Port ClientWinMask to XCB
    Enum values are nicer than defines.

--- utils.h
+++ utils.h
@@ -54,16 +54,16 @@ const int SUPPORTED_UNMANAGED_WINDOW_TYPES_MASK = NET::NormalMask | NET::Desktop
         | NET::UtilityMask | NET::SplashMask | NET::DropdownMenuMask | NET::PopupMenuMask
         | NET::TooltipMask | NET::NotificationMask | NET::ComboBoxMask | NET::DNDIconMask;
 
-const long ClientWinMask = KeyPressMask | KeyReleaseMask |
-                           ButtonPressMask | ButtonReleaseMask |
-                           KeymapStateMask |
-                           ButtonMotionMask |
-                           PointerMotionMask | // need this, too!
-                           EnterWindowMask | LeaveWindowMask |
-                           FocusChangeMask |
-                           ExposureMask |
-                           StructureNotifyMask |
-                           SubstructureRedirectMask;
+const long ClientWinMask = XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE |
+                           XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE |
+                           XCB_EVENT_MASK_KEYMAP_STATE |
+                           XCB_EVENT_MASK_BUTTON_MOTION |
+                           XCB_EVENT_MASK_POINTER_MOTION | // need this, too!
+                           XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_LEAVE_WINDOW |
+                           XCB_EVENT_MASK_FOCUS_CHANGE |
+                           XCB_EVENT_MASK_EXPOSURE |
+                           XCB_EVENT_MASK_STRUCTURE_NOTIFY |
+                           XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT;
 
 const QPoint invalidPoint(INT_MIN, INT_MIN);
 
