95a6329fe55c7c8aa4dcdc84c45c3c08e2bf72cc
Martin Gräßlin <mgraesslin@kde.org>
2013-09-05 10:39:47 +0200

    Remove some no longer needed includes
    less warnings

--- effects/coverswitch/coverswitch.cpp
+++ effects/coverswitch/coverswitch.cpp
@@ -27,7 +27,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QMatrix4x4>
 #include <QMouseEvent>
 #include <KDE/KLocalizedString>
-#include <kapplication.h>
 #include <kcolorscheme.h>
 
 #include <kwinglutils.h>
