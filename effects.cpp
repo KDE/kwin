91c420ada0c92f4ccea755ebd230110f94376928
Martin Gräßlin <mgraesslin@kde.org>
2014-01-22 08:22:55 +0100

    [kwin] Fix possible crash in desktopPresenceChanged
    It's possible that the Client does not have an effect window when
    the desktop presence changes. This results in a crash.
    
    Unit test which triggered the crash on
    https://git.reviewboard.kde.org/r/115190/
    
    REVIEW: 115214

--- effects.cpp
+++ effects.cpp
@@ -645,6 +645,9 @@ void EffectsHandlerImpl::slotDesktopChanged(int old, Client *c)
 
 void EffectsHandlerImpl::slotDesktopPresenceChanged(Client *c, int old)
 {
+    if (!c->effectWindow()) {
+        return;
+    }
     emit desktopPresenceChanged(c->effectWindow(), old, c->desktop());
 }
 
