KDE/kwin

commit 8543033d59c5bfb2ae034bf39fa5cf61a626d2ed

Reparent decoration window by using a QWindow wrapper for the frame
Qt doesn't like that we reparent the decoration using low level xcb
calls. So let's use a QWindow wrapper for the frame and let Qt do
the reparenting itself.

BUG: 334768
REVIEW: 118159

client.cpp
@@ -558,7 +558,7 @@ void Client::createDecoration(const QRect& oldgeom)
     } else if (decoration->window()) {
         decoration->window()->installEventFilter(this);
     }
-    xcb_reparent_window(connection(), decoration->window()->winId(), frameId(), 0, 0);
+    decoration->window()->setParent(m_frameWrapper.data());
     decoration->window()->lower();
     decoration->borders(border_left, border_right, border_top, border_bottom);
     padding_left = padding_right = padding_top = padding_bottom = 0;

client.h
@@ -859,6 +859,8 @@ private:
     Xcb::Window m_client;
     Xcb::Window m_wrapper;
     Xcb::Window m_frame;
+    // wrapper around m_frame to use as a parent for the decoration
+    QScopedPointer<QWindow> m_frameWrapper;
     KDecoration* decoration;
     Bridge* bridge;
     int desk;

manage.cpp
@@ -683,6 +683,7 @@ void Client::embedClient(xcb_window_t w, xcb_visualid_t visualid, xcb_colormap_t
     xcb_create_window(conn, depth, frame, rootWindow(), 0, 0, 1, 1, 0,
                       XCB_WINDOW_CLASS_INPUT_OUTPUT, visualid, cw_mask, cw_values);
     m_frame.reset(frame);
+    m_frameWrapper.reset(QWindow::fromWinId(m_frame));
 
     setWindowHandles(m_client);
 

commit 76b05f241bac347ffa69ff011a0d5bcc5cdd2f02

Only reparent qwidget based deco to a QWindow wrapper
This partially reverts 8543033d59c5bfb2ae034bf39fa5cf61a626d2ed which
caused major issues with QWindow based decorations.

BUG: 335762

client.cpp
@@ -555,10 +555,11 @@ void Client::createDecoration(const QRect& oldgeom)
     decoration->init();
     if (decoration->widget()) {
         decoration->widget()->installEventFilter(this);
+        decoration->window()->setParent(m_frameWrapper.data());
     } else if (decoration->window()) {
         decoration->window()->installEventFilter(this);
+        xcb_reparent_window(connection(), decoration->window()->winId(), frameId(), 0, 0);
     }
-    decoration->window()->setParent(m_frameWrapper.data());
     decoration->window()->lower();
     decoration->borders(border_left, border_right, border_top, border_bottom);
     padding_left = padding_right = padding_top = padding_bottom = 0;