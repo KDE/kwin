3c87cea7a87400eda223ad6ba8cc6a9096fa896d
Martin Gräßlin <mgraesslin@kde.org>
2014-09-17 15:57:56 +0200

    Change to LGPLv2+
    I'm the only copyright holder of the code in this repository, thus
    I can change from GPL to LGPL!

--- src/wayland/seat_interface.h
+++ src/wayland/seat_interface.h
@@ -1,21 +1,21 @@
 /********************************************************************
- KWin - the KDE window manager
- This file is part of the KDE project.
+Copyright 2014  Martin Gräßlin <mgraesslin@kde.org>
 
-Copyright (C) 2014 Martin Gräßlin <mgraesslin@kde.org>
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) version 3, or any
+later version accepted by the membership of KDE e.V. (or its
+successor approved by the membership of KDE e.V.), which shall
+act as a proxy defined in Section 6 of version 3 of the license.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
+This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public
+License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 #ifndef KWIN_WAYLAND_SERVER_SEAT_INTERFACE_H
 #define KWIN_WAYLAND_SERVER_SEAT_INTERFACE_H
