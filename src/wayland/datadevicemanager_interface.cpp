f8bc3e659821c6986ab325217f4a4cce288dfbd5
Martin Gräßlin <mgraesslin@kde.org>
2014-11-19 19:07:14 +0100

    Drop incorrect Q_UNUSED

--- src/wayland/datadevicemanager_interface.cpp
+++ src/wayland/datadevicemanager_interface.cpp
@@ -87,9 +87,6 @@ void DataDeviceManagerInterface::Private::createDataSourceCallback(wl_client *cl
 
 void DataDeviceManagerInterface::Private::createDataSource(wl_client *client, wl_resource *resource, uint32_t id)
 {
-    Q_UNUSED(client)
-    Q_UNUSED(resource)
-    Q_UNUSED(id)
     DataSourceInterface *dataSource = new DataSourceInterface(q);
     dataSource->create(display->getConnection(client), wl_resource_get_version(resource), id);
     if (!dataSource->resource()) {
