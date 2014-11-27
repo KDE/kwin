f4c0930a266af3302ce286e96a07ed82bda0a7f5
Martin Gräßlin <mgraesslin@kde.org>
2014-11-27 19:25:17 +0100

    renderingservertest: Proper DPI for the Output
    According to Wikipedia:
    * screens have a default DPI of 96
    * 96 dot/in are 38 dot/cm
    * 1024/768 -> 269/202 mm

--- src/wayland/tests/renderingservertest.cpp
+++ src/wayland/tests/renderingservertest.cpp
@@ -205,7 +205,7 @@ int main(int argc, char **argv)
     shell->create();
     display.createShm();
     OutputInterface *output = display.createOutput(&display);
-    output->setPhysicalSize(QSize(400, 300));
+    output->setPhysicalSize(QSize(269, 202));
     const QSize windowSize(1024, 768);
     output->addMode(windowSize);
     output->create();
