20a1cb2e33371bff6b4fc3643bbe2f895c7a6a35
Lukáš Tinkl <lukas@kde.org>
2014-12-16 21:01:40 +0100

    extract the strings also from QML

--- kcmkwin/kwindecoration/Messages.sh
+++ kcmkwin/kwindecoration/Messages.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT *.cpp -o $podir/kcmkwindecoration.pot
+#!bin/sh
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find . -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/kcmkwindecoration.pot
 rm -f rc.cpp
