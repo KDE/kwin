85d87297b04d756138b506ef18aa883b096af102
Michael Pyne <mpyne@kde.org>
2015-03-19 23:22:46 -0400

    Return false for a bool instead of nullptr.
    Fixes compiler warning (error by default in gcc 5).
    
    REVIEW:123077

--- autotests/mock_effectshandler.h
+++ autotests/mock_effectshandler.h
@@ -131,7 +131,7 @@ public:
         return nullptr;
     }
     bool grabKeyboard(KWin::Effect *) override {
-        return nullptr;
+        return false;
     }
     bool hasDecorationShadows() const override {
         return false;
