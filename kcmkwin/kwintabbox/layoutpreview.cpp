2e2ea354b62e73f0307719002e6705bca771295e
Martin Gräßlin <mgraesslin@kde.org>
2015-03-25 08:18:12 +0100

    [kcmkwin/tabbox] static_cast QKeyEvent
    Fixes a defect reported by coverity.

--- kcmkwin/kwintabbox/layoutpreview.cpp
+++ kcmkwin/kwintabbox/layoutpreview.cpp
@@ -88,7 +88,7 @@ LayoutPreview::~LayoutPreview()
 bool LayoutPreview::eventFilter(QObject *object, QEvent *event)
 {
     if (event->type() == QEvent::KeyPress) {
-        QKeyEvent *keyEvent = dynamic_cast<QKeyEvent*>(event);
+        QKeyEvent *keyEvent = static_cast<QKeyEvent*>(event);
         if (keyEvent->key() == Qt::Key_Escape ||
                 keyEvent->key() == Qt::Key_Return ||
                 keyEvent->key() == Qt::Key_Enter ||
