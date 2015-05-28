KDE/kwin

commit 90a6814513f79952df1addac0552fdfc338a6de9

[wayland] Place ShellClients
When a ShellClient is added and it's not internal, it get placed just
like any other Client. This needs to happen after the initial size is
determined.

Please note: this breaks the positioning of popup windows (e.g. menus)
as they are placed like any other Client. This needs proper popup support
which right now does not yet exist and thus is not much difference to
before.

wayland_server.cpp
@@ -127,7 +127,7 @@ void WaylandServer::init(const QByteArray &socketName)
                 connect(client, &ShellClient::windowShown, this,
                     [this, client] {
                         emit shellClientAdded(client);
-                    }
+                    }, Qt::QueuedConnection
                 );
             }
         }

workspace.cpp
@@ -379,6 +379,8 @@ void Workspace::init()
         connect(w, &WaylandServer::shellClientAdded, this,
             [this] (ShellClient *c) {
                 if (!c->isInternal()) {
+                    QRect area = clientArea(PlacementArea, Screens::self()->current(), c->desktop());
+                    Placement::self()->place(c, area);
                     if (!unconstrained_stacking_order.contains(c))
                         unconstrained_stacking_order.append(c);   // Raise if it hasn't got any stacking position yet
                     if (!stacking_order.contains(c))    // It'll be updated later, and updateToolWindows() requires

commit 988ce28943dbdbda2d418ee777a0c2259cb50343

[wayland] Ensure size is updated before marking window as ready for painting
Fixes regression introduced with 90a6814: we may not queue a signal
taking a pointer to a ShellClient as the ShellClient might be destroyed
before the queued signal is delivered.

The idea for the queued signal was to ensure that the size is set when
windowShown is emitted - this can also be achieved by first updating the
size.

shell_client.cpp
@@ -151,11 +151,11 @@ void ShellClient::setOpacity(double opacity)
 
 void ShellClient::addDamage(const QRegion &damage)
 {
-    setReadyForPainting();
     if (m_shellSurface->surface()->buffer()->size().isValid()) {
         m_clientSize = m_shellSurface->surface()->buffer()->size();
         setGeometry(QRect(geom.topLeft(), m_clientSize));
     }
+    setReadyForPainting();
     Toplevel::addDamage(damage);
 }
 

wayland_server.cpp
@@ -127,7 +127,7 @@ void WaylandServer::init(const QByteArray &socketName)
                 connect(client, &ShellClient::windowShown, this,
                     [this, client] {
                         emit shellClientAdded(client);
-                    }, Qt::QueuedConnection
+                    }
                 );
             }
         }