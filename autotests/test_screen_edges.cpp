7365069ba0fafdc773d3b16dea80da2afca0781a
Martin Gräßlin <mgraesslin@kde.org>
2015-09-30 10:29:17 +0200

    [autotests] Adjust TestScreenEdges::testCallback to changes
    We need to add more delay to get it trigger/pushback again.

--- autotests/test_screen_edges.cpp
+++ autotests/test_screen_edges.cpp
@@ -477,7 +477,7 @@ void TestScreenEdges::testCallback()
     QCOMPARE(Cursor::pos(), QPoint(1, 101));
 
     // now let's try to trigger again
-    QTest::qWait(100);
+    QTest::qWait(351);
     setPos(QPoint(0, 100));
     event.time = QDateTime::currentMSecsSinceEpoch();
     QVERIFY(s->isEntered(&event));
@@ -507,6 +507,7 @@ void TestScreenEdges::testCallback()
     s->setConfig(config);
     s->reconfigure();
     // it should trigger directly
+    QTest::qWait(350);
     event.time = QDateTime::currentMSecsSinceEpoch();
     QVERIFY(s->isEntered(&event));
     QCOMPARE(spy.count(), 3);
