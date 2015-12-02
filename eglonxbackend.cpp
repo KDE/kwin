KDE/kwin

commit b1914b4b2cc4a97d2522e8b83cee54b30f8a13c5

Add pure virtual OpenGLBackend::init() method
All backends already have an init method so far called from the ctor.
This change moves the call to init out of the OpenGL backends and makes
it the responsibility of the creating code to also call init on the
backend.

This change makes it easier to have virtual methods being called during
the initialization.

eglonxbackend.cpp
@@ -63,7 +63,6 @@ EglOnXBackend::EglOnXBackend(xcb_connection_t *connection, Display *display, xcb
     , m_x11ScreenNumber(screenNumber)
     , m_renderingWindow(renderingWindow)
 {
-    init();
     // Egl is always direct rendering
     setIsDirectRendering(true);
 }

backends/drm/egl_gbm_backend.cpp
@@ -39,8 +39,6 @@ EglGbmBackend::EglGbmBackend(DrmBackend *b)
     , AbstractEglBackend()
     , m_backend(b)
 {
-    initializeEgl();
-    init();
     // Egl is always direct rendering
     setIsDirectRendering(true);
     setSyncsToVBlank(true);
@@ -117,6 +115,10 @@ bool EglGbmBackend::initializeEgl()
 
 void EglGbmBackend::init()
 {
+    if (!initializeEgl()) {
+        setFailed("Could not initialize egl");
+        return;
+    }
     if (!initRenderingContext()) {
         setFailed("Could not initialize rendering context");
         return;

backends/drm/egl_gbm_backend.h
@@ -48,13 +48,13 @@ public:
     bool usesOverlayWindow() const override;
     bool perScreenRendering() const override;
     QRegion prepareRenderingForScreen(int screenId) override;
+    void init() override;
 
 protected:
     void present() override;
     void cleanupSurfaces() override;
 
 private:
-    void init();
     bool initializeEgl();
     bool initBufferConfigs();
     bool initRenderingContext();

backends/hwcomposer/egl_hwcomposer_backend.cpp
@@ -28,11 +28,6 @@ EglHwcomposerBackend::EglHwcomposerBackend(HwcomposerBackend *backend)
     : AbstractEglBackend()
     , m_backend(backend)
 {
-    if (!initializeEgl()) {
-        setFailed("Failed to initialize egl");
-        return;
-    }
-    init();
     // EGL is always direct rendering
     setIsDirectRendering(true);
     setSyncsToVBlank(true);
@@ -60,6 +55,10 @@ bool EglHwcomposerBackend::initializeEgl()
 
 void EglHwcomposerBackend::init()
 {
+    if (!initializeEgl()) {
+        setFailed("Failed to initialize egl");
+        return;
+    }
     if (!initRenderingContext()) {
         setFailed("Could not initialize rendering context");
         return;

backends/hwcomposer/egl_hwcomposer_backend.h
@@ -37,12 +37,12 @@ public:
     void screenGeometryChanged(const QSize &size) override;
     QRegion prepareRenderingFrame() override;
     void endRenderingFrame(const QRegion &renderedRegion, const QRegion &damagedRegion) override;
+    void init() override;
 
 protected:
     void present() override;
 
 private:
-    void init();
     bool initializeEgl();
     bool initRenderingContext();
     bool initBufferConfigs();

backends/virtual/egl_gbm_backend.cpp
@@ -36,8 +36,6 @@ EglGbmBackend::EglGbmBackend(VirtualBackend *b)
     , AbstractEglBackend()
     , m_backend(b)
 {
-    initializeEgl();
-    init();
     // Egl is always direct rendering
     setIsDirectRendering(true);
 }
@@ -75,6 +73,10 @@ bool EglGbmBackend::initializeEgl()
 
 void EglGbmBackend::init()
 {
+    if (!initializeEgl()) {
+        setFailed("Could not initialize egl");
+        return;
+    }
     if (!initRenderingContext()) {
         setFailed("Could not initialize rendering context");
         return;

backends/virtual/egl_gbm_backend.h
@@ -42,12 +42,12 @@ public:
     QRegion prepareRenderingFrame() override;
     void endRenderingFrame(const QRegion &renderedRegion, const QRegion &damagedRegion) override;
     bool usesOverlayWindow() const override;
+    void init() override;
 
 protected:
     void present() override;
 
 private:
-    void init();
     bool initializeEgl();
     bool initBufferConfigs();
     bool initRenderingContext();

backends/wayland/egl_wayland_backend.cpp
@@ -55,8 +55,6 @@ EglWaylandBackend::EglWaylandBackend(Wayland::WaylandBackend *b)
         return;
     }
     connect(m_wayland, SIGNAL(shellSurfaceSizeChanged(QSize)), SLOT(overlaySizeChanged(QSize)));
-    initializeEgl();
-    init();
     // Egl is always direct rendering
     setIsDirectRendering(true);
 }
@@ -95,6 +93,10 @@ bool EglWaylandBackend::initializeEgl()
 
 void EglWaylandBackend::init()
 {
+    if (!initializeEgl()) {
+        setFailed("Could not initialize egl");
+        return;
+    }
     if (!initRenderingContext()) {
         setFailed("Could not initialize rendering context");
         return;

commit c5959a5ffa54bb87cf654f54363637acb34becc1

Fix regression in EglOnXBackend caused by b1914b4b2cc4a97d2522e8b83cee54b30f8a13c5
The ctor used by kwin_x11 still had the call to init which is now
performed by SceneOpenGL. This means init was performed twice
ultimately hitting an assert when creating the overlay window.

eglonxbackend.cpp
@@ -46,7 +46,6 @@ EglOnXBackend::EglOnXBackend()
     , m_rootWindow(rootWindow())
     , m_x11ScreenNumber(kwinApp()->x11ScreenNumber())
 {
-    init();
     // Egl is always direct rendering
     setIsDirectRendering(true);
 }