3aeec88449d1e7f3aca16b4b9e3b9ec2caa0fee4
Martin Gräßlin <mgraesslin@kde.org>
2016-02-11 12:48:42 +0100

    [autotest] Add test for internal pointer warping
    When warping a pointer through Cursor::setPos it should be processed
    just like any other pointer event. It should generate enter/leave event,
    create motion events, etc. This is currently not the case as the test
    shows.

---
+++ autotests/wayland/pointer_input.cpp
@@ -0,0 +1,279 @@
+/********************************************************************
+KWin - the KDE window manager
+This file is part of the KDE project.
+
+Copyright (C) 2016 Martin Gräßlin <mgraesslin@kde.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+#include "kwin_wayland_test.h"
+#include "abstract_backend.h"
+#include "abstract_client.h"
+#include "cursor.h"
+#include "deleted.h"
+#include "screenedge.h"
+#include "screens.h"
+#include "wayland_server.h"
+#include "workspace.h"
+#include "shell_client.h"
+#include <kwineffects.h>
+
+#include <KWayland/Client/connection_thread.h>
+#include <KWayland/Client/compositor.h>
+#include <KWayland/Client/event_queue.h>
+#include <KWayland/Client/registry.h>
+#include <KWayland/Client/pointer.h>
+#include <KWayland/Client/shell.h>
+#include <KWayland/Client/seat.h>
+#include <KWayland/Client/shm_pool.h>
+#include <KWayland/Client/surface.h>
+
+#include <KWayland/Server/seat_interface.h>
+
+namespace KWin
+{
+
+static const QString s_socketName = QStringLiteral("wayland_test_kwin_pointer_input-0");
+
+class PointerInputTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void initTestCase();
+    void init();
+    void cleanup();
+    void testWarpingUpdatesFocus();
+    void testWarpingGeneratesPointerMotion();
+
+private:
+    void render(KWayland::Client::Surface *surface);
+    KWayland::Client::ConnectionThread *m_connection = nullptr;
+    KWayland::Client::Compositor *m_compositor = nullptr;
+    KWayland::Client::Seat *m_seat = nullptr;
+    KWayland::Client::ShmPool *m_shm = nullptr;
+    KWayland::Client::Shell *m_shell = nullptr;
+    KWayland::Client::EventQueue *m_queue = nullptr;
+    QThread *m_thread = nullptr;
+};
+
+void PointerInputTest::initTestCase()
+{
+    qRegisterMetaType<KWin::ShellClient*>();
+    qRegisterMetaType<KWin::AbstractClient*>();
+    qRegisterMetaType<KWin::Deleted*>();
+    QSignalSpy workspaceCreatedSpy(kwinApp(), &Application::workspaceCreated);
+    QVERIFY(workspaceCreatedSpy.isValid());
+    waylandServer()->backend()->setInitialWindowSize(QSize(1280, 1024));
+    QMetaObject::invokeMethod(waylandServer()->backend(), "setOutputCount", Qt::DirectConnection, Q_ARG(int, 2));
+    waylandServer()->init(s_socketName.toLocal8Bit());
+
+    kwinApp()->start();
+    QVERIFY(workspaceCreatedSpy.wait());
+    QCOMPARE(screens()->count(), 2);
+    QCOMPARE(screens()->geometry(0), QRect(0, 0, 1280, 1024));
+    QCOMPARE(screens()->geometry(1), QRect(1280, 0, 1280, 1024));
+    setenv("QT_QPA_PLATFORM", "wayland", true);
+    waylandServer()->initWorkspace();
+}
+
+void PointerInputTest::init()
+{
+    using namespace KWayland::Client;
+    // setup connection
+    m_connection = new ConnectionThread;
+    QSignalSpy connectedSpy(m_connection, &ConnectionThread::connected);
+    QVERIFY(connectedSpy.isValid());
+    m_connection->setSocketName(s_socketName);
+
+    m_thread = new QThread(this);
+    m_connection->moveToThread(m_thread);
+    m_thread->start();
+
+    m_connection->initConnection();
+    QVERIFY(connectedSpy.wait());
+
+    m_queue = new EventQueue(this);
+    QVERIFY(!m_queue->isValid());
+    m_queue->setup(m_connection);
+    QVERIFY(m_queue->isValid());
+
+    Registry registry;
+    registry.setEventQueue(m_queue);
+    QSignalSpy compositorSpy(&registry, &Registry::compositorAnnounced);
+    QSignalSpy shmSpy(&registry, &Registry::shmAnnounced);
+    QSignalSpy shellSpy(&registry, &Registry::shellAnnounced);
+    QSignalSpy seatSpy(&registry, &Registry::seatAnnounced);
+    QSignalSpy allAnnounced(&registry, &Registry::interfacesAnnounced);
+    QVERIFY(allAnnounced.isValid());
+    QVERIFY(shmSpy.isValid());
+    QVERIFY(shellSpy.isValid());
+    QVERIFY(compositorSpy.isValid());
+    QVERIFY(seatSpy.isValid());
+    registry.create(m_connection->display());
+    QVERIFY(registry.isValid());
+    registry.setup();
+    QVERIFY(allAnnounced.wait());
+    QVERIFY(!compositorSpy.isEmpty());
+    QVERIFY(!shmSpy.isEmpty());
+    QVERIFY(!shellSpy.isEmpty());
+    QVERIFY(!seatSpy.isEmpty());
+
+    m_compositor = registry.createCompositor(compositorSpy.first().first().value<quint32>(), compositorSpy.first().last().value<quint32>(), this);
+    QVERIFY(m_compositor->isValid());
+    m_shm = registry.createShmPool(shmSpy.first().first().value<quint32>(), shmSpy.first().last().value<quint32>(), this);
+    QVERIFY(m_shm->isValid());
+    m_shell = registry.createShell(shellSpy.first().first().value<quint32>(), shellSpy.first().last().value<quint32>(), this);
+    QVERIFY(m_shell->isValid());
+    m_seat = registry.createSeat(seatSpy.first().first().value<quint32>(), seatSpy.first().last().value<quint32>(), this);
+    QVERIFY(m_seat->isValid());
+    QSignalSpy hasPointerSpy(m_seat, &Seat::hasPointerChanged);
+    QVERIFY(hasPointerSpy.isValid());
+    QVERIFY(hasPointerSpy.wait());
+
+    screens()->setCurrent(0);
+    Cursor::setPos(QPoint(640, 512));
+}
+
+void PointerInputTest::cleanup()
+{
+    delete m_compositor;
+    m_compositor = nullptr;
+    delete m_seat;
+    m_seat = nullptr;
+    delete m_shm;
+    m_shm = nullptr;
+    delete m_shell;
+    m_shell = nullptr;
+    delete m_queue;
+    m_queue = nullptr;
+    if (m_thread) {
+        m_connection->deleteLater();
+        m_thread->quit();
+        m_thread->wait();
+        delete m_thread;
+        m_thread = nullptr;
+        m_connection = nullptr;
+    }
+}
+
+void PointerInputTest::render(KWayland::Client::Surface *surface)
+{
+    QImage img(QSize(100, 50), QImage::Format_ARGB32);
+    img.fill(Qt::blue);
+    surface->attachBuffer(m_shm->createBuffer(img));
+    surface->damage(QRect(0, 0, 100, 50));
+    surface->commit(KWayland::Client::Surface::CommitFlag::None);
+    m_connection->flush();
+}
+
+void PointerInputTest::testWarpingUpdatesFocus()
+{
+    // this test verifies that warping the pointer creates pointer enter and leave events
+    using namespace KWayland::Client;
+    // create pointer and signal spy for enter and leave signals
+    auto pointer = m_seat->createPointer(m_seat);
+    QVERIFY(pointer);
+    QVERIFY(pointer->isValid());
+    QSignalSpy enteredSpy(pointer, &Pointer::entered);
+    QVERIFY(enteredSpy.isValid());
+    QSignalSpy leftSpy(pointer, &Pointer::left);
+    QVERIFY(leftSpy.isValid());
+
+    // create a window
+    QSignalSpy clientAddedSpy(waylandServer(), &WaylandServer::shellClientAdded);
+    QVERIFY(clientAddedSpy.isValid());
+    Surface *surface = m_compositor->createSurface(m_compositor);
+    QVERIFY(surface);
+    ShellSurface *shellSurface = m_shell->createSurface(surface, surface);
+    QVERIFY(shellSurface);
+    render(surface);
+    QVERIFY(clientAddedSpy.wait());
+    AbstractClient *window = workspace()->activeClient();
+    QVERIFY(window);
+
+    // currently there should not be a focused pointer surface
+    QVERIFY(!waylandServer()->seat()->focusedPointerSurface());
+    QVERIFY(!pointer->enteredSurface());
+
+    // enter
+    Cursor::setPos(QPoint(25, 25));
+    QEXPECT_FAIL("", "Not yet imlemented", Continue);
+    QVERIFY(enteredSpy.wait());
+    QEXPECT_FAIL("", "Not yet imlemented", Continue);
+    QCOMPARE(enteredSpy.count(), 1);
+    // window should have focus
+    QEXPECT_FAIL("", "Not yet imlemented", Continue);
+    QCOMPARE(pointer->enteredSurface(), surface);
+    // also on the server
+    QEXPECT_FAIL("", "Not yet imlemented", Continue);
+    QCOMPARE(waylandServer()->seat()->focusedPointerSurface(), window->surface());
+
+    // and out again
+    Cursor::setPos(QPoint(250, 250));;
+    QEXPECT_FAIL("", "Not yet imlemented", Continue);
+    QVERIFY(leftSpy.wait());
+    QEXPECT_FAIL("", "Not yet imlemented", Continue);
+    QCOMPARE(leftSpy.count(), 1);
+    // there should not be a focused pointer surface anymore
+    QVERIFY(!waylandServer()->seat()->focusedPointerSurface());
+    QVERIFY(!pointer->enteredSurface());
+}
+
+void PointerInputTest::testWarpingGeneratesPointerMotion()
+{
+    // this test verifies that warping the pointer creates pointer motion events
+    using namespace KWayland::Client;
+    // create pointer and signal spy for enter and motion
+    auto pointer = m_seat->createPointer(m_seat);
+    QVERIFY(pointer);
+    QVERIFY(pointer->isValid());
+    QSignalSpy enteredSpy(pointer, &Pointer::entered);
+    QVERIFY(enteredSpy.isValid());
+    QSignalSpy movedSpy(pointer, &Pointer::motion);
+    QVERIFY(movedSpy.isValid());
+
+    // create a window
+    QSignalSpy clientAddedSpy(waylandServer(), &WaylandServer::shellClientAdded);
+    QVERIFY(clientAddedSpy.isValid());
+    Surface *surface = m_compositor->createSurface(m_compositor);
+    QVERIFY(surface);
+    ShellSurface *shellSurface = m_shell->createSurface(surface, surface);
+    QVERIFY(shellSurface);
+    render(surface);
+    QVERIFY(clientAddedSpy.wait());
+    AbstractClient *window = workspace()->activeClient();
+    QVERIFY(window);
+
+    // enter
+    waylandServer()->backend()->pointerMotion(QPointF(25, 25), 1);
+    QVERIFY(enteredSpy.wait());
+    // we get a move event together with the enter, that's actually wrong but also shouldn't harm
+    QVERIFY(movedSpy.wait());
+    QCOMPARE(movedSpy.count(), 1);
+    QCOMPARE(movedSpy.first().first().toPointF(), QPointF(25, 25));
+
+    // now warp
+    Cursor::setPos(QPoint(26, 26));
+    QEXPECT_FAIL("", "Not yet imlemented", Continue);
+    QVERIFY(movedSpy.wait());
+    QEXPECT_FAIL("", "Not yet imlemented", Continue);
+    QCOMPARE(movedSpy.count(), 2);
+    QEXPECT_FAIL("", "Not yet imlemented", Continue);
+    QCOMPARE(movedSpy.last().first().toPointF(), QPointF(26, 26));
+}
+
+}
+
+WAYLANDTEST_MAIN(KWin::PointerInputTest)
+#include "pointer_input.moc"
