8e6f387a6ef7f39a1ece12969f789688983fdbc5
Martin Gräßlin <mgraesslin@kde.org>
2016-02-11 10:47:21 +0100

    [autotest] Test case for stacking order changes should change focus
    A new test case which ensures that when stacking order changes the
    pointer focus gets re-evaluated and updated. I was positively surprised
    to notice that this already works.
    
    Noticed two other problems while writing the test case:
    * warping pointer does not re-evaluate the pointer pos
    * deleting a ShellSurface (client) does not destroy the ShellClient (server)

---
+++ autotests/wayland/input_stacking_order.cpp
@@ -0,0 +1,256 @@
+/********************************************************************
+KWin - the KDE window manager
+This file is part of the KDE project.
+
+Copyright (C) 2016 Martin Gräßlin <mgraesslin@kde.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+#include "kwin_wayland_test.h"
+#include "abstract_backend.h"
+#include "abstract_client.h"
+#include "cursor.h"
+#include "deleted.h"
+#include "screenedge.h"
+#include "screens.h"
+#include "wayland_server.h"
+#include "workspace.h"
+#include "shell_client.h"
+#include <kwineffects.h>
+
+#include <KWayland/Client/connection_thread.h>
+#include <KWayland/Client/compositor.h>
+#include <KWayland/Client/event_queue.h>
+#include <KWayland/Client/registry.h>
+#include <KWayland/Client/pointer.h>
+#include <KWayland/Client/shell.h>
+#include <KWayland/Client/seat.h>
+#include <KWayland/Client/shm_pool.h>
+#include <KWayland/Client/surface.h>
+
+#include <KWayland/Server/seat_interface.h>
+
+namespace KWin
+{
+
+static const QString s_socketName = QStringLiteral("wayland_test_kwin_input_stacking_order-0");
+
+class InputStackingOrderTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void initTestCase();
+    void init();
+    void cleanup();
+    void testPointerFocusUpdatesOnStackingOrderChange();
+
+private:
+    void render(KWayland::Client::Surface *surface);
+    KWayland::Client::ConnectionThread *m_connection = nullptr;
+    KWayland::Client::Compositor *m_compositor = nullptr;
+    KWayland::Client::Seat *m_seat = nullptr;
+    KWayland::Client::ShmPool *m_shm = nullptr;
+    KWayland::Client::Shell *m_shell = nullptr;
+    KWayland::Client::EventQueue *m_queue = nullptr;
+    QThread *m_thread = nullptr;
+};
+
+void InputStackingOrderTest::initTestCase()
+{
+    qRegisterMetaType<KWin::ShellClient*>();
+    qRegisterMetaType<KWin::AbstractClient*>();
+    qRegisterMetaType<KWin::Deleted*>();
+    QSignalSpy workspaceCreatedSpy(kwinApp(), &Application::workspaceCreated);
+    QVERIFY(workspaceCreatedSpy.isValid());
+    waylandServer()->backend()->setInitialWindowSize(QSize(1280, 1024));
+    QMetaObject::invokeMethod(waylandServer()->backend(), "setOutputCount", Qt::DirectConnection, Q_ARG(int, 2));
+    waylandServer()->init(s_socketName.toLocal8Bit());
+
+    kwinApp()->start();
+    QVERIFY(workspaceCreatedSpy.wait());
+    QCOMPARE(screens()->count(), 2);
+    QCOMPARE(screens()->geometry(0), QRect(0, 0, 1280, 1024));
+    QCOMPARE(screens()->geometry(1), QRect(1280, 0, 1280, 1024));
+    setenv("QT_QPA_PLATFORM", "wayland", true);
+    waylandServer()->initWorkspace();
+}
+
+void InputStackingOrderTest::init()
+{
+    using namespace KWayland::Client;
+    // setup connection
+    m_connection = new ConnectionThread;
+    QSignalSpy connectedSpy(m_connection, &ConnectionThread::connected);
+    QVERIFY(connectedSpy.isValid());
+    m_connection->setSocketName(s_socketName);
+
+    m_thread = new QThread(this);
+    m_connection->moveToThread(m_thread);
+    m_thread->start();
+
+    m_connection->initConnection();
+    QVERIFY(connectedSpy.wait());
+
+    m_queue = new EventQueue(this);
+    QVERIFY(!m_queue->isValid());
+    m_queue->setup(m_connection);
+    QVERIFY(m_queue->isValid());
+
+    Registry registry;
+    registry.setEventQueue(m_queue);
+    QSignalSpy compositorSpy(&registry, &Registry::compositorAnnounced);
+    QSignalSpy shmSpy(&registry, &Registry::shmAnnounced);
+    QSignalSpy shellSpy(&registry, &Registry::shellAnnounced);
+    QSignalSpy seatSpy(&registry, &Registry::seatAnnounced);
+    QSignalSpy allAnnounced(&registry, &Registry::interfacesAnnounced);
+    QVERIFY(allAnnounced.isValid());
+    QVERIFY(shmSpy.isValid());
+    QVERIFY(shellSpy.isValid());
+    QVERIFY(compositorSpy.isValid());
+    QVERIFY(seatSpy.isValid());
+    registry.create(m_connection->display());
+    QVERIFY(registry.isValid());
+    registry.setup();
+    QVERIFY(allAnnounced.wait());
+    QVERIFY(!compositorSpy.isEmpty());
+    QVERIFY(!shmSpy.isEmpty());
+    QVERIFY(!shellSpy.isEmpty());
+    QVERIFY(!seatSpy.isEmpty());
+
+    m_compositor = registry.createCompositor(compositorSpy.first().first().value<quint32>(), compositorSpy.first().last().value<quint32>(), this);
+    QVERIFY(m_compositor->isValid());
+    m_shm = registry.createShmPool(shmSpy.first().first().value<quint32>(), shmSpy.first().last().value<quint32>(), this);
+    QVERIFY(m_shm->isValid());
+    m_shell = registry.createShell(shellSpy.first().first().value<quint32>(), shellSpy.first().last().value<quint32>(), this);
+    QVERIFY(m_shell->isValid());
+    m_seat = registry.createSeat(seatSpy.first().first().value<quint32>(), seatSpy.first().last().value<quint32>(), this);
+    QVERIFY(m_seat->isValid());
+    QSignalSpy hasPointerSpy(m_seat, &Seat::hasPointerChanged);
+    QVERIFY(hasPointerSpy.isValid());
+    QVERIFY(hasPointerSpy.wait());
+
+    screens()->setCurrent(0);
+    Cursor::setPos(QPoint(640, 512));
+}
+
+void InputStackingOrderTest::cleanup()
+{
+    delete m_compositor;
+    m_compositor = nullptr;
+    delete m_seat;
+    m_seat = nullptr;
+    delete m_shm;
+    m_shm = nullptr;
+    delete m_shell;
+    m_shell = nullptr;
+    delete m_queue;
+    m_queue = nullptr;
+    if (m_thread) {
+        m_connection->deleteLater();
+        m_thread->quit();
+        m_thread->wait();
+        delete m_thread;
+        m_thread = nullptr;
+        m_connection = nullptr;
+    }
+}
+
+void InputStackingOrderTest::render(KWayland::Client::Surface *surface)
+{
+    QImage img(QSize(100, 50), QImage::Format_ARGB32);
+    img.fill(Qt::blue);
+    surface->attachBuffer(m_shm->createBuffer(img));
+    surface->damage(QRect(0, 0, 100, 50));
+    surface->commit(KWayland::Client::Surface::CommitFlag::None);
+    m_connection->flush();
+}
+
+void InputStackingOrderTest::testPointerFocusUpdatesOnStackingOrderChange()
+{
+    // this test creates two windows which overlap
+    // the pointer is in the overlapping area which means the top most window has focus
+    // as soon as the top most window gets lowered the window should lose focus and the
+    // other window should gain focus without a mouse event in between
+    using namespace KWayland::Client;
+    // create pointer and signal spy for enter and leave signals
+    auto pointer = m_seat->createPointer(m_seat);
+    QVERIFY(pointer);
+    QVERIFY(pointer->isValid());
+    QSignalSpy enteredSpy(pointer, &Pointer::entered);
+    QVERIFY(enteredSpy.isValid());
+    QSignalSpy leftSpy(pointer, &Pointer::left);
+    QVERIFY(leftSpy.isValid());
+
+    // now create the two windows and make them overlap
+    QSignalSpy clientAddedSpy(waylandServer(), &WaylandServer::shellClientAdded);
+    QVERIFY(clientAddedSpy.isValid());
+    Surface *surface1 = m_compositor->createSurface(m_compositor);
+    QVERIFY(surface1);
+    ShellSurface *shellSurface1 = m_shell->createSurface(surface1, surface1);
+    QVERIFY(shellSurface1);
+    render(surface1);
+    QVERIFY(clientAddedSpy.wait());
+    AbstractClient *window1 = workspace()->activeClient();
+    QVERIFY(window1);
+
+    Surface *surface2 = m_compositor->createSurface(m_compositor);
+    QVERIFY(surface2);
+    ShellSurface *shellSurface2 = m_shell->createSurface(surface2, surface2);
+    QVERIFY(shellSurface2);
+    render(surface2);
+    QVERIFY(clientAddedSpy.wait());
+
+    AbstractClient *window2 = workspace()->activeClient();
+    QVERIFY(window2);
+    QVERIFY(window1 != window2);
+
+    // now make windows overlap
+    window2->move(window1->pos());
+    QCOMPARE(window1->geometry(), window2->geometry());
+
+    // enter
+    waylandServer()->backend()->pointerMotion(QPointF(25, 25), 1);
+    QVERIFY(enteredSpy.wait());
+    QCOMPARE(enteredSpy.count(), 1);
+    // window 2 should have focus
+    QCOMPARE(pointer->enteredSurface(), surface2);
+    // also on the server
+    QCOMPARE(waylandServer()->seat()->focusedPointerSurface(), window2->surface());
+
+    // raise window 1 above window 2
+    QVERIFY(leftSpy.isEmpty());
+    workspace()->raiseClient(window1);
+    // should send leave to window2
+    QVERIFY(leftSpy.wait());
+    QCOMPARE(leftSpy.count(), 1);
+    // and an enter to window1
+    QCOMPARE(enteredSpy.count(), 2);
+    QCOMPARE(pointer->enteredSurface(), surface1);
+    QCOMPARE(waylandServer()->seat()->focusedPointerSurface(), window1->surface());
+
+    // let's destroy window1, that should pass focus to window2 again
+    QSignalSpy windowClosedSpy(window1, &Toplevel::windowClosed);
+    QVERIFY(windowClosedSpy.isValid());
+    surface1->deleteLater();
+    QVERIFY(windowClosedSpy.wait());
+    QVERIFY(enteredSpy.wait());
+    QCOMPARE(enteredSpy.count(), 3);
+    QCOMPARE(pointer->enteredSurface(), surface2);
+    QCOMPARE(waylandServer()->seat()->focusedPointerSurface(), window2->surface());
+}
+
+}
+
+WAYLANDTEST_MAIN(KWin::InputStackingOrderTest)
+#include "input_stacking_order.moc"
