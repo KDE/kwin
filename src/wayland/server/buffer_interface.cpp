9a44631c245b2dd5bc40949c8ab3cb1c02a51277
Martin Gräßlin <mgraesslin@kde.org>
2016-06-07 08:27:01 +0200

    [server] Don't assert when destroying a still referenced BufferInterface
    Summary:
    The fact that the buffer is still referenced - that is used - when the
    BufferInterface gets destroyed is an error, but it is not fatal.
    
    Unfortunately KWin/Wayland is still hitting this assert from time to
    time and the assert is not helping to find the cause as a backtrace
    does not show where a reference is still hold.
    
    This change removes the hard assert by a soft warning. The advantage
    of the warning is that the compositor is not killed and that we can
    observe the reason and find a usage pattern which triggers the condition.
    
    With that we will hopefully be able to find the case where the buffer
    is still referenced when being destroyed and fix that.
    
    Reviewers: #plasma_on_wayland
    
    Differential Revision: https://phabricator.kde.org/D1783

--- src/wayland/server/buffer_interface.cpp
+++ src/wayland/server/buffer_interface.cpp
@@ -19,6 +19,7 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 #include "buffer_interface.h"
 #include "display.h"
+#include "logging_p.h"
 #include "surface_interface.h"
 // Wayland
 #include <wayland-server.h>
@@ -184,7 +185,9 @@ BufferInterface::BufferInterface(wl_resource *resource, SurfaceInterface *parent
 
 BufferInterface::~BufferInterface()
 {
-    Q_ASSERT(d->refCount == 0);
+    if (d->refCount != 0) {
+        qCWarning(KWAYLAND_SERVER) << "Buffer destroyed while still being referenced, ref count:" << d->refCount;
+    }
 }
 
 void BufferInterface::Private::destroyListenerCallback(wl_listener *listener, void *data)
