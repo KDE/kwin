94e7adc53c1d5ec723684d24bf20eb3aca4874f1
Burkhard Lück <lueck@hube-lueck.de>
2016-06-30 11:25:43 +0200

    killer/ moved to helpers/killer

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 $EXTRACTRC *.kcfg >> rc.cpp
-$XGETTEXT *.h *.cpp killer/*.cpp tabbox/*.cpp scripting/*.cpp -o $podir/kwin.pot
+$XGETTEXT *.h *.cpp helpers/killer/*.cpp tabbox/*.cpp scripting/*.cpp -o $podir/kwin.pot
