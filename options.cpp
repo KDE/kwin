9b30cfef1d212ed2b40510a70d1f92c497f44398
Martin Gräßlin <mgraesslin@kde.org>
2016-08-05 11:22:13 +0200

    Launch Plasma's launcher menu on meta modifier only shortcut by default
    Summary: Invokes org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.activateLauncherMenu.
    
    Reviewers: #plasma, #kwin, hein
    
    Subscribers: kwin
    
    Tags: #kwin
    
    Differential Revision: https://phabricator.kde.org/D2357

--- options.cpp
+++ options.cpp
@@ -844,9 +844,10 @@ void Options::loadConfig()
     if (config.hasKey("Alt")) {
         m_modifierOnlyShortcuts.insert(Qt::AltModifier, config.readEntry("Alt", QStringList()));
     }
-    if (config.hasKey("Meta")) {
-        m_modifierOnlyShortcuts.insert(Qt::MetaModifier, config.readEntry("Meta", QStringList()));
-    }
+    m_modifierOnlyShortcuts.insert(Qt::MetaModifier, config.readEntry("Meta", QStringList{QStringLiteral("org.kde.plasmashell"),
+                                                                                          QStringLiteral("/PlasmaShell"),
+                                                                                          QStringLiteral("org.kde.PlasmaShell"),
+                                                                                          QStringLiteral("activateLauncherMenu")}));
 }
 
 void Options::syncFromKcfgc()
