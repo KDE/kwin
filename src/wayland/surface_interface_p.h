cd29376ad714e829cedb257307a3eb3f3d14dfc2
Martin Gräßlin <mgraesslin@kde.org>
2016-09-13 08:46:39 +0200

    Try to fix build: add missing includes
    Sorry about that, it compiled on my system.

--- src/wayland/surface_interface_p.h
+++ src/wayland/surface_interface_p.h
@@ -22,6 +22,8 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "surface_interface.h"
 #include "resource_p.h"
+// Qt
+#include <QVector>
 // Wayland
 #include <wayland-server.h>
 
