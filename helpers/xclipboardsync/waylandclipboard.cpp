7b0676ebaf30f8eeb31bb82368071d9ca4df9439
Tobias C. Berner <tcberner@gmail.com>
2016-09-16 10:04:43 +0200

    Include <errno.h> in waylandclipboard.cpp
    Summary:
    `errno.h` needs to be includes, as `errno`, `EAGAIN` and `EWOULDBLOCK` are used.
    
    Without:
    > --- helpers/xclipboardsync/CMakeFiles/org_kde_kwin_xclipboard_syncer.dir/all ---
    > /wrkdirs/usr/ports/x11-wm/plasma5-kwin/work/kwin-5.7.95/helpers/xclipboardsync/waylandclipboard.cpp:110:25: error: use of undeclared identifier 'errno'
    >         if (n == -1 && (errno == EAGAIN || errno == EWOULDBLOCK) && ++retryCount < 1000) {
    >                         ^
    > /wrkdirs/usr/ports/x11-wm/plasma5-kwin/work/kwin-5.7.95/helpers/xclipboardsync/waylandclipboard.cpp:110:34: error: use of undeclared identifier 'EAGAIN'
    >         if (n == -1 && (errno == EAGAIN || errno == EWOULDBLOCK) && ++retryCount < 1000) {
    >                                  ^
    > /wrkdirs/usr/ports/x11-wm/plasma5-kwin/work/kwin-5.7.95/helpers/xclipboardsync/waylandclipboard.cpp:110:44: error: use of undeclared identifier 'errno'
    >        if (n == -1 && (errno == EAGAIN || errno == EWOULDBLOCK) && ++retryCount < 1000) {
    >                                           ^
    > /wrkdirs/usr/ports/x11-wm/plasma5-kwin/work/kwin-5.7.95/helpers/xclipboardsync/waylandclipboard.cpp:110:53: error: use of undeclared identifier 'EWOULDBLOCK'
    >        if (n == -1 && (errno == EAGAIN || errno == EWOULDBLOCK) && ++retryCount < 1000) {
    >                                                    ^
    > 4 errors generated.
    > *** [helpers/xclipboardsync/CMakeFiles/org_kde_kwin_xclipboard_syncer.dir/waylandclipboard.cpp.o] Error code 1
    
    Reviewers: graesslin
    
    Reviewed By: graesslin
    
    Subscribers: kwin
    
    Tags: #kwin
    
    Differential Revision: https://phabricator.kde.org/D2794

--- helpers/xclipboardsync/waylandclipboard.cpp
+++ helpers/xclipboardsync/waylandclipboard.cpp
@@ -35,6 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QMimeType>
 #include <qplatformdefs.h>
 
+#include <errno.h>
 #include <unistd.h>
 
 using namespace KWayland::Client;
