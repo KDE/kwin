a9ac70f00719d6887611e7a3c74d46cf5984e595
Martin Gräßlin <mgraesslin@kde.org>
2016-12-30 19:32:47 +0100

    It makes sense to actually also build the new input_event_spy.cpp
    Sometimes C++ surprises me. How could that compile without being in
    the build set?

--- input_event_spy.cpp
+++ input_event_spy.cpp
@@ -20,6 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "input_event_spy.h"
 #include "input.h"
 
+#include <QPointF>
+#include <QSizeF>
+
 namespace KWin
 {
 
--- input_event_spy.h
+++ input_event_spy.h
@@ -23,6 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <QtGlobal>
 
+class QPointF;
+class QSizeF;
+
 namespace KWin
 {
 class KeyEvent;
