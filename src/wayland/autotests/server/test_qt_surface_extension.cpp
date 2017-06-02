2da0a9428d8c65a227421976423ab26f97a07992
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-06-03 00:16:59 +0200

    Fix for TestQtSurfaceExtension in path with spaces
    Use the QProcess::start() variant with explicit (empty, in these cases)
    arguments, so the program strings are not parsed as shell commands,
    thus preserving paths with spaces as such.

--- src/wayland/autotests/server/test_qt_surface_extension.cpp
+++ src/wayland/autotests/server/test_qt_surface_extension.cpp
@@ -79,7 +79,7 @@ void TestQtSurfaceExtension::testCloseWindow()
     QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
     env.insert(QStringLiteral("WAYLAND_DISPLAY"), s_socketName);
     process.setProcessEnvironment(env);
-    process.start(binary);
+    process.start(binary, QStringList());
     QVERIFY(surfaceExtensionSpy.wait());
     QCOMPARE(surfaceExtensionSpy.count(), 1);
     auto *extension = surfaceExtensionSpy.first().first().value<QtExtendedSurfaceInterface*>();
