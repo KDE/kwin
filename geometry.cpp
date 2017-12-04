KDE/kwin

commit 9934f5b57537feae54afd0c4366c90253638ada2

Properly implement maximize of ShellClient
Summary:
This brings some more checks from Client to ShellClient. Thus the
states are better adjusted.

Unfortunately the X11 implementation is also slightly adjusted, so could
create regressions in worst case.

BUG: 368393

Reviewers: #kwin, #plasma_on_wayland

Subscribers: plasma-devel, kwin

Tags: #plasma_on_wayland, #kwin

Differential Revision: https://phabricator.kde.org/D3507

geometry.cpp
@@ -3299,6 +3299,7 @@ void AbstractClient::setQuickTileMode(QuickTileMode mode, bool keyboard)
             setMaximize(false, false);
         } else {
             QRect prev_geom_restore = geometryRestore(); // setMaximize() would set moveResizeGeom as geom_restore
+            m_quickTileMode = QuickTileMaximize;
             setMaximize(true, true);
             QRect clientArea = workspace()->clientArea(MaximizeArea, this);
             if (geometry().top() != clientArea.top()) {
@@ -3306,7 +3307,6 @@ void AbstractClient::setQuickTileMode(QuickTileMode mode, bool keyboard)
                 r.moveTop(clientArea.top());
                 setGeometry(r);
             }
-            m_quickTileMode = QuickTileMaximize;
             setGeometryRestore(prev_geom_restore);
         }
         emit quickTileModeChanged();
@@ -3326,8 +3326,6 @@ void AbstractClient::setQuickTileMode(QuickTileMode mode, bool keyboard)
 
         TabSynchronizer syncer(this, TabGroup::QuickTile|TabGroup::Geometry|TabGroup::Maximized);
 
-        setMaximize(false, false);
-
         if (mode != QuickTileNone) {
             m_quickTileMode = mode;
             // decorations may turn off some borders when tiled
@@ -3337,6 +3335,9 @@ void AbstractClient::setQuickTileMode(QuickTileMode mode, bool keyboard)
         }
         // Store the mode change
         m_quickTileMode = mode;
+
+        setMaximize(false, false);
+
         emit quickTileModeChanged();
 
         return;

autotests/integration/quick_tiling_test.cpp
@@ -178,7 +178,6 @@ void QuickTilingTest::testQuickTiling()
     c->sendToScreen(1);
     QCOMPARE(c->screen(), 1);
     // quick tile should not be changed
-    QEXPECT_FAIL("maximize", "Maximize loses the state", Continue);
     QCOMPARE(c->quickTileMode(), mode);
     QTEST(c->geometry(), "secondScreen");
 }

shell_client.cpp
@@ -658,6 +658,15 @@ void ShellClient::changeMaximize(bool horizontal, bool vertical, bool adjust)
     if (changeMaximizeRecursion) {
         return;
     }
+
+    if (!isResizable()) {
+        return;
+    }
+
+    const QRect clientArea = isElectricBorderMaximizing() ?
+        workspace()->clientArea(MaximizeArea, Cursor::pos(), desktop()) :
+        workspace()->clientArea(MaximizeArea, this);
+
     MaximizeMode oldMode = m_maximizeMode;
     StackingUpdatesBlocker blocker(workspace());
     RequestGeometryBlocker geometryBlocker(this);
@@ -686,12 +695,45 @@ void ShellClient::changeMaximize(bool horizontal, bool vertical, bool adjust)
         changeMaximizeRecursion = false;
     }
 
+    // TODO: borderless maximized windows
+
+    // Conditional quick tiling exit points
+    const auto oldQuickTileMode = quickTileMode();
+    if (quickTileMode() != QuickTileNone) {
+        if (oldMode == MaximizeFull &&
+                !clientArea.contains(m_geomMaximizeRestore.center())) {
+            // Not restoring on the same screen
+            // TODO: The following doesn't work for some reason
+            //quick_tile_mode = QuickTileNone; // And exit quick tile mode manually
+        } else if ((oldMode == MaximizeVertical && m_maximizeMode == MaximizeRestore) ||
+                  (oldMode == MaximizeFull && m_maximizeMode == MaximizeHorizontal)) {
+            // Modifying geometry of a tiled window
+            updateQuickTileMode(QuickTileNone); // Exit quick tile mode without restoring geometry
+        }
+    }
+
     // TODO: check rules
     if (m_maximizeMode == MaximizeFull) {
         m_geomMaximizeRestore = geometry();
+        // TODO: Client has more checks
+        if (options->electricBorderMaximize()) {
+            updateQuickTileMode(QuickTileMaximize);
+        } else {
+            updateQuickTileMode(QuickTileNone);
+        }
+        if (quickTileMode() != oldQuickTileMode) {
+            emit quickTileModeChanged();
+        }
         requestGeometry(workspace()->clientArea(MaximizeArea, this));
         workspace()->raiseClient(this);
     } else {
+        if (m_maximizeMode == MaximizeRestore) {
+            updateQuickTileMode(QuickTileNone);
+        }
+        if (quickTileMode() != oldQuickTileMode) {
+            emit quickTileModeChanged();
+        }
+
         if (m_geomMaximizeRestore.isValid()) {
             requestGeometry(m_geomMaximizeRestore);
         } else {

commit 8a2796d280dd7e891724cb6e6d873a57e2ae8c52

Fix regression from KWin 5.9 regarding quick tiling
Summary:
The regression got introduced with 9934f5b57537feae54afd0c4366c90253638ada2.
The order when setMaximize(false, false) was called changed in regard to
when the quick tiling mode was adjusted. But just changing the ordering
back was no solution as that would cause regressions in other areas
(unit tests fail).

This change builds up on the support for geometry update blocker on
Wayland to be able to better support this situation without causing
further regressions.

Also this change rethinks the code area. There is an idea behind
temporarily setting the quick tile mode to none and that is even
documented in a comment: it should not confuse maximize. So let's do
exactly that: call the maximize in the block where the quick tile
mode is temporarily wrong. As that is only one branch the else branch
performs the same steps.

BUG: 376104
FIXED-IN: 5.12.0

Test Plan: Confirmation in bug report that patch fixes issue

Reviewers: #kwin, #plasma

Subscribers: plasma-devel, kwin

Tags: #kwin

Differential Revision: https://phabricator.kde.org/D9178

geometry.cpp
@@ -3360,16 +3360,19 @@ void AbstractClient::setQuickTileMode(QuickTileMode mode, bool keyboard)
         TabSynchronizer syncer(this, TabGroup::QuickTile|TabGroup::Geometry|TabGroup::Maximized);
 
         if (mode != QuickTileMode(QuickTileFlag::None)) {
-            m_quickTileMode = mode;
             // decorations may turn off some borders when tiled
             const ForceGeometry_t geom_mode = isDecorated() ? ForceGeometrySet : NormalGeometrySet;
             m_quickTileMode = int(QuickTileFlag::None); // Temporary, so the maximize code doesn't get all confused
-            setGeometry(electricBorderMaximizeGeometry(keyboard ? geometry().center() : Cursor::pos(), desktop()), geom_mode);
-        }
-        // Store the mode change
-        m_quickTileMode = mode;
 
-        setMaximize(false, false);
+            setMaximize(false, false);
+
+            setGeometry(electricBorderMaximizeGeometry(keyboard ? geometry().center() : Cursor::pos(), desktop()), geom_mode);
+            // Store the mode change
+            m_quickTileMode = mode;
+        } else {
+            m_quickTileMode = mode;
+            setMaximize(false, false);
+        }
 
         emit quickTileModeChanged();
 

autotests/integration/quick_tiling_test.cpp
@@ -584,23 +584,7 @@ void QuickTilingTest::testX11QuickTilingAfterVertMaximize()
     QVERIFY(quickTileChangedSpy.isValid());
     QFETCH(QuickTileMode, mode);
     client->setQuickTileMode(mode, true);
-    QEXPECT_FAIL("left", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("right", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("top", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("bottom", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("top left", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("top right", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("bottom left", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("bottom right", "Quick tiling not working", Continue);
     QCOMPARE(client->quickTileMode(), mode);
-    QEXPECT_FAIL("left", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("right", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("top", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("bottom", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("top left", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("top right", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("bottom left", "Quick tiling not working", Continue);
-    QEXPECT_FAIL("bottom right", "Quick tiling not working", Continue);
     QTEST(client->geometry(), "expectedGeometry");
     QEXPECT_FAIL("", "We get two changed events", Continue);
     QCOMPARE(quickTileChangedSpy.count(), 1);