eb47c08204deca0814fd84dcf53b45e611ac4442
Yuri Chornoivan <yurchor@ukr.net>
2018-05-20 08:14:50 +0300

    Fix message extraction

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 $EXTRACTRC *.kcfg >> rc.cpp
-$XGETTEXT *.h *.cpp helpers/killer/*.cpp tabbox/*.cpp scripting/*.cpp -o $podir/kwin.pot
+$XGETTEXT *.h *.cpp helpers/killer/*.cpp plugins/scenes/opengl/*.cpp tabbox/*.cpp scripting/*.cpp -o $podir/kwin.pot
--- scripting/Messages.sh
+++ scripting/Messages.sh
@@ -1,4 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.h -o -name \*.cpp` -o $podir/kwin_scripting.pot
-rm -f rc.cpp
