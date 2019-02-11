0cf7cca2ab6a26db35251706124b6a903a32d2b6
Victor Ruzhykh <victorr2007@yandex.ru>
2019-02-11 20:12:41 +0200

    Make the new Window Decoration KCM translatable
    Differential Revision: https://phabricator.kde.org/D18931

--- kcmkwin/kwindecoration/Messages.sh
+++ kcmkwin/kwindecoration/Messages.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/kcmkwindecoration.pot
+$XGETTEXT `find . -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/kcm_kwindecoration.pot
 rm -f rc.cpp
