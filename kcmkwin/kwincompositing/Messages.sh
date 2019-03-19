d51070223b0a5589361e9f06fe11234caa336b58
Albert Astals Cid <aacid@kde.org>
2019-03-19 22:03:43 +0100

    There's no qml folder here anymore
    CCMAIL: vladzzag@gmail.com

--- kcmkwin/kwincompositing/Messages.sh
+++ kcmkwin/kwincompositing/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT *.h *.cpp qml/*.qml -o $podir/kcmkwincompositing.pot
+$XGETTEXT *.h *.cpp -o $podir/kcmkwincompositing.pot
