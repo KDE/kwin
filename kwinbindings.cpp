c795f1389b63803f71aafe33a956ed50c14e3bb4
Nate Graham <nate@kde.org>
2019-03-26 11:49:31 -0600

    Use Meta+D to Show Desktop by default
    Summary: Bind the keyboard shortcut {key Meta D} to {nav Show Desktop}.
    
    Test Plan:
    Apply patch, compile KWin, do `kwin --replace`, reset KWin shortcuts to default values if you've previously changed the Show Desktop's keyboard shortcut
    
    {key Meta D} now shows and hides the desktop.
    
    Reviewers: #kwin, zzag
    
    Reviewed By: #kwin, zzag
    
    Subscribers: filipf, abetts, zzag, kwin
    
    Tags: #kwin
    
    Differential Revision: https://phabricator.kde.org/D20066

--- kwinbindings.cpp
+++ kwinbindings.cpp
@@ -149,7 +149,7 @@ for (int i = 0; i < 8; ++i) {
 }
 DEF(I18N_NOOP("Window to Next Screen"),            0, slotWindowToNextScreen);
 DEF(I18N_NOOP("Window to Previous Screen"),        0, slotWindowToPrevScreen);
-DEF(I18N_NOOP("Show Desktop"),                     0, slotToggleShowDesktop);
+DEF(I18N_NOOP("Show Desktop"),                     Qt::META + Qt::Key_D, slotToggleShowDesktop);
 
 for (int i = 0; i < 8; ++i) {
     DEF3(I18N_NOOP("Switch to Screen %1"),         0, slotSwitchToScreen, i);
