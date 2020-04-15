1a359d5e93947bd06276405c7b0ee0bb979cd1b6
David Edmundson <kde@davidedmundson.co.uk>
2020-04-16 00:58:05 +0100

    [wayland] Fix teardown order
    Summary:
    Valgrind flags an error on teardown.
    
    EventQueue has a pointer to ConnectionThread internally
    Registry has a pointer to the EventQueue internally
    
    teardown order needs to be
    
    Registry
    EventQueue
    Connection
    
    registry was explicitly deleted before connectionthread already, we just
    need to put event queue in the right place.
    
    Test Plan: Ran kwin_wayland nested in valgrind
    
    Reviewers: #kwin, cblack, apol
    
    Reviewed By: cblack, apol
    
    Subscribers: zzag, apol, kwin
    
    Tags: #kwin
    
    Differential Revision: https://phabricator.kde.org/D28668

--- wayland_server.cpp
+++ wayland_server.cpp
@@ -605,7 +605,7 @@ void WaylandServer::createInternalConnection()
     connect(m_internalConnection.client, &ConnectionThread::connected, this,
         [this] {
             Registry *registry = new Registry(this);
-            EventQueue *eventQueue = new EventQueue(this);
+            EventQueue *eventQueue = new EventQueue(registry);
             eventQueue->setup(m_internalConnection.client);
             registry->setEventQueue(eventQueue);
             registry->create(m_internalConnection.client);
