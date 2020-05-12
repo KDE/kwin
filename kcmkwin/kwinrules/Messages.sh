65f611f7008c3f94febb2f202257a6bb50e2c38c
Kai Uwe Broulik <kde@privat.broulik.de>
2020-05-12 10:58:11 +0200

    [kcmkwin/kwinrules] Change translation domain
    Make it consistently kcm_kwinrules to match the KAboutData
    
    Differential Revision: https://phabricator.kde.org/D29674

--- kcmkwin/kwinrules/Messages.sh
+++ kcmkwin/kwinrules/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/kcmkwinrules.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/kcm_kwinrules.pot
