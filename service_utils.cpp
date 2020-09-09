7b99a28050ae8f962dd9b25745ae863b8d6ccb01
Méven Car <meven.car@enioka.com>
2020-09-09 18:33:22 +0200

    Fix email address

--- service_utils.cpp
+++ service_utils.cpp
@@ -2,7 +2,7 @@
     KWin - the KDE window manager
     This file is part of the KDE project.
 
-    SPDX-FileCopyrightText: 2020 Méven Car <meven.car@enika.com>
+    SPDX-FileCopyrightText: 2020 Méven Car <meven.car@enioka.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
