bfb257c9ed86fd830065178088d133b7a4c68409
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2020-09-09 16:22:19 +0300

    kcmkwin/tabbox: Port away from deprecated KTitleWidget::setPixmap()
    KTitleWidget::setPixmap(MessageType, ImageAligment) has been deprecated
    in favor of KTitleWidget::setIcon() method.

--- kcmkwin/kwintabbox/main.cpp
+++ kcmkwin/kwintabbox/main.cpp
@@ -77,7 +77,7 @@ KWinTabBoxConfig::KWinTabBoxConfig(QWidget* parent, const QVariantList& args)
     KTitleWidget* infoLabel = new KTitleWidget(tabWidget);
     infoLabel->setText(i18n("Focus policy settings limit the functionality of navigating through windows."),
                        KTitleWidget::InfoMessage);
-    infoLabel->setPixmap(KTitleWidget::InfoMessage, KTitleWidget::ImageLeft);
+    infoLabel->setIcon(KTitleWidget::InfoMessage, KTitleWidget::ImageLeft);
     layout->addWidget(infoLabel,0);
     layout->addWidget(tabWidget,1);
     layout->addLayout(buttonBar);
