KDE/kwin

commit 9cbac6fe3a1e5973bcf15d3d9006c0162a2134ed

[kwinrules] Show defaults indicator in System Settings
The default is having no rules, so the indicator will show
whenever there is one or more rules in the list.

kcmkwin/kwinrules/rulebookmodel.cpp
@@ -6,13 +6,15 @@
 
 #include "rulebookmodel.h"
 
+#include "rulebookdata.h"
+#include "rulebooksettings.h"
 
 namespace KWin
 {
 
 RuleBookModel::RuleBookModel(QObject *parent)
     : QAbstractListModel(parent)
-    , m_ruleBook(new RuleBookSettings(this))
+    , m_ruleBookData(new RuleBookData(this))
 {
 }
 
@@ -70,7 +72,7 @@ bool RuleBookModel::insertRows(int row, int count, const QModelIndex &parent)
         m_rules.insert(row + i, newRule);
     }
 
-    m_ruleBook->setCount(m_rules.count());
+    m_ruleBookData->settings()->setCount(m_rules.count());
 
     endInsertRows();
     return true;
@@ -87,7 +89,7 @@ bool RuleBookModel::removeRows(int row, int count, const QModelIndex &parent)
         delete m_rules.at(row + i);
     }
     m_rules.remove(row, count);
-    m_ruleBook->setCount(m_rules.count());
+    m_ruleBookData->settings()->setCount(m_rules.count());
 
     endRemoveRows();
     return true;
@@ -157,17 +159,17 @@ void RuleBookModel::load()
 {
     beginResetModel();
 
-    m_ruleBook->load();
+    m_ruleBookData->settings()->load();
     qDeleteAll(m_rules);
-    m_rules = m_ruleBook->rules();
+    m_rules = m_ruleBookData->settings()->rules();
 
     endResetModel();
 }
 
 void RuleBookModel::save()
 {
-    m_ruleBook->setRules(m_rules);
-    m_ruleBook->save();
+    m_ruleBookData->settings()->setRules(m_rules);
+    m_ruleBookData->settings()->save();
 }
 
 } // namespace

kcmkwin/kwinrules/rulebookmodel.h
@@ -6,7 +6,6 @@
 
 #pragma once
 
-#include "rulebooksettings.h"
 #include <rules.h>
 
 #include <QAbstractListModel>
@@ -14,6 +13,8 @@
 
 namespace KWin
 {
+
+class RuleBookData;
 
 class RuleBookModel : public QAbstractListModel
 {
@@ -43,7 +44,7 @@ public:
     void save();
 
 private:
-    RuleBookSettings *m_ruleBook;
+    RuleBookData *m_ruleBookData;
     QVector<Rules *> m_rules;
 };
 

kcmkwin/kwinrules/CMakeLists.txt
@@ -21,6 +21,15 @@ set(kwinrules_SRCS
 kconfig_add_kcfg_files(kwinrules_SRCS ../../rulesettings.kcfgc)
 kconfig_add_kcfg_files(kwinrules_SRCS ../../rulebooksettingsbase.kcfgc)
 
+kcmutils_generate_module_data(
+    kwinrules_SRCS
+    MODULE_DATA_HEADER rulebookdata.h
+    MODULE_DATA_CLASS_NAME RuleBookData
+    SETTINGS_HEADERS rulebooksettings.h
+    SETTINGS_CLASSES RuleBookSettings
+    NAMESPACE KWin
+)
+
 add_library(KWinRulesObjects STATIC ${kwinrules_SRCS})
 
 set(kwin_kcm_rules_XCB_LIBS
@@ -34,6 +43,7 @@ set(kcm_libs
     Qt5::QuickWidgets
 
     KF5::I18n
+    KF5::KCMUtils
     KF5::QuickAddons
     KF5::WindowSystem
     KF5::XmlGui

kcmkwin/kwinrules/kcmrules.cpp
@@ -7,6 +7,8 @@
 
 #include "kcmrules.h"
 
+#include "rulebookdata.h"
+
 #include <QDBusConnection>
 #include <QDBusMessage>
 
@@ -233,7 +235,9 @@ void KCMKWinRules::importFromFile(const QUrl &path)
     updateNeedsSave();
 }
 
-K_PLUGIN_CLASS_WITH_JSON(KCMKWinRules, "kcm_kwinrules.json");
+K_PLUGIN_FACTORY_WITH_JSON(KCMKWinRulesFactory, "kcm_kwinrules.json",
+                           registerPlugin<KWin::KCMKWinRules>();
+                           registerPlugin<KWin::RuleBookData>(););
 
 } // namespace
 

commit 3e15f64055d952e5ab773bff197ffcde364dd88b

Fix CI error caused by 9cbac6fe
Include header "rulebookdata.h" instead of class forwarding.
The header is automatically generated by kcmutils_generate_module_data
and includes a namespace

kcmkwin/kwinrules/rulebookmodel.cpp
@@ -6,8 +6,8 @@
 
 #include "rulebookmodel.h"
 
-#include "rulebookdata.h"
 #include "rulebooksettings.h"
+
 
 namespace KWin
 {

kcmkwin/kwinrules/rulebookmodel.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include "rulebookdata.h"
 #include <rules.h>
 
 #include <QAbstractListModel>
@@ -13,8 +14,6 @@
 
 namespace KWin
 {
-
-class RuleBookData;
 
 class RuleBookModel : public QAbstractListModel
 {