arkroyal01/volcanic

[libkwineffects] Add windowId to EffectWindow
My use case is integrating with a system that tracks window properties based on the windowId. This allows the effect to do window-specific things.

effects.cpp
@@ -1833,6 +1833,7 @@ TOPLEVEL_HELPER(KWaylandServer::SurfaceInterface *, surface, surface)
 TOPLEVEL_HELPER(bool, isPopupWindow, isPopupWindow)
 TOPLEVEL_HELPER(bool, isOutline, isOutline)
 TOPLEVEL_HELPER(pid_t, pid, pid)
+TOPLEVEL_HELPER(qlonglong, windowId, windowId)
 
 #undef TOPLEVEL_HELPER
 

KDE/kwin

effects.cpp
@@ -1828,6 +1828,7 @@ TOPLEVEL_HELPER(KWaylandServer::SurfaceInterface *, surface, surface)
 TOPLEVEL_HELPER(bool, isPopupWindow, isPopupWindow)
 TOPLEVEL_HELPER(bool, isOutline, isOutline)
 TOPLEVEL_HELPER(pid_t, pid, pid)
+TOPLEVEL_HELPER(qlonglong, windowId, window)
 
 #undef TOPLEVEL_HELPER
 