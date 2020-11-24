459aa66d8e07417a6a54bba2dda7b4fd188bd762
Xaver Hugl <xaver.hugl@gmail.com>
2020-11-24 18:17:48 +0000

    Set setMoveResize(true) after stopping fullscreen and quick tiling
    BUG: 427848

--- xdgshellclient.cpp
+++ xdgshellclient.cpp
@@ -1552,7 +1552,9 @@ void XdgToplevelClient::setFullScreen(bool set, bool user)
     }
     StackingUpdatesBlocker blocker1(workspace());
     GeometryUpdatesBlocker blocker2(this);
-    dontMoveResize();
+    if (set) {
+        dontMoveResize();
+    }
 
     workspace()->updateClientLayer(this);   // active fullscreens get different layer
     updateDecoration(false, false);
@@ -1614,7 +1616,9 @@ void XdgToplevelClient::changeMaximize(bool horizontal, bool vertical, bool adju
     }
 
     StackingUpdatesBlocker blocker(workspace());
-    dontMoveResize();
+    if (m_requestedMaximizeMode != MaximizeRestore) {
+        dontMoveResize();
+    }
 
     // call into decoration update borders
     if (isDecorated() && decoration()->client() && !(options->borderlessMaximizedWindows() && m_requestedMaximizeMode == KWin::MaximizeFull)) {
