b42e4d1884d0c15d13f3c7207b9aafb1c5b6b4ce
Adriaan de Groot <groot@kde.org>
2020-12-16 11:19:02 +0100

    Missing includes (for strdup() and strlen())

--- helpers/wayland_wrapper/kwin_wrapper.c
+++ helpers/wayland_wrapper/kwin_wrapper.c
@@ -24,6 +24,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/un.h>
 #include <sys/wait.h>
 #include <unistd.h>
--- helpers/wayland_wrapper/wl-socket.c
+++ helpers/wayland_wrapper/wl-socket.c
@@ -12,6 +12,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/file.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
