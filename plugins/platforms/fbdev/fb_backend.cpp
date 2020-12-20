KDE/kwin

commit 79ca5ccd2646964483b213e6558d08eafa8b0685

Merge OutputScreens into Screens
Since all platforms have been ported to AbstractOutput, OutputScreens
can be merged into Screens. This simplifies the class hierarchy.

plugins/platforms/fbdev/fb_backend.cpp
@@ -12,7 +12,7 @@
 #include "logging.h"
 #include "logind.h"
 #include "scene_qpainter_fb_backend.h"
-#include "outputscreens.h"
+#include "screens.h"
 #include "virtual_terminal.h"
 #include "udev.h"
 // system
@@ -58,7 +58,7 @@ FramebufferBackend::~FramebufferBackend()
 
 Screens *FramebufferBackend::createScreens(QObject *parent)
 {
-    return new OutputScreens(this, parent);
+    return Screens::create(parent);
 }
 
 QPainterBackend *FramebufferBackend::createQPainterBackend()

plugins/platforms/wayland/wayland_backend.cpp
@@ -25,7 +25,7 @@
 #include "cursor.h"
 #include "input.h"
 #include "main.h"
-#include "outputscreens.h"
+#include "screens.h"
 #include "pointer_input.h"
 #include "screens.h"
 #include "wayland_server.h"
@@ -729,7 +729,7 @@ void WaylandBackend::createOutputs()
 
 Screens *WaylandBackend::createScreens(QObject *parent)
 {
-    return new OutputScreens(this, parent);
+    return Screens::create(parent);
 }
 
 OpenGLBackend *WaylandBackend::createOpenGLBackend()

plugins/platforms/x11/windowed/x11windowed_backend.cpp
@@ -13,7 +13,7 @@
 #include "wayland_server.h"
 #include "xcbutils.h"
 #include "egl_x11_backend.h"
-#include "outputscreens.h"
+#include "screens.h"
 #include <kwinxrenderutils.h>
 #include <cursor.h>
 #include <pointer_input.h>
@@ -501,7 +501,7 @@ xcb_window_t X11WindowedBackend::rootWindow() const
 
 Screens *X11WindowedBackend::createScreens(QObject *parent)
 {
-    return new OutputScreens(this, parent);
+    return Screens::create(parent);
 }
 
 OpenGLBackend *X11WindowedBackend::createOpenGLBackend()

screens.h
@@ -23,6 +23,7 @@
 namespace KWin
 {
 class AbstractClient;
+class AbstractOutput;
 class Platform;
 
 class KWIN_EXPORT Screens : public QObject
@@ -53,7 +54,7 @@ public:
     void setCurrent(const AbstractClient *c);
     bool isCurrentFollowsMouse() const;
     void setCurrentFollowsMouse(bool follows);
-    virtual QRect geometry(int screen) const = 0;
+    virtual QRect geometry(int screen) const;
     /**
      * The bounding geometry of all screens combined. Overlapping areas
      * are not counted multiple times.
@@ -74,7 +75,7 @@ public:
      * To get the size of all screens combined use size().
      * @see size()
      */
-    virtual QSize size(int screen) const = 0;
+    virtual QSize size(int screen) const;
 
     /**
      * The highest scale() of all connected screens
@@ -96,7 +97,7 @@ public:
      * @see sizeChanged()
      */
     QSize size() const;
-    virtual int number(const QPoint &pos) const = 0;
+    virtual int number(const QPoint &pos) const;
 
     inline bool isChanging() { return m_changedTimer->isActive(); }
 
@@ -163,7 +164,7 @@ Q_SIGNALS:
 protected Q_SLOTS:
     void setCount(int count);
     void startChangedTimer();
-    virtual void updateCount() = 0;
+    virtual void updateCount();
 
 protected:
     /**
@@ -177,6 +178,8 @@ private Q_SLOTS:
     void updateSize();
 
 private:
+    AbstractOutput *findOutput(int screenId) const;
+
     int m_count;
     int m_current;
     bool m_currentFollowsMouse;

CMakeLists.txt
@@ -504,7 +504,6 @@ set(kwin_SRCS
     options.cpp
     osd.cpp
     outline.cpp
-    outputscreens.cpp
     overlaywindow.cpp
     placement.cpp
     platform.cpp

autotests/mock_screens.cpp
@@ -43,6 +43,11 @@ QSize MockScreens::size(int screen) const
     return geometry(screen).size();
 }
 
+QSizeF MockScreens::physicalSize(int screen) const
+{
+    return QSizeF(size(screen)) / 3.8;
+}
+
 int MockScreens::number(const QPoint &pos) const
 {
     int bestScreen = 0;

autotests/mock_screens.h
@@ -25,6 +25,7 @@ public:
     QString name(int screen) const override;
     float refreshRate(int screen) const override;
     QSize size(int screen) const override;
+    QSizeF physicalSize(int screen) const override;
     void init() override;
 
     void setGeometries(const QList<QRect> &geometries);

outputscreens.cpp
@@ -1,119 +1,0 @@
-/*
-    KWin - the KDE window manager
-    This file is part of the KDE project.
-
-    SPDX-FileCopyrightText: 2018 Roman Gilg <subdiff@gmail.com>
-
-    SPDX-License-Identifier: GPL-2.0-or-later
-*/
-#include "outputscreens.h"
-#include "platform.h"
-#include "abstract_output.h"
-
-namespace KWin
-{
-
-OutputScreens::OutputScreens(Platform *platform, QObject *parent)
-    : Screens(parent),
-      m_platform(platform)
-{
-}
-
-OutputScreens::~OutputScreens() = default;
-
-void OutputScreens::init()
-{
-    updateCount();
-    KWin::Screens::init();
-    emit changed();
-}
-
-QString OutputScreens::name(int screen) const
-{
-    if (AbstractOutput *output = findOutput(screen)) {
-        return output->name();
-    }
-    return QString();
-}
-
-bool OutputScreens::isInternal(int screen) const
-{
-    if (AbstractOutput *output = findOutput(screen)) {
-        return output->isInternal();
-    }
-    return false;
-}
-
-QRect OutputScreens::geometry(int screen) const
-{
-    if (AbstractOutput *output = findOutput(screen)) {
-        return output->geometry();
-    }
-    return QRect();
-}
-
-QSize OutputScreens::size(int screen) const
-{
-    if (AbstractOutput *output = findOutput(screen)) {
-        return output->geometry().size();
-    }
-    return QSize();
-}
-
-qreal OutputScreens::scale(int screen) const
-{
-    if (AbstractOutput *output = findOutput(screen)) {
-        return output->scale();
-    }
-    return 1.0;
-}
-
-QSizeF OutputScreens::physicalSize(int screen) const
-{
-    if (AbstractOutput *output = findOutput(screen)) {
-        return output->physicalSize();
-    }
-    return QSizeF();
-}
-
-float OutputScreens::refreshRate(int screen) const
-{
-    if (AbstractOutput *output = findOutput(screen)) {
-        return output->refreshRate() / 1000.0;
-    }
-    return 60.0;
-}
-
-void OutputScreens::updateCount()
-{
-    setCount(m_platform->enabledOutputs().size());
-}
-
-int OutputScreens::number(const QPoint &pos) const
-{
-    int bestScreen = 0;
-    int minDistance = INT_MAX;
-    const auto outputs = m_platform->enabledOutputs();
-    for (int i = 0; i < outputs.size(); ++i) {
-        const QRect &geo = outputs[i]->geometry();
-        if (geo.contains(pos)) {
-            return i;
-        }
-        int distance = QPoint(geo.topLeft() - pos).manhattanLength();
-        distance = qMin(distance, QPoint(geo.topRight() - pos).manhattanLength());
-        distance = qMin(distance, QPoint(geo.bottomRight() - pos).manhattanLength());
-        distance = qMin(distance, QPoint(geo.bottomLeft() - pos).manhattanLength());
-        if (distance < minDistance) {
-            minDistance = distance;
-            bestScreen = i;
-        }
-    }
-    return bestScreen;
-}
-
-AbstractOutput *OutputScreens::findOutput(int screen) const
-{
-    return m_platform->findOutput(screen);
-}
-
-} // namespace

commit 8f516777568ea0cce39aedd78509ed01f6e0275c

Avoid infinite loop on screen creation
Screens is a singleton. The factory class calls into the platform to
make the instance.

79ca5ccd introduced a regression that made the platforms call the
factory class again. We want the platforms to call the base constructor.

The port away from the KWIN_SINGLETON macro is to make the Screens
constructor public to platforms.

plugins/platforms/fbdev/fb_backend.cpp
@@ -58,7 +58,7 @@ FramebufferBackend::~FramebufferBackend()
 
 Screens *FramebufferBackend::createScreens(QObject *parent)
 {
-    return Screens::create(parent);
+    return new Screens(parent);
 }
 
 QPainterBackend *FramebufferBackend::createQPainterBackend()

plugins/platforms/wayland/wayland_backend.cpp
@@ -729,7 +729,7 @@ void WaylandBackend::createOutputs()
 
 Screens *WaylandBackend::createScreens(QObject *parent)
 {
-    return Screens::create(parent);
+    return new Screens(parent);
 }
 
 OpenGLBackend *WaylandBackend::createOpenGLBackend()

plugins/platforms/x11/windowed/x11windowed_backend.cpp
@@ -501,7 +501,7 @@ xcb_window_t X11WindowedBackend::rootWindow() const
 
 Screens *X11WindowedBackend::createScreens(QObject *parent)
 {
-    return Screens::create(parent);
+    return new Screens(parent);
 }
 
 OpenGLBackend *X11WindowedBackend::createOpenGLBackend()

screens.h
@@ -34,6 +34,10 @@ class KWIN_EXPORT Screens : public QObject
     Q_PROPERTY(bool currentFollowsMouse READ isCurrentFollowsMouse WRITE setCurrentFollowsMouse)
 
 public:
+    static Screens *create(QObject *parent = nullptr);\
+    static Screens *self() { return s_self; }\
+
+    Screens(QObject *parent);
     ~Screens() override;
     /**
      * @internal
@@ -188,7 +192,7 @@ private:
     QSize m_boundingSize;
     qreal m_maxScale;
 
-    KWIN_SINGLETON(Screens)
+    static Screens *s_self;
 };
 
 inline