14ef99155312af2aabe40894362c8e336263a5c2
David Edmundson <kde@davidedmundson.co.uk>
2021-01-27 13:18:35 +0000

    Initialise SessionState
    It was used undefined

--- sm.h
+++ sm.h
@@ -47,7 +47,7 @@ public Q_SLOTS: // DBus API
 
 private:
     void setState(SessionState state);
-    SessionState m_sessionState;
+    SessionState m_sessionState = SessionState::Normal;
 };
 
 struct SessionInfo {
