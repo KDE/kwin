be65b8414112967dcca0e3eecf99dd2c2d905a18
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2021-01-28 11:52:30 +0000

    platforms/x11: Move GLXFBConfig selection code in a separate file
    This allows reusing the GLXFBConfig selection code.
    
    
    (cherry picked from commit b338c4cb0ef1035f832e975fd51605191a24e077)

---
+++ plugins/platforms/x11/standalone/glxconvenience.cpp
@@ -0,0 +1,57 @@
+/*
+    SPDX-FileCopyrightText: 2006 Lubos Lunak <l.lunak@kde.org>
+    SPDX-FileCopyrightText: 2012 Martin Gräßlin <mgraesslin@kde.org>
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "glxconvenience.h"
+
+#include <deque>
+
+namespace KWin
+{
+
+GLXFBConfig chooseGlxFbConfig(Display *display, const int attributes[])
+{
+    int configCount = 0;
+    GLXFBConfig *configs = glXChooseFBConfig(display, DefaultScreen(display),
+                                             attributes, &configCount);
+
+    struct FBConfig {
+        GLXFBConfig config;
+        int depth;
+        int stencil;
+    };
+
+    std::deque<FBConfig> candidates;
+
+    for (int i = 0; i < configCount; i++) {
+        int depth, stencil;
+        glXGetFBConfigAttrib(display, configs[i], GLX_DEPTH_SIZE, &depth);
+        glXGetFBConfigAttrib(display, configs[i], GLX_STENCIL_SIZE, &stencil);
+
+        candidates.emplace_back(FBConfig{configs[i], depth, stencil});
+    }
+
+    if (configCount > 0) {
+        XFree(configs);
+    }
+
+    std::stable_sort(candidates.begin(), candidates.end(), [](const FBConfig &left, const FBConfig &right) {
+        if (left.depth < right.depth) {
+            return true;
+        }
+
+        if (left.stencil < right.stencil) {
+            return true;
+        }
+
+        return false;
+    });
+
+    return candidates.empty() ? nullptr : candidates.front().config;
+}
+
+} // namespace KWin
