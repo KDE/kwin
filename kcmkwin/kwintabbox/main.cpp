2b721200fb5c8a0b935a1ba633d69cd1fb992719
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-02-08 17:38:25 +0100

    Port kwintabbox KCM away from KNS3::DownloadDialog

--- kcmkwin/kwintabbox/main.cpp
+++ kcmkwin/kwintabbox/main.cpp
@@ -13,6 +13,7 @@
 
 // Qt
 #include <QtDBus>
+#include <QDialog>
 #include <QDialogButtonBox>
 #include <QHBoxLayout>
 #include <QPushButton>
@@ -30,7 +31,7 @@
 #include <KPluginTrader>
 #include <KTitleWidget>
 #include <KServiceTypeTrader>
-#include <KNewStuff3/KNS3/DownloadDialog>
+#include <KNewStuff3/KNS3/QtQuickDialogWrapper>
 // Plasma
 #include <KPackage/Package>
 #include <KPackage/PackageLoader>
@@ -471,13 +472,9 @@ void KWinTabBoxConfig::configureEffectClicked()
 
 void KWinTabBoxConfig::slotGHNS()
 {
-    QPointer<KNS3::DownloadDialog> downloadDialog = new KNS3::DownloadDialog("kwinswitcher.knsrc", this);
-    if (downloadDialog->exec() == QDialog::Accepted) {
-        if (!downloadDialog->changedEntries().isEmpty()) {
-            initLayoutLists();
-        }
+    if (!KNS3::QtQuickDialogWrapper("kwinswitcher.knsrc").exec().isEmpty()) {
+        initLayoutLists();
     }
-    delete downloadDialog;
 }
 
 } // namespace
