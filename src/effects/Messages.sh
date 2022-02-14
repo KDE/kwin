2fbc821899f6425e2c336958446f39dad45941d9
Kai Uwe Broulik <kde@privat.broulik.de>
2022-02-14 12:06:08 +0100

    effects: Extract messages from QML files

--- src/effects/Messages.sh
+++ src/effects/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/kwin_effects.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/kwin_effects.pot
 rm -f rc.cpp
