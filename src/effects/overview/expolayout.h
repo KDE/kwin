f6d14f04016fc015a8b732cc8318072b59171579
Volker Krause <vkrause@kde.org>
2022-03-10 19:02:36 +0100

    Adapt to QQuickItem::geometryChanged() rename in Qt6

--- src/effects/overview/expolayout.h
+++ src/effects/overview/expolayout.h
@@ -47,7 +47,11 @@ public:
     void setReady();
 
 protected:
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     void geometryChanged(const QRectF &newGeometry, const QRectF &oldGeometry) override;
+#else
+    void geometryChange(const QRectF &newGeometry, const QRectF &oldGeometry) override;
+#endif
     void updatePolish() override;
 
 Q_SIGNALS:
