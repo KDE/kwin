ba43e3d662e32a1e3cfced759a13b7ca1e1f7650
Xaver Hugl <xaver.hugl@gmail.com>
2022-04-28 02:23:18 +0200

    backends/drm: reduce "max bpc" to what is actually used
    This prevents triggering a bug in the BenQ GW2765 monitor, and should
    in theory have no downsides.
    
    BUG: 449906
    FIXED-IN: 5.24.5

--- src/backends/drm/drm_pipeline.cpp
+++ src/backends/drm/drm_pipeline.cpp
@@ -242,7 +242,11 @@ void DrmPipeline::prepareAtomicModeset()
         m_connector->getProp(DrmConnector::PropertyIndex::Underscan_hborder)->setPending(hborder);
     }
     if (const auto bpc = m_connector->getProp(DrmConnector::PropertyIndex::MaxBpc)) {
-        bpc->setPending(bpc->maxValue());
+        uint64_t preferred = 8;
+        if (gpu()->eglBackend() && gpu()->eglBackend()->prefer10bpc()) {
+            preferred = 10;
+        }
+        bpc->setPending(std::min(bpc->maxValue(), preferred));
     }
 
     pending.crtc->setPending(DrmCrtc::PropertyIndex::Active, activePending());
