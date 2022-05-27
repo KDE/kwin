2b7fdc584001fb36b94f5b72d49b7b46ff54b3b8
Nate Graham <nate@kde.org>
2022-05-27 10:18:02 -0600

    Change global shortcuts to use the Meta key by default
    The current shortcuts could conceivably override app shortcuts, since
    they don't use the Meta key. The following changes are made:
    
    Switch keyboard layout:               Ctrl+Alt+K   -> Meta+Alt+K
    Activate window that wants attention: Ctrl+Alt+A   -> Meta+Ctrl+A
    Kill Window:                          Ctrl+Alt+Esc -> Meta+Ctrl+Esc
    
    These are safe to change in the code rather than adding new ones and
    keeping the old ones around for compatibility's sake because
    KGlobalAccel::setDefaultShortcuts by default preserves old shortcuts
    when they are changed, and we aren't overriding that behavior here. So
    only new installs will get the new shortcuts and existing user installs
    will be untouched.
    
    
    (cherry picked from commit 529c3c29d300345219f3fcc173b3022c087bf32d)

--- src/keyboard_layout.cpp
+++ src/keyboard_layout.cpp
@@ -44,9 +44,10 @@ void KeyboardLayout::init()
     switchKeyboardAction->setObjectName(QStringLiteral("Switch to Next Keyboard Layout"));
     switchKeyboardAction->setProperty("componentName", QStringLiteral("KDE Keyboard Layout Switcher"));
     switchKeyboardAction->setProperty("componentDisplayName", i18n("Keyboard Layout Switcher"));
-    const QKeySequence sequence = QKeySequence(Qt::ALT | Qt::CTRL | Qt::Key_K);
+    const QKeySequence sequence = QKeySequence(Qt::META | Qt::ALT | Qt::Key_K);
     KGlobalAccel::self()->setDefaultShortcut(switchKeyboardAction, QList<QKeySequence>({sequence}));
     KGlobalAccel::self()->setShortcut(switchKeyboardAction, QList<QKeySequence>({sequence}));
+
     kwinApp()->platform()->setupActionForGlobalAccel(switchKeyboardAction);
     connect(switchKeyboardAction, &QAction::triggered, this, &KeyboardLayout::switchToNextLayout);
 
