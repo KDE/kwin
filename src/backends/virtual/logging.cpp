0fbd5fa3773059dd02f8101a39cbf3de859e234c
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2022-07-22 17:18:24 +0300

    backends/virtual: Provide own logging.h file
    target_sources() doesn't work as expected with generated source files.

---
+++ src/backends/virtual/logging.cpp
@@ -0,0 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2022 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "logging.h"
+
+Q_LOGGING_CATEGORY(KWIN_VIRTUAL, "kwin_platform_virtual", QtWarningMsg)
---
+++ src/backends/virtual/logging.h
@@ -0,0 +1,11 @@
+/*
+    SPDX-FileCopyrightText: 2022 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QLoggingCategory>
+
+Q_DECLARE_LOGGING_CATEGORY(KWIN_VIRTUAL)
