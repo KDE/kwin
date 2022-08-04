ebc5a6c6384aa7168ad76892e106edb053c527fe
Arjen Hiemstra <ahiemstra@heimr.nl>
2022-08-04 11:46:08 +0200

    Add scaledRect as a helper function to scale a QRectF by a scalar
    I have no idea why QRectF doesn't have this...

--- src/libkwineffects/kwineffects.h
+++ src/libkwineffects/kwineffects.h
@@ -188,6 +188,14 @@ enum EffectFrameStyle {
 };
 
 /**
+ * Scale a rect by a scalar.
+ */
+KWINEFFECTS_EXPORT inline QRectF scaledRect(const QRectF &rect, qreal scale)
+{
+    return QRectF{rect.x() * scale, rect.y() * scale, rect.width() * scale, rect.height() * scale};
+}
+
+/**
  * @short Base class for all KWin effects
  *
  * This is the base class for all effects. By reimplementing virtual methods
