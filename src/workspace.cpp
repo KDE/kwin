ff1a4d24e7570e393be4b96d7e4977cbb73afbe5
Xaver Hugl <xaver.hugl@gmail.com>
2022-09-10 18:11:30 +0200

    workspace: correct behavior when removing the placeholder output
    It needs to be removed from m_outputs before desktopResized(), or windows
    won't be moved to the new output

--- src/workspace.cpp
+++ src/workspace.cpp
@@ -1443,6 +1443,11 @@ void Workspace::addOutput(Output *output)
     if (!m_primaryOutput) {
         setPrimaryOutput(output);
     }
+    if (m_placeholderOutput) {
+        m_outputs.removeOne(m_placeholderOutput.get());
+        m_activeOutput = output;
+        m_primaryOutput = output;
+    }
 
     m_outputs.append(output);
 
@@ -1457,7 +1462,6 @@ void Workspace::addOutput(Output *output)
     Q_EMIT outputAdded(output);
 
     if (m_placeholderOutput) {
-        m_outputs.removeOne(m_placeholderOutput.get());
         Q_EMIT outputRemoved(m_placeholderOutput.get());
         m_placeholderOutput.reset();
         m_placeholderFilter.reset();
