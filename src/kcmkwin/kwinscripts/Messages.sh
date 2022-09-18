76c1d736d14bed72ad204864a059e96f71c4d9ed
Nicolas Fella <nicolas.fella@gmx.de>
2022-09-18 14:11:00 +0200

    [kcms/scripts] Make plugin id and translation domain match
    KQuickAddons::ConfigModule sets the translation domain based on the plugin id
    
    That is kcm_kwin_scripts. However the translation domain is kcm-kwin-scripts, which breaks loading translations
    
    Also fix Messages.sh to extract from QML
    
    BUG: 458438
    
    
    (cherry picked from commit fdb40395f02a74de368ec25d8516f7a1d8d04d2c)

--- src/kcmkwin/kwinscripts/Messages.sh
+++ src/kcmkwin/kwinscripts/Messages.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kcm-kwin-scripts.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/kcm_kwin_scripts.pot
 rm -f rc.cpp
