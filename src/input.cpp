88bc25b37605acede59e3d4b3dd08fd68152faff
Arjen Hiemstra <ahiemstra@heimr.nl>
2022-10-24 16:11:27 +0200

    DecorationInputFilter: Use QPointF instead of QPoint for events
    `QMouseEvent::globalPos()` returns QPoint rather than QPointF, which
    internally rounds the screen position. This can result in mouse
    positions that are outside the screen, leading to problems. Since
    screenPos() seems to be the same, use that so we get a QPointF instead.
    Additionally, apply similar changes to the other events.

--- src/input.cpp
+++ src/input.cpp
@@ -1441,12 +1441,12 @@ public:
         if (!decoration) {
             return false;
         }
-        const QPointF p = event->globalPos() - decoration->window()->pos();
+        const QPointF p = event->screenPos() - decoration->window()->pos();
         switch (event->type()) {
         case QEvent::MouseMove: {
             QHoverEvent e(QEvent::HoverMove, p, p);
             QCoreApplication::instance()->sendEvent(decoration->decoration(), &e);
-            decoration->window()->processDecorationMove(p, event->globalPos());
+            decoration->window()->processDecorationMove(p, event->screenPos());
             return true;
         }
         case QEvent::MouseButtonPress:
@@ -1455,7 +1455,7 @@ public:
             if (actionResult.first) {
                 return actionResult.second;
             }
-            QMouseEvent e(event->type(), p, event->globalPos(), event->button(), event->buttons(), event->modifiers());
+            QMouseEvent e(event->type(), p, event->screenPos(), event->button(), event->buttons(), event->modifiers());
             e.setTimestamp(event->timestamp());
             e.setAccepted(false);
             QCoreApplication::sendEvent(decoration->decoration(), &e);
@@ -1600,13 +1600,13 @@ public:
         if (!decoration) {
             return false;
         }
-        const QPointF p = event->globalPos() - decoration->window()->pos();
+        const QPointF p = event->globalPosF() - decoration->window()->pos();
         switch (event->type()) {
         case QEvent::TabletMove:
         case QEvent::TabletEnterProximity: {
             QHoverEvent e(QEvent::HoverMove, p, p);
             QCoreApplication::instance()->sendEvent(decoration->decoration(), &e);
-            decoration->window()->processDecorationMove(p, event->globalPos());
+            decoration->window()->processDecorationMove(p, event->globalPosF());
             break;
         }
         case QEvent::TabletPress:
@@ -1614,7 +1614,7 @@ public:
             const bool isPressed = event->type() == QEvent::TabletPress;
             QMouseEvent e(isPressed ? QEvent::MouseButtonPress : QEvent::MouseButtonRelease,
                           p,
-                          event->globalPos(),
+                          event->globalPosF(),
                           Qt::LeftButton,
                           isPressed ? Qt::LeftButton : Qt::MouseButtons(),
                           input()->keyboardModifiers());
