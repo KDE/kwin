829e2f9a91e5af066c00268c9b9ef6c049064ade
Xaver Hugl <xaver.hugl@gmail.com>
2022-11-03 14:02:14 +0100

    backends/drm: enable hardware rotation by default

--- src/backends/drm/drm_output.cpp
+++ src/backends/drm/drm_output.cpp
@@ -399,7 +399,7 @@ DrmPipeline *DrmOutput::pipeline() const
 bool DrmOutput::queueChanges(const OutputConfiguration &config)
 {
     static bool valid;
-    static int envOnlySoftwareRotations = qEnvironmentVariableIntValue("KWIN_DRM_SW_ROTATIONS_ONLY", &valid) == 1 || !valid;
+    static int envOnlySoftwareRotations = qEnvironmentVariableIntValue("KWIN_DRM_SW_ROTATIONS_ONLY", &valid) == 1 && valid;
 
     const auto props = config.constChangeSet(this);
     const auto mode = props->mode.lock();
