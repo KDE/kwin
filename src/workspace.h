d910622b7b59480b03d06e2a9ecb2e5222605b14
Natalie Clarius <natalie_clarius@yahoo.de>
2022-11-21 19:27:47 +0100

    workspace: add minimize all function

--- src/workspace.h
+++ src/workspace.h
@@ -380,6 +380,8 @@ public:
     void setShowingDesktop(bool showing, bool animated = true);
     bool showingDesktop() const;
 
+    void toggleMinimizeAll();
+
     void removeX11Window(X11Window *); // Only called from X11Window::destroyWindow() or X11Window::releaseWindow()
     void setActiveWindow(Window *window);
     Group *findGroup(xcb_window_t leader) const;
@@ -717,6 +719,7 @@ private:
     QList<Window *> attention_chain;
 
     bool showing_desktop;
+    QList<Window *> desktopMinimizedWindows;
 
     QList<Group *> groups;
 
