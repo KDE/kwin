d8da7a4eaa4a61b2f0c379fb0d6ce9d7dd0b6d2d
Asahi Lina <lina@asahilina.net>
2022-12-21 00:53:29 +0900

    kwinglutils: Restore the default FB when the last FBO is unbound
    Fixes GL warnings in GbmSurface::makeContextCurrent() when using
    screencasting.

--- src/libkwineffects/kwinglutils.cpp
+++ src/libkwineffects/kwinglutils.cpp
@@ -983,6 +983,8 @@ GLFramebuffer *GLFramebuffer::popFramebuffer()
     GLFramebuffer *ret = s_fbos.pop();
     if (!s_fbos.isEmpty()) {
         s_fbos.top()->bind();
+    } else {
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
     }
 
     return ret;
