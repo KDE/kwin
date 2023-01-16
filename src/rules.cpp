cf0531208d15c3d412e6f50c545b2a4a1e742cf0
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-01-16 16:21:53 +0200

    Add a note to drop temporary window rules
    It's an obscure feature, which perhaps not worth putting an effort to
    make it work on wayland and x11.

--- src/rules.cpp
+++ src/rules.cpp
@@ -903,6 +903,7 @@ void RuleBook::initializeX11()
     if (!c) {
         return;
     }
+    // TODO Plasma 6: Drop _KDE_NET_WM_TEMPORARY_RULES support in kwin and kstart.
     m_temporaryRulesMessages.reset(new KXMessages(c, kwinApp()->x11RootWindow(), "_KDE_NET_WM_TEMPORARY_RULES", nullptr));
     connect(m_temporaryRulesMessages.get(), &KXMessages::gotMessage, this, &RuleBook::temporaryRulesMessage);
 }
