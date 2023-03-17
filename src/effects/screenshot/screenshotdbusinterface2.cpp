1f7975e2715f955c059251089277612fc74f9459
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-03-17 17:16:08 +0200

    effects/screenshot: Provide screenshot scale information

--- src/effects/screenshot/screenshotdbusinterface2.cpp
+++ src/effects/screenshot/screenshotdbusinterface2.cpp
@@ -284,6 +284,7 @@ void ScreenShotSinkPipe2::flush(const QImage &image, const QVariantMap &attribut
     results.insert(QStringLiteral("width"), quint32(image.width()));
     results.insert(QStringLiteral("height"), quint32(image.height()));
     results.insert(QStringLiteral("stride"), quint32(image.bytesPerLine()));
+    results.insert(QStringLiteral("scale"), double(image.devicePixelRatio()));
     QDBusConnection::sessionBus().send(m_replyMessage.createReply(results));
 
     QtConcurrent::run([fileDescriptor = std::move(m_fileDescriptor), image]() mutable {
