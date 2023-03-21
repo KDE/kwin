16a07d5df2ee7433694a18be4383fe5912025fbb
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-03-21 15:03:25 +0200

    wayland: Handle xdg_wm_base being destroyed before surface role
    If a client owns several windows (for example it can be the case with
    plasmashell) and it crashes, it's possible to encounter the following
    case:
    
    - xdg_wm_base resources are destroyed
    - xdg_toplevel is destroyed
    - another xdg_toplevel is destroyed
    
    When kwin processes the destruction of the first xdg_toplevel, it may
    ping the second xdg_toplevel. But the xdg_wm_base is already free()d by
    that time, so kwin can access already released memory.
    
    In order to prevent that, make the associated XdgSurfaceInterface
    objects inert. Since XdgToplevelInterface and XdgPopupInterface will
    become useless after destroying XdgSurfaceInterface, make them inert too.
    
    As the spec states, it's illegal to destroy a bound xdg_wm_base object
    while there are still alive xdg surfaces created by it so destroying the
    surface role objects should be fine.

--- src/wayland/xdgshell_interface_p.h
+++ src/wayland/xdgshell_interface_p.h
@@ -34,6 +34,7 @@ public:
     QMap<quint32, QTimer *> pings;
 
 protected:
+    void xdg_wm_base_destroy_resource(Resource *resource) override;
     void xdg_wm_base_destroy(Resource *resource) override;
     void xdg_wm_base_create_positioner(Resource *resource, uint32_t id) override;
     void xdg_wm_base_get_xdg_surface(Resource *resource, uint32_t id, ::wl_resource *surface) override;
