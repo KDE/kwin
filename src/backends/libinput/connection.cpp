91b37e6fe5b91f775d691c0761db398ba7b30903
Alexander Volkov <avolkov@astralinux.ru>
2023-05-15 19:56:59 +0300

    libinput: Prefer internal screen as touchscreen match
    It looks like it should have been from the very beginning
    but the logic was broken by mistake.

--- src/backends/libinput/connection.cpp
+++ src/backends/libinput/connection.cpp
@@ -666,11 +666,13 @@ void Connection::applyScreenToDevice(Device *device)
         if (internalOutput && testScreenMatches(internalOutput)) {
             deviceOutput = internalOutput;
         }
-        // let's compare all screens for size
-        for (BackendOutput *output : outputs) {
-            if (testScreenMatches(output)) {
-                deviceOutput = output;
-                break;
+        if (!deviceOutput) {
+            // let's compare all screens for size
+            for (BackendOutput *output : outputs) {
+                if (testScreenMatches(output)) {
+                    deviceOutput = output;
+                    break;
+                }
             }
         }
         if (!deviceOutput) {
