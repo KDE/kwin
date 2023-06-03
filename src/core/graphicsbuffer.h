f579b073c9e766774d1f74707195a0ffc8dee910
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-06-03 11:33:57 +0300

    core: Change type of DmaBufAttributes::{pitch,offset}
    Change it to uint32_t[] to match drmModeAddFB() API.

--- src/core/graphicsbuffer.h
+++ src/core/graphicsbuffer.h
@@ -24,8 +24,8 @@ struct DmaBufAttributes
     uint64_t modifier = 0;
 
     FileDescriptor fd[4];
-    int offset[4] = {0, 0, 0, 0};
-    int pitch[4] = {0, 0, 0, 0};
+    uint32_t offset[4] = {0, 0, 0, 0};
+    uint32_t pitch[4] = {0, 0, 0, 0};
 };
 
 struct ShmAttributes
