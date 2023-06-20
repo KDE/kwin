acf1333bee2492cca7d1632c220d202934cdba15
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-06-20 17:54:19 +0300

    plugins/overview: Precompile qml code
    This saves us qml parsing extra step.

--- src/plugins/overview/overvieweffect.cpp
+++ src/plugins/overview/overvieweffect.cpp
@@ -54,7 +54,7 @@ OverviewEffect::OverviewEffect()
     initConfig<OverviewConfig>();
     reconfigure(ReconfigureAll);
 
-    setSource(QUrl::fromLocalFile(QStandardPaths::locate(QStandardPaths::GenericDataLocation, QStringLiteral("kwin/effects/overview/qml/main.qml"))));
+    setSource(QUrl(QStringLiteral("qrc:/overview/qml/main.qml")));
 }
 
 OverviewEffect::~OverviewEffect()
