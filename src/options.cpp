f22da5f62a2f0a6b1977e2cdb0859cbb7d6ef6df
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-10-20 14:32:36 +0300

    Fix reloading animation speed option
    Amends 4d2c9f5d886f796dd690b8ad1e1761ae74bd2e66.
    
    Prior to 4d2c9f5d886f796dd690b8ad1e1761ae74bd2e66, the Compositor used
    to force the Options to reload settings when starting compositing.
    
    Unfortunately it was overlooked that Options::animationTimeFactor() can
    return an old value when the Options::animationSpeedChanged() signal is
    emitted. This change addresses that.

--- src/options.cpp
+++ src/options.cpp
@@ -96,6 +96,7 @@ Options::Options(QObject *parent)
     m_configWatcher = KConfigWatcher::create(m_settings->sharedConfig());
     connect(m_configWatcher.data(), &KConfigWatcher::configChanged, this, [this](const KConfigGroup &group, const QByteArrayList &names) {
         if (group.name() == QLatin1String("KDE") && names.contains(QByteArrayLiteral("AnimationDurationFactor"))) {
+            m_settings->load();
             Q_EMIT animationSpeedChanged();
         } else if (group.name() == QLatin1String("Xwayland")) {
             workspace()->reconfigure();
