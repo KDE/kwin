38032bebc4399396927e91954a898852378f83ca
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-11-21 23:54:42 +0200

    effects: Fix type of X-KDE-Ordering
    It's an integer. Needed to fix json-validation pipeline.

--- src/scripting/scriptedeffect.cpp
+++ src/scripting/scriptedeffect.cpp
@@ -168,7 +168,7 @@ ScriptedEffect *ScriptedEffect::create(const KPluginMetaData &effect)
         return nullptr;
     }
 
-    return ScriptedEffect::create(name, scriptFile, effect.value(QStringLiteral("X-KDE-Ordering")).toInt(), effect.value(QStringLiteral("X-KWin-Exclusive-Category")));
+    return ScriptedEffect::create(name, scriptFile, effect.value(QStringLiteral("X-KDE-Ordering"), 0), effect.value(QStringLiteral("X-KWin-Exclusive-Category")));
 }
 
 ScriptedEffect *ScriptedEffect::create(const QString &effectName, const QString &pathToScript, int chainPosition, const QString &exclusiveCategory)
