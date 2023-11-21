75935c126c717b20460998539be7360b2a6fe019
Kai Uwe Broulik <kde@privat.broulik.de>
2023-11-21 13:50:10 +0100

    layershellv1window: Check isDeleted before activating it
    Opening the user actions menu causes auto-hiding layer-shell windows
    (such as Kickoff, KRunner) to hide, closing the menu and having KWin
    transfer focus back to the window which is now half-destroyed.
    
    XdgToplevelWindow::acceptsFocus() also checks for isDeleted().

--- src/layershellv1window.cpp
+++ src/layershellv1window.cpp
@@ -218,7 +218,7 @@ Layer LayerShellV1Window::belongsToLayer() const
 
 bool LayerShellV1Window::acceptsFocus() const
 {
-    return m_shellSurface->acceptsFocus();
+    return !isDeleted() && m_shellSurface->acceptsFocus();
 }
 
 void LayerShellV1Window::moveResizeInternal(const QRectF &rect, MoveResizeMode mode)
