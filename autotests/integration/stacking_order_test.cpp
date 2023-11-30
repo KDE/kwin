152a4322e906d88a914a059da936201b43514ba6
Jarek Janik <ann.of.the.green.gables@gmail.com>
2023-11-30 01:57:11 +0100

    autotests: add test for Workspace::lowerWindow
    This test covers bugs: 478382 and 478383.
    
    (cherry picked from commit dc34da07dcf35bbcc21a7d5aa295287e15295fc3)

--- autotests/integration/stacking_order_test.cpp
+++ autotests/integration/stacking_order_test.cpp
@@ -37,6 +37,7 @@ private Q_SLOTS:
 
     void testTransientIsAboveParent();
     void testRaiseTransient();
+    void testLowerTransient();
     void testDeletedTransient();
 
     void testGroupTransientIsAboveWindowGroup();
@@ -181,6 +182,94 @@ void StackingOrderTest::testRaiseTransient()
     QTRY_VERIFY(transient->isActive());
     QTRY_VERIFY(!anotherWindow->isActive());
     QCOMPARE(workspace()->stackingOrder(), (QList<Window *>{anotherWindow, parent, transient}));
+}
+
+void StackingOrderTest::testLowerTransient()
+{
+    // This test verifies that when a window that has transients or is a transient itself is lowered,
+    // then the whole window hierarchy that this window is a part of is lowered too,
+    // as well as that a relative stacking of windows within this hierarchy is left intact.
+
+    const int windowsQuantity = 5;
+
+    std::unique_ptr<KWayland::Client::Surface> surfaces[windowsQuantity];
+    std::unique_ptr<Test::XdgToplevel> shellSurfaces[windowsQuantity];
+    Window *windows[windowsQuantity];
+
+    for (int i = 0; i < windowsQuantity; i++) {
+        surfaces[i] = Test::createSurface();
+        QVERIFY(surfaces[i]);
+        shellSurfaces[i] = std::unique_ptr<Test::XdgToplevel>(Test::createXdgToplevelSurface(surfaces[i].get()));
+        QVERIFY(shellSurfaces[i]);
+    }
+
+    // link 5 windows into the following hierarchy:
+    //      * 0 - unrelated to 1..4
+    //      * 1 - parent of 2, 3
+    //      * 3 - parent of 4
+    //
+    //                   +---+
+    //                   | 4 |
+    //                   +-+-+
+    //                     |
+    //       +-+-+       +---+       +---+
+    //       | 2 |       | 3 |       | 0 |
+    //       +---+       +---+       +---+
+    //            \     /
+    //             +---+
+    //             | 1 |
+    //             +---+
+
+    shellSurfaces[2]->set_parent(shellSurfaces[1]->object());
+    shellSurfaces[3]->set_parent(shellSurfaces[1]->object());
+    shellSurfaces[4]->set_parent(shellSurfaces[3]->object());
+
+    for (int i = 0; i < windowsQuantity; i++) {
+        windows[i] = Test::renderAndWaitForShown(surfaces[i].get(), QSize(128, 128), Qt::green);
+        QVERIFY(windows[i]);
+    }
+
+    // base windows order for following tests
+    QList<Window *> baseWindowsOrder{windows[0], windows[1], windows[2], windows[3], windows[4]};
+    // expected result for the following tests - window hierarchy 1..4 went below window 0,
+    // relative order within hierarchy is preserved
+    QList<Window *> expectedWindowsOrder{windows[1], windows[2], windows[3], windows[4], windows[0]};
+
+    // verify initial windows order
+    QCOMPARE(workspace()->stackingOrder(), baseWindowsOrder);
+
+    // for every window from 1..4 hierarchy
+    for (int i = 1; i < 5; i++) {
+        // lower that window
+        workspace()->lowerWindow(windows[i]);
+        // verify result
+        QCOMPARE(workspace()->stackingOrder(), expectedWindowsOrder);
+        // lower unrelated window 0 and verify that we restored base windows order
+        workspace()->lowerWindow(windows[0]);
+        QCOMPARE(workspace()->stackingOrder(), baseWindowsOrder);
+    }
+
+    // restack transients so that 2 goes on top
+    workspace()->raiseWindow(windows[2]);
+    // update base order of windows for the rest of tests
+    baseWindowsOrder = QList<Window *>{windows[0], windows[1], windows[3], windows[4], windows[2]};
+    // update expected order of windows for the rest of tests - window hierarchy 1..4 goes below window 0,
+    // relative order within hierarchy is preserved
+    expectedWindowsOrder = QList<Window *>{windows[1], windows[3], windows[4], windows[2], windows[0]};
+
+    // verify initial windows order
+    QCOMPARE(workspace()->stackingOrder(), baseWindowsOrder);
+
+    // for every window from 1..4 hierarchy
+    for (int i = 1; i <= 4; i++) {
+        // lower that window
+        workspace()->lowerWindow(windows[i]);
+        // verify result
+        QCOMPARE(workspace()->stackingOrder(), expectedWindowsOrder);
+        // lower unrelated window 0 and verify that we restored base windows order
+        workspace()->lowerWindow(windows[0]);
+        QCOMPARE(workspace()->stackingOrder(), baseWindowsOrder);
+    }
 }
 
 struct WindowUnrefDeleter
