f76bd7a70eb802c29ed68cc714531ee5cfd67b3b
Aleix Pol Gonzalez <aleix.pol_gonzalez@mercedes-benz.com>
2023-12-06 15:45:33 +0100

    Address licence of plugin.h
    The rest of the project is GPLv2, this one is GPLv3 for no apparent
    reason. Even the .cpp file is GPLv2.
    
    Signed-off-by: Victoria Fischer <victoria.fischer@mbition.io>
    
    
    (cherry picked from commit 0527d301dd0ed8dc462fe4f600eb221dddd56b32)

--- src/effects/private/plugin.h
+++ src/effects/private/plugin.h
@@ -1,7 +1,7 @@
 /*
     SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
 
-    SPDX-License-Identifier: GPL-3.0-or-later
+    SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 #pragma once
