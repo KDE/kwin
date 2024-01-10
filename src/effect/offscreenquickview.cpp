bce104708096f754ef091aec9a1bd4b71c0b2edc
Xaver Hugl <xaver.hugl@gmail.com>
2024-01-10 20:56:15 +0100

    effect/offscreenquickview: always use GL_RGBA8
    GL_RGB8 isn't supported by OpenGL ES
    
    BUG: 479055
    
    
    (cherry picked from commit a05a6d8727e0644106e38191b2aeb26270a23dbd)

--- src/effect/offscreenquickview.cpp
+++ src/effect/offscreenquickview.cpp
@@ -220,7 +220,7 @@ void OffscreenQuickView::update()
 
             QOpenGLFramebufferObjectFormat fboFormat;
             fboFormat.setAttachment(QOpenGLFramebufferObject::CombinedDepthStencil);
-            fboFormat.setInternalTextureFormat(d->m_hasAlphaChannel ? GL_RGBA8 : GL_RGB8);
+            fboFormat.setInternalTextureFormat(GL_RGBA8);
 
             d->m_fbo = std::make_unique<QOpenGLFramebufferObject>(nativeSize, fboFormat);
             if (!d->m_fbo->isValid()) {
