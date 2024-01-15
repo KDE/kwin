359f36d6ab832b97e4edecebe5b796f1c9dbd713
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2024-01-15 11:44:09 +0200

    autotests: Add a test to verify that show desktop mode quits after activating a window

--- autotests/integration/showing_desktop_test.cpp
+++ autotests/integration/showing_desktop_test.cpp
@@ -28,6 +28,8 @@ private Q_SLOTS:
 
     void testRestoreFocus();
     void testRestoreFocusWithDesktopWindow();
+    void testQuitAfterActivatingHiddenWindow();
+    void testDontQuitAfterActivatingDock();
 };
 
 void ShowingDesktopTest::initTestCase()
@@ -113,6 +115,76 @@ void ShowingDesktopTest::testRestoreFocusWithDesktopWindow()
 
     QVERIFY(workspace()->activeWindow());
     QCOMPARE(workspace()->activeWindow(), window2);
+}
+
+void ShowingDesktopTest::testQuitAfterActivatingHiddenWindow()
+{
+    // This test verifies that the show desktop mode is deactivated after activating a hidden window.
+
+    std::unique_ptr<KWayland::Client::Surface> surface1(Test::createSurface());
+    std::unique_ptr<Test::XdgToplevel> shellSurface1(Test::createXdgToplevelSurface(surface1.get()));
+    auto window1 = Test::renderAndWaitForShown(surface1.get(), QSize(100, 50), Qt::blue);
+    std::unique_ptr<KWayland::Client::Surface> surface2(Test::createSurface());
+    std::unique_ptr<Test::XdgToplevel> shellSurface2(Test::createXdgToplevelSurface(surface2.get()));
+    auto window2 = Test::renderAndWaitForShown(surface2.get(), QSize(100, 50), Qt::blue);
+    QCOMPARE(workspace()->activeWindow(), window2);
+
+    workspace()->slotToggleShowDesktop();
+    QVERIFY(workspace()->showingDesktop());
+
+    workspace()->activateWindow(window1);
+    QVERIFY(!workspace()->showingDesktop());
+}
+
+void ShowingDesktopTest::testDontQuitAfterActivatingDock()
+{
+    // This test verifies that activating windows belonging to desktop doesn't break showing desktop mode.
+
+    std::unique_ptr<KWayland::Client::Surface> desktopSurface(Test::createSurface());
+    std::unique_ptr<Test::LayerSurfaceV1> desktopShellSurface(Test::createLayerSurfaceV1(desktopSurface.get(), QStringLiteral("desktop")));
+    desktopShellSurface->set_keyboard_interactivity(1);
+    desktopShellSurface->set_layer(Test::LayerShellV1::layer_background);
+    desktopShellSurface->set_size(0, 0);
+    desktopShellSurface->set_exclusive_zone(-1);
+    desktopShellSurface->set_anchor(Test::LayerSurfaceV1::anchor_bottom
+                                    | Test::LayerSurfaceV1::anchor_top
+                                    | Test::LayerSurfaceV1::anchor_left
+                                    | Test::LayerSurfaceV1::anchor_right);
+    desktopSurface->commit(KWayland::Client::Surface::CommitFlag::None);
+    QSignalSpy desktopConfigureRequestedSpy(desktopShellSurface.get(), &Test::LayerSurfaceV1::configureRequested);
+    QVERIFY(desktopConfigureRequestedSpy.wait());
+    auto desktop = Test::renderAndWaitForShown(desktopSurface.get(), desktopConfigureRequestedSpy.last().at(1).toSize(), Qt::blue);
+
+    std::unique_ptr<KWayland::Client::Surface> dockSurface{Test::createSurface()};
+    std::unique_ptr<Test::LayerSurfaceV1> dockShellSurface{Test::createLayerSurfaceV1(dockSurface.get(), QStringLiteral("dock"))};
+    dockShellSurface->set_size(1280, 50);
+    dockShellSurface->set_anchor(Test::LayerSurfaceV1::anchor_bottom);
+    dockShellSurface->set_exclusive_zone(50);
+    dockShellSurface->set_keyboard_interactivity(1);
+    dockSurface->commit(KWayland::Client::Surface::CommitFlag::None);
+    QSignalSpy dockConfigureRequestedSpy(dockShellSurface.get(), &Test::LayerSurfaceV1::configureRequested);
+    QVERIFY(dockConfigureRequestedSpy.wait());
+    auto dock = Test::renderAndWaitForShown(dockSurface.get(), dockConfigureRequestedSpy.last().at(1).toSize(), Qt::blue);
+
+    std::unique_ptr<KWayland::Client::Surface> surface(Test::createSurface());
+    std::unique_ptr<Test::XdgToplevel> shellSurface(Test::createXdgToplevelSurface(surface.get()));
+    auto window = Test::renderAndWaitForShown(surface.get(), QSize(100, 50), Qt::blue);
+    QVERIFY(window->isActive());
+
+    workspace()->slotToggleShowDesktop();
+    QVERIFY(workspace()->showingDesktop());
+    QVERIFY(desktop->isActive());
+
+    workspace()->activateWindow(dock);
+    QVERIFY(workspace()->showingDesktop());
+    QVERIFY(dock->isActive());
+
+    workspace()->activateWindow(desktop);
+    QVERIFY(workspace()->showingDesktop());
+    QVERIFY(desktop->isActive());
+
+    workspace()->slotToggleShowDesktop();
+    QVERIFY(!workspace()->showingDesktop());
 }
 
 WAYLANDTEST_MAIN(ShowingDesktopTest)
