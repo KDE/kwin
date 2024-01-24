2505fd03f1bbbaaef33668ad8030c6b0578328a6
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2024-01-24 16:46:15 +0200

    kconf_update: Add script to drop old desktop switching shortcuts
    BUG: 479984

---
+++ kconf_update/kwin-6.0-delete-desktop-switching-shortcuts.cpp
@@ -0,0 +1,33 @@
+/*
+    SPDX-FileCopyrightText: 2023 Marco Martin <mart@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include <KGlobalAccel>
+
+#include <QAction>
+#include <QGuiApplication>
+#include <QStandardPaths>
+
+int main(int argc, char **argv)
+{
+    QGuiApplication app(argc, argv);
+
+    const QStringList actionNames{
+        QStringLiteral("Walk Through Desktops"),
+        QStringLiteral("Walk Through Desktops (Reverse)"),
+        QStringLiteral("Walk Through Desktop List"),
+        QStringLiteral("Walk Through Desktop List (Reverse)"),
+    };
+
+    for (const QString &actionName : actionNames) {
+        QAction action;
+        action.setObjectName(actionName);
+        action.setProperty("componentName", QStringLiteral("kwin"));
+        KGlobalAccel::self()->setShortcut(&action, {QKeySequence()}, KGlobalAccel::NoAutoloading);
+        KGlobalAccel::self()->removeAllShortcuts(&action);
+    }
+
+    return 0;
+}
