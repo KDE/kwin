0a3b8178a33c727695e849229ca9e22932007ee5
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2024-03-10 13:23:59 +0200

    Introduce Window::interactiveMoveResizeAnchor
    This provides the grab point that controls the interactive move resize
    operation. It can be used outside Window::handleInteractiveMoveResize()
    to position XdgToplevelWindow when a configure event is acked.

--- src/events.cpp
+++ src/events.cpp
@@ -1323,6 +1323,7 @@ void X11Window::NETMoveResize(qreal x_root, qreal y_root, NET::Direction directi
                 finishInteractiveMoveResize(false);
             }
             setInteractiveMoveResizePointerButtonDown(true);
+            setInteractiveMoveResizeAnchor(QPointF(x_root, y_root));
             setInteractiveMoveOffset(QPointF(qreal(x_root - x()) / width(), qreal(y_root - y())) / height()); // map from global
             setUnrestrictedInteractiveMoveResize(false);
             setInteractiveMoveResizeGravity(convert[direction]);
