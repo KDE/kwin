158b47846f413e1df36b8c0275145cae454b6cae
Marco Martin <notmart@gmail.com>
2024-03-14 13:33:27 +0100

    remove pure formatting change

--- src/window.cpp
+++ src/window.cpp
@@ -3489,7 +3489,6 @@ void Window::setQuickTileMode(QuickTileMode mode, bool keyboard)
             setMaximize(true, true);
             setGeometryRestore(effectiveGeometryRestore);
         }
-
         doSetQuickTileMode();
         Q_EMIT quickTileModeChanged();
         return;
