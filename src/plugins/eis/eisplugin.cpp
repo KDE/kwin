bca99d0b4c1343bead63755af31943947efdec28
David Redondo <kde@david-redondo.de>
2024-04-15 16:13:00 +0200

    Add missing license header
    Amends 499d006e3a31f83b5fb25481c30025cde9b6b64f
    GIT_SILENT

--- src/plugins/eis/eisplugin.cpp
+++ src/plugins/eis/eisplugin.cpp
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2024 David Redondo <kde@david-redono.de>
+
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+
 #include "eisplugin.h"
 
 #include "eisbackend.h"
