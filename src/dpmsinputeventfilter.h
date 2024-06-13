c2141dc4efac6e7c51815aa57272b5537e60f101
David Redondo <kde@david-redondo.de>
2024-06-13 16:53:23 +0200

    Also wake up screens on tablet interactions
    BUG:451359
    FIXED-IN:6.1.1

--- src/dpmsinputeventfilter.h
+++ src/dpmsinputeventfilter.h
@@ -32,6 +32,11 @@ public:
     bool touchDown(qint32 id, const QPointF &pos, std::chrono::microseconds time) override;
     bool touchMotion(qint32 id, const QPointF &pos, std::chrono::microseconds time) override;
     bool touchUp(qint32 id, std::chrono::microseconds time) override;
+    bool tabletToolEvent(TabletEvent *event) override;
+    bool tabletToolButtonEvent(uint button, bool pressed, const TabletToolId &tabletToolId, std::chrono::microseconds time) override;
+    bool tabletPadButtonEvent(uint button, bool pressed, const TabletPadId &tabletPadId, std::chrono::microseconds time) override;
+    bool tabletPadStripEvent(int number, int position, bool isFinger, const TabletPadId &tabletPadId, std::chrono::microseconds time) override;
+    bool tabletPadRingEvent(int number, int position, bool isFinger, const TabletPadId &tabletPadId, std::chrono::microseconds time) override;
 
 private:
     void notify();
