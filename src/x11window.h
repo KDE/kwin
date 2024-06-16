955a3ecbbbe9f641730eb359f8d63a66c8e0ed91
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2024-06-16 22:40:56 +0300

    Track xwayland buffer scale in X11Window
    This is needed to ensure that X11Window properly updates the geometry of
    X windows in the device pixels even though the logical geometry is unaffected
    by the new xwayland scale factor.

--- src/x11window.h
+++ src/x11window.h
@@ -442,6 +442,7 @@ private:
     Xcb::Window m_client;
     Xcb::Window m_wrapper;
     Xcb::Window m_frame;
+    qreal m_bufferScale = 1;
     xcb_window_t m_wmClientLeader = XCB_WINDOW_NONE;
     int m_activityUpdatesBlocked;
     bool m_blockedActivityUpdatesRequireTransients;
