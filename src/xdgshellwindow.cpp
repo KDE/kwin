49f656a3d9e4cf1167d8212dabcc4cc36f29b1ae
Xaver Hugl <xaver.hugl@gmail.com>
2024-07-18 16:21:46 +0200

    xdgshellwindow: guard against sending negative configure bounds
    It's unlikely to happen, but if it were to happen, it would cause crashes

--- src/xdgshellwindow.cpp
+++ src/xdgshellwindow.cpp
@@ -697,8 +697,8 @@ XdgSurfaceConfigure *XdgToplevelWindow::sendRoleConfigure() const
 
     if (nextClientSize.isEmpty()) {
         QSizeF bounds = workspace()->clientArea(PlacementArea, this, moveResizeOutput()).size();
-        bounds.rwidth() -= framePadding.width();
-        bounds.rheight() -= framePadding.height();
+        bounds.setWidth(std::max(1.0, bounds.width() - framePadding.width()));
+        bounds.setHeight(std::max(1.0, bounds.height() - framePadding.height()));
         m_shellSurface->sendConfigureBounds(bounds.toSize());
     }
 
