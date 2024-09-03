4b1caed5644bf34aabfa7af533d19fad420568d5
Xaver Hugl <xaver.hugl@gmail.com>
2024-09-03 13:43:26 +0000

    workspace: don't rearrange immediately when a window with struts gets removed
    Otherwise, rearrange can happen on intermediate output configurations, as layer shell
    windows can get closed in response to outputs being disabled.
    That rearrange on intermediate output configurations can confuse the placement tracker
    logic, which may then move windows to weird locations or even offscreen
    
    CCBUG: 479694
    
    
    (cherry picked from commit 24ab95bdd76dd189aa9a190d75f19ee94f733077)
    
    Co-authored-by: Xaver Hugl <xaver.hugl@gmail.com>

--- src/workspace.cpp
+++ src/workspace.cpp
@@ -867,7 +867,7 @@ void Workspace::removeWindow(Window *window)
         setupWindowShortcutDone(false);
     }
     if (window->hasStrut()) {
-        rearrange();
+        scheduleRearrange();
     }
 
     Q_EMIT windowRemoved(window);
