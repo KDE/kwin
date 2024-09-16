5ea9007b084562097c631eeeebdf16cae8ba3650
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2024-09-16 15:01:43 +0000

    kpackage-structure: add declarativescript structure to KWin/Script
    Fix `kpackagetool6 --install <declarativescript>` complaining about missing `main.js`.
    
    
    (cherry picked from commit 79bc7a5b812cc8c248b601fd17bd414f863f6607)

--- src/plugins/kpackage/scripts/scripts.cpp
+++ src/plugins/kpackage/scripts/scripts.cpp
@@ -22,10 +22,23 @@ public:
 
         package->addDirectoryDefinition("code", QStringLiteral("code"));
 
-        package->addFileDefinition("mainscript", QStringLiteral("code/main.js"));
-        package->setRequired("mainscript", true);
-
         package->setMimeTypes("scripts", QStringList{QStringLiteral("text/plain")});
+    }
+
+    void pathChanged(KPackage::Package *package) override
+    {
+        if (!package->metadata().isValid()) {
+            return;
+        }
+
+        const QString api = package->metadata().value(QStringLiteral("X-Plasma-API"));
+        if (api == QStringLiteral("javascript")) {
+            package->addFileDefinition("mainscript", QStringLiteral("code/main.js"));
+            package->setRequired("mainscript", true);
+        } else if (api == QStringLiteral("declarativescript")) {
+            package->addFileDefinition("mainscript", QStringLiteral("ui/main.qml"));
+            package->setRequired("mainscript", true);
+        }
     }
 };
 
