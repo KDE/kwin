2d4d2913a02917d092f9d00754a9fe53a831597d
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2024-09-26 15:01:34 +0000

    Guard closed windows against move and resize operations
    A closed window cannot react to new geometry, also requesting new
    geometry can involve accessing resources that are gone/deleted now.
    
    Scripts trying to move or resize a closed window is a bug though.
    
    BUG: 492213
    
    
    (cherry picked from commit f339c4281f6c0513e1b847599f28f750eec01923)
    
    Co-authored-by: Vlad Zahorodnii <vlad.zahorodnii@kde.org>

--- src/window.cpp
+++ src/window.cpp
@@ -3363,6 +3363,10 @@ void Window::setMoveResizeOutput(Output *output)
 
 void Window::move(const QPointF &point)
 {
+    if (isDeleted()) {
+        return;
+    }
+
     const QRectF rect = QRectF(point, m_moveResizeGeometry.size());
 
     setMoveResizeGeometry(rect);
@@ -3371,6 +3375,10 @@ void Window::move(const QPointF &point)
 
 void Window::resize(const QSizeF &size)
 {
+    if (isDeleted()) {
+        return;
+    }
+
     const QRectF rect = QRectF(m_moveResizeGeometry.topLeft(), size);
 
     setMoveResizeGeometry(rect);
@@ -3379,6 +3387,10 @@ void Window::resize(const QSizeF &size)
 
 void Window::moveResize(const QRectF &rect)
 {
+    if (isDeleted()) {
+        return;
+    }
+
     setMoveResizeGeometry(rect);
     moveResizeInternal(rect, MoveResizeMode::MoveResize);
 }
