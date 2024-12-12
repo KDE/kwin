b8e9b459af73b753a0744cbb0a09f55d244b9a30
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2024-12-12 19:10:08 +0200

    Fix multi screen quick tiling
    When a quick tiled window is moved between the screens, it will migrate
    between the tile trees. The Tile::addWindow() will evacuate the window
    from the previous tile only if both tiles belong to the same tree. It's
    not going to be the same when moving a window between outputs. We need to
    handle it explicitly.

--- autotests/integration/quick_tiling_test.cpp
+++ autotests/integration/quick_tiling_test.cpp
@@ -69,6 +69,7 @@ private Q_SLOTS:
     void testX11QuickTilingAfterVertMaximize();
     void testShortcut_data();
     void testShortcut();
+    void testMultiScreen();
     void testScript_data();
     void testScript();
     void testDontCrashWithMaximizeWindowRule();
@@ -797,6 +798,95 @@ void QuickTilingTest::testShortcut()
 
     QEXPECT_FAIL("maximize", "Geometry changed called twice for maximize", Continue);
     QCOMPARE(window->frameGeometry(), expectedGeometry);
+}
+
+void QuickTilingTest::testMultiScreen()
+{
+    // This test verifies that a window can be moved between screens by continuously pressing Meta+arrow.
+
+    std::unique_ptr<KWayland::Client::Surface> surface(Test::createSurface());
+    std::unique_ptr<Test::XdgToplevel> shellSurface(Test::createXdgToplevelSurface(surface.get()));
+    auto window = Test::renderAndWaitForShown(surface.get(), QSize(100, 100), Qt::blue);
+
+    // We have to receive a configure event when the window becomes active.
+    QSignalSpy tileChangedSpy(window, &Window::tileChanged);
+    QSignalSpy toplevelConfigureRequestedSpy(shellSurface.get(), &Test::XdgToplevel::configureRequested);
+    QSignalSpy surfaceConfigureRequestedSpy(shellSurface->xdgSurface(), &Test::XdgSurface::configureRequested);
+    QVERIFY(surfaceConfigureRequestedSpy.wait());
+    QCOMPARE(surfaceConfigureRequestedSpy.count(), 1);
+
+    const struct
+    {
+        QuickTileMode shortcut;
+        QuickTileMode previous;
+        QuickTileMode next;
+        QRectF geometry;
+    } steps[] = {
+        // Not tiled -> tiled on the left half of the first screen
+        {
+            .shortcut = QuickTileFlag::Left,
+            .previous = QuickTileFlag::None,
+            .next = QuickTileFlag::Left,
+            .geometry = QRectF(0, 0, 640, 1024),
+        },
+        // Tiled on the left half of the first screen -> tiled on the right half of the first screen
+        {
+            .shortcut = QuickTileFlag::Right,
+            .previous = QuickTileFlag::Left,
+            .next = QuickTileFlag::Right,
+            .geometry = QRectF(640, 0, 640, 1024),
+        },
+        // Tiled on the right half of the first screen -> tiled on the left half of the second screen
+        {
+            .shortcut = QuickTileFlag::Right,
+            .previous = QuickTileFlag::Right,
+            .next = QuickTileFlag::Left,
+            .geometry = QRectF(1280, 0, 640, 1024),
+        },
+        // Tiled on the left half of the second screen -> tiled on the right half of the second screen
+        {
+            .shortcut = QuickTileFlag::Right,
+            .previous = QuickTileFlag::Left,
+            .next = QuickTileFlag::Right,
+            .geometry = QRectF(1920, 0, 640, 1024),
+        },
+        // Tiled on the right half of the second screen -> tiled on the left half of the second screen
+        {
+            .shortcut = QuickTileFlag::Left,
+            .previous = QuickTileFlag::Right,
+            .next = QuickTileFlag::Left,
+            .geometry = QRectF(1280, 0, 640, 1024),
+        },
+        // Tiled on the left half of the second screen -> tiled on the right half of the first screen
+        {
+            .shortcut = QuickTileFlag::Left,
+            .previous = QuickTileFlag::Left,
+            .next = QuickTileFlag::Right,
+            .geometry = QRectF(640, 0, 640, 1024),
+        },
+        // Tiled on the right half of the first screen -> tiled on the left half of the first screen
+        {
+            .shortcut = QuickTileFlag::Left,
+            .previous = QuickTileFlag::Right,
+            .next = QuickTileFlag::Left,
+            .geometry = QRectF(0, 0, 640, 1024),
+        },
+    };
+
+    for (const auto &step : steps) {
+        window->handleQuickTileShortcut(step.shortcut);
+        QCOMPARE(window->quickTileMode(), step.previous);
+        QCOMPARE(window->requestedQuickTileMode(), step.next);
+        QCOMPARE(window->moveResizeGeometry(), step.geometry);
+        QVERIFY(surfaceConfigureRequestedSpy.wait());
+        shellSurface->xdgSurface()->ack_configure(surfaceConfigureRequestedSpy.last().at(0).value<quint32>());
+        Test::render(surface.get(), toplevelConfigureRequestedSpy.last().at(0).toSize(), Qt::blue);
+        QVERIFY(tileChangedSpy.wait());
+        QCOMPARE(window->quickTileMode(), step.next);
+        QCOMPARE(window->requestedQuickTileMode(), step.next);
+        QCOMPARE(window->frameGeometry(), step.geometry);
+        QCOMPARE(window->moveResizeGeometry(), step.geometry);
+    }
 }
 
 void QuickTilingTest::testScript_data()
