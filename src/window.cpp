1fb514914abefae9a58907e9651ab86eaa7eeebf
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2024-12-16 16:23:21 +0200

    tiles: Add Tile::forgetWindow()

--- src/window.cpp
+++ src/window.cpp
@@ -3852,6 +3852,16 @@ void Window::requestTile(Tile *tile)
     Q_EMIT requestedTileChanged();
 }
 
+void Window::forgetTile(Tile *tile)
+{
+    if (m_requestedTile != tile) {
+        return;
+    }
+
+    m_requestedTile = nullptr;
+    doSetQuickTileMode();
+}
+
 void Window::setTileCompatibility(Tile *tile)
 {
     qCWarning(KWIN_CORE) << "Writing to the property window.tile is deprecated: use tile.addWindow() instead";
