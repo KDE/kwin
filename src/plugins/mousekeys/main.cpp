luisbocanegra/kwin-lightlyshaders

Implement MouseKeys on Wayland
This allows to move the mouse cursor using numpad keys.

This is an accessibility feature. On X11 this is implemented in XKB, on Wayland we get to do it outself.

It's implemented as a plugin that filters for relevant keys and generates mouse events through a virtual pointer device

src/plugins/mousekeys/main.cpp
@@ -1,0 +1,34 @@
+/*
+    SPDX-FileCopyrightText: 2022 Nicolas Fella <nicolas.fella@gmx.de>
+
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+
+#include <main.h>
+#include <plugin.h>
+
+#include "mousekeys.h"
+
+class KWIN_EXPORT MouseKeysFactory : public KWin::PluginFactory
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID PluginFactory_iid FILE "metadata.json")
+    Q_INTERFACES(KWin::PluginFactory)
+
+public:
+    std::unique_ptr<KWin::Plugin> create() const override
+    {
+        switch (KWin::kwinApp()->operationMode()) {
+        case KWin::Application::OperationModeXwayland:
+            [[fallthrough]];
+        case KWin::Application::OperationModeWaylandOnly:
+            return std::make_unique<MouseKeysFilter>();
+        case KWin::Application::OperationModeX11:
+            [[fallthrough]];
+        default:
+            return nullptr;
+        }
+    }
+};
+
+#include "main.moc"

KDE/kwin

BUG: 478620

src/plugins/mousekeys/main.cpp
@@ -1,0 +1,31 @@
+/*
+    SPDX-FileCopyrightText: 2022 Nicolas Fella <nicolas.fella@gmx.de>
+
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+
+#include <main.h>
+#include <plugin.h>
+
+#include "mousekeys.h"
+
+class KWIN_EXPORT MouseKeysFactory : public KWin::PluginFactory
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID PluginFactory_iid FILE "metadata.json")
+    Q_INTERFACES(KWin::PluginFactory)
+
+public:
+    std::unique_ptr<KWin::Plugin> create() const override
+    {
+        switch (KWin::kwinApp()->operationMode()) {
+        case KWin::Application::OperationModeWayland:
+            return std::make_unique<MouseKeysFilter>();
+        case KWin::Application::OperationModeX11:
+        default:
+            return nullptr;
+        }
+    }
+};
+
+#include "main.moc"