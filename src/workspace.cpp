40315cc5d007597ac7093c604b31673a6d8b5e6a
Xaver Hugl <xaver.hugl@gmail.com>
2025-01-02 02:50:01 +0100

    x11window: drop Xorg-specific code paths

--- src/workspace.cpp
+++ src/workspace.cpp
@@ -453,7 +453,6 @@ void Workspace::cleanupX11()
     VirtualDesktopManager *desktopManager = VirtualDesktopManager::self();
     desktopManager->setRootInfo(nullptr);
 
-    X11Window::cleanupX11();
     RootInfo::destroy();
     Xcb::Extensions::destroy();
 
@@ -1682,10 +1681,6 @@ void Workspace::disableGlobalShortcutsForClient(bool disable)
     QDBusConnection::sessionBus().asyncCall(message);
 
     m_globalShortcutsDisabledForWindow = disable;
-    // Update also Meta+LMB actions etc.
-    for (auto it = m_windows.constBegin(); it != m_windows.constEnd(); ++it) {
-        (*it)->updateMouseGrab();
-    }
 }
 
 QString Workspace::supportInformation() const
@@ -1996,10 +1991,6 @@ X11Window *Workspace::findClient(Predicate predicate, xcb_window_t w) const
     case Predicate::FrameIdMatch:
         return findClient([w](const X11Window *c) {
             return c->frameId() == w;
-        });
-    case Predicate::InputIdMatch:
-        return findClient([w](const X11Window *c) {
-            return c->inputId() == w;
         });
     }
     return nullptr;
