a879d60792cb44a145d8bf56608a334e35155059
Xaver Hugl <xaver.hugl@gmail.com>
2025-01-19 12:34:57 +0100

    backends/drm: allow some inaccuracy for matching a 3x3 matrix
    When two matrices cancel each other out, due to floating point math we can still
    have tiny errors in the fourth column of the matrix. These are not relevant for
    the end result and need to be ignored for direct scanout to work.

--- src/backends/drm/drm_colorop.cpp
+++ src/backends/drm/drm_colorop.cpp
@@ -183,13 +183,13 @@ bool LegacyMatrixColorOp::canBeUsedFor(const ColorOp &op)
 {
     // this isn't necessarily true, but let's keep things simple for now
     if (auto matrix = std::get_if<ColorMatrix>(&op.operation)) {
-        return matrix->mat(3, 0) == 0
-            && matrix->mat(3, 1) == 0
-            && matrix->mat(3, 2) == 0
-            && matrix->mat(3, 3) == 1
-            && matrix->mat(0, 3) == 0
-            && matrix->mat(1, 3) == 0
-            && matrix->mat(2, 3) == 0;
+        return std::abs(matrix->mat(3, 0) - 0) < ColorPipeline::s_maxResolution
+            && std::abs(matrix->mat(3, 1) - 0) < ColorPipeline::s_maxResolution
+            && std::abs(matrix->mat(3, 2) - 0) < ColorPipeline::s_maxResolution
+            && std::abs(matrix->mat(3, 3) - 1) < ColorPipeline::s_maxResolution
+            && std::abs(matrix->mat(0, 3) - 0) < ColorPipeline::s_maxResolution
+            && std::abs(matrix->mat(1, 3) - 0) < ColorPipeline::s_maxResolution
+            && std::abs(matrix->mat(2, 3) - 0) < ColorPipeline::s_maxResolution;
     } else if (std::holds_alternative<ColorMultiplier>(op.operation)) {
         return true;
     }
