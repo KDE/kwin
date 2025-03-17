210c56ca2f1468e38a49aa2cf0df595c170a92b9
Xaver Hugl <xaver.hugl@kde.org>
2025-03-18 00:06:39 +0100

    input: always allow passing focus to popup children or parent windows
    Otherwise, keeping the mouse button pressed after opening a popup won't transfer
    pointer focus over to it. This change makes KWin behave like other compositors.
    
    BUG: 477740

--- src/input.cpp
+++ src/input.cpp
@@ -3642,6 +3642,18 @@ void InputDeviceHandler::updateDecoration()
     setDecoration(decoration);
 }
 
+static bool isPopupParentOf(Window *maybePopup, Window *maybeParent)
+{
+    if (!maybePopup || !maybeParent) {
+        return false;
+    }
+    if (!maybePopup->isPopupWindow()) {
+        return false;
+    }
+    Window *const parent = maybePopup->transientFor();
+    return parent == maybeParent || (parent && isPopupParentOf(parent, maybeParent));
+}
+
 void InputDeviceHandler::update()
 {
     if (!m_inited) {
@@ -3655,7 +3667,8 @@ void InputDeviceHandler::update()
     // Always set the window at the position of the input device.
     setHover(window);
 
-    if (focusUpdatesBlocked()) {
+    const bool popupRelationship = isPopupParentOf(window, m_focus.window) || isPopupParentOf(m_focus.window, window);
+    if (focusUpdatesBlocked() && !popupRelationship) {
         workspace()->updateFocusMousePosition(position());
         return;
     }
