josephcrowell/kwin-x11-improved

wayland: add support for the xdg toplevel tag protocol
It allows applications to tag their windows with a name, like "main window",
"email composer", "pip video" or similar, which the compositor can use for
window rules or restoring state of application windows without xdg session
management support.

src/wayland/xdgshell.cpp
@@ -577,6 +577,11 @@ QString XdgToplevelInterface::windowClass() const
     return d->windowClass;
 }
 
+QString XdgToplevelInterface::tag() const
+{
+    return d->tag;
+}
+
 QSize XdgToplevelInterface::minimumSize() const
 {
     return d->minimumSize;

src/wayland/xdgshell_p.h
@@ -148,6 +148,7 @@ public:
     XdgSurfaceInterface *xdgSurface;
     QString windowTitle;
     QString windowClass;
+    QString tag;
     QSize minimumSize = QSize(0, 0);
     QSize maximumSize = QSize(0, 0);
 

KDE/kwin

src/wayland/xdgshell.cpp
@@ -578,6 +578,16 @@ QString XdgToplevelInterface::windowClass() const
     return d->windowClass;
 }
 
+QString XdgToplevelInterface::windowTag() const
+{
+    return d->windowTag;
+}
+
+QString XdgToplevelInterface::windowDescription() const
+{
+    return d->windowDescription;
+}
+
 QSize XdgToplevelInterface::minimumSize() const
 {
     return d->minimumSize;

src/wayland/xdgshell_p.h
@@ -150,6 +150,8 @@ public:
     XdgSurfaceInterface *xdgSurface;
     QString windowTitle;
     QString windowClass;
+    QString windowTag;
+    QString windowDescription;
     QSize minimumSize = QSize(0, 0);
     QSize maximumSize = QSize(0, 0);
     QIcon customIcon; // managed externally by the xdg_toplevel_icon interface