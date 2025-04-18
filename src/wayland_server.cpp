josephcrowell/kwin-x11-improved

wayland: add support for the xdg toplevel tag protocol
It allows applications to tag their windows with a name, like "main window",
"email composer", "pip video" or similar, which the compositor can use for
window rules or restoring state of application windows without xdg session
management support.

src/wayland_server.cpp
@@ -69,6 +69,7 @@
 #include "wayland/subcompositor.h"
 #include "wayland/tablet_v2.h"
 #include "wayland/tearingcontrol_v1.h"
+#include "wayland/toplevel_tag_v1.h"
 #include "wayland/viewporter.h"
 #include "wayland/xdgactivation_v1.h"
 #include "wayland/xdgdecoration_v1.h"
@@ -525,6 +526,7 @@ bool WaylandServer::init()
 
     m_externalBrightness = new ExternalBrightnessV1(m_display, m_display);
     m_alphaModifierManager = new AlphaModifierManagerV1(m_display, m_display);
+    m_toplevelTag = new ToplevelTagManagerV1(m_display, m_display);
     return true;
 }
 

KDE/kwin

src/wayland_server.cpp
@@ -85,7 +85,7 @@
 #include "wayland/xdgshell.h"
 #include "wayland/xdgtopleveldrag_v1.h"
 #include "wayland/xdgtoplevelicon_v1.h"
-
+#include "wayland/xdgtopleveltag_v1.h"
 #include "workspace.h"
 #include "xdgactivationv1.h"
 #include "xdgshellintegration.h"
@@ -525,6 +525,7 @@ bool WaylandServer::init()
 #endif
     m_fifoManager = new FifoManagerV1(m_display, m_display);
     m_singlePixelBuffer = new SinglePixelBufferManagerV1(m_display, m_display);
+    m_toplevelTag = new XdgToplevelTagManagerV1(m_display, m_display);
     return true;
 }
 