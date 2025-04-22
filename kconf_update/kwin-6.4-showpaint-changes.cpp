60bbc46dc85a4020c08af267376f8fca46d28b20
Oliver Beard <olib141@outlook.com>
2025-04-22 21:52:02 +0100

    kconf_upgrade: Add upgrade for showpaint effect changes We removed activating the showpaint effect — it is always running when loaded.
    Previously, when loaded, it was initially inactive until toggled, so now we must disable it or it'll start appearing without being invoked.

---
+++ kconf_update/kwin-6.4-showpaint-changes.cpp
@@ -0,0 +1,41 @@
+/*
+    SPDX-FileCopyrightText: 2025 Oliver Beard <olib141@outlook.com>
+
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+
+#include <QAction>
+#include <QGuiApplication>
+
+#include <KConfigGroup>
+#include <KGlobalAccel>
+#include <KSharedConfig>
+
+int main(int argc, char **argv)
+{
+    QGuiApplication app(argc, argv);
+
+    // We removed activating the showpaint effect — it is always running when
+    // loaded. Previously, when loaded, it was initially inactive until toggled,
+    // so now we must disable it or it'll start appearing without being invoked.
+
+    // Remove old toggle action shortcut (that activates the effect)
+    QAction toggleAction;
+    toggleAction.setObjectName(QStringLiteral("Toggle"));
+    toggleAction.setProperty("componentName", QStringLiteral("kwin"));
+    toggleAction.setProperty("componentDisplayName", QStringLiteral("KWin"));
+
+    KGlobalAccel::self()->setShortcut(&toggleAction, {QKeySequence()}, KGlobalAccel::NoAutoloading);
+    KGlobalAccel::self()->removeAllShortcuts(&toggleAction);
+
+    // Disable effect
+    auto config = KSharedConfig::openConfig(QStringLiteral("kwinrc"));
+    KConfigGroup pluginsGroup = config->group(QStringLiteral("Plugins"));
+
+    if (pluginsGroup.exists() && pluginsGroup.hasKey(QStringLiteral("showpaintEnabled"))) {
+        pluginsGroup.deleteEntry(QStringLiteral("showpaintEnabled"));
+        pluginsGroup.sync();
+    }
+
+    return EXIT_SUCCESS;
+}
