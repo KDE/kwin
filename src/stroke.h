b18ae81513253c14e7c4621aa4db1b2ba86ad952
Jakob Petsovits <jpetso@petsovits.com>
2025-05-19 17:24:19 +0200

    stroke: StrokeGesture and StrokeGestures are now our C++ wrappers
    The original EasyStroke C code, and its stroke_t struct,
    remains hidden away in the implementation file.
    
    StrokeGesture, with a triggered() signal, replaces Stroke as the
    primary C++ wrapper class. Stroke is now only a typedef for an
    opaque smart pointer type of the C struct, and is only necessary
    in the API for comparison with StrokeGesture objects.

--- src/stroke.h
+++ src/stroke.h
@@ -1,26 +1,24 @@
 /*
- * Copyright (c) 2009, Thomas Jaeger <ThJaeger@gmail.com>
- * Copyright (c) 2023, Daniel Kondor <kondor.dani@gmail.com>
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
- * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-#ifndef __STROKE_H__
-#define __STROKE_H__
+    KWin - the KDE window manager
+    This file is part of the KDE project.
+
+    SPDX-FileCopyrightText: 2009 Thomas Jaeger <ThJaeger@gmail.com>
+    SPDX-FileCopyrightText: 2023 Daniel Kondor <kondor.dani@gmail.com>
+    SPDX-FileCopyrightText: 2025 Jakob Petsovits <jpetso@petsovits.com>
+
+    SPDX-License-Identifier: MIT
+*/
+
+#pragma once
 
 #include <QList>
+#include <QObject>
 #include <QPointF>
 
 #include <memory> // std::unique_ptr
+
+namespace KWin
+{
 
 struct stroke_t;
 
@@ -29,28 +27,58 @@ struct stroke_deleter
     void operator()(stroke_t *s) const;
 };
 
-class Stroke
+using Stroke = std::unique_ptr<stroke_t, stroke_deleter>;
+
+// not derived from Gesture because we can't offer started() & cancelled() on a per-stroke basis
+class StrokeGesture : public QObject
 {
+    Q_OBJECT
+
 public:
-    std::unique_ptr<stroke_t, stroke_deleter> stroke;
+    explicit StrokeGesture(const QList<QPointF> &points, QObject *parent = nullptr);
+    ~StrokeGesture() override;
 
-    Stroke()
-        : stroke(nullptr, stroke_deleter())
-    {
-    }
-    Stroke(const QList<QPointF> &s);
-    Stroke clone() const;
+    StrokeGesture(const StrokeGesture &) = delete;
+    StrokeGesture &operator=(const StrokeGesture &) = delete;
 
-    static bool compare(const Stroke &, const Stroke &, double &score_out);
-    static double min_matching_score();
+    bool operator==(const StrokeGesture &) const;
 
-    unsigned int size() const;
-    bool trivial() const
-    {
-        return size() == 0;
-    }
-    QPointF pointAt(int n) const;
+    const QList<QPointF> &points() const;
     double time(int n) const;
+
+    static Stroke make_stroke(const QList<QPointF> &points);
+    static double min_matching_score();
+    bool compare(const Stroke &drawn, double &score_out) const;
+
+Q_SIGNALS:
+    /**
+     * Gesture matching ended and this Gesture matched.
+     */
+    void triggered();
+
+private:
+    QList<QPointF> m_points;
+    Stroke m_stroke;
 };
 
-#endif
+class StrokeGestures : public QObject
+{
+    Q_OBJECT
+
+public:
+    StrokeGestures();
+    ~StrokeGestures() override;
+
+    bool isEmpty() const;
+    const QList<StrokeGesture *> &list() const;
+
+    void add(StrokeGesture *gesture);
+    void remove(StrokeGesture *gesture);
+
+    StrokeGesture *bestMatch(const QList<QPointF> &points, double &bestScore) const;
+
+private:
+    QList<StrokeGesture *> m_gestures;
+};
+
+} // namespace KWin
