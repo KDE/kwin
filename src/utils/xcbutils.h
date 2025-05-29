7ac5150939934225710478afd006f6390ab02284
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-05-29 11:30:26 +0000

    xwayland: use output names to find the primary output
    Instead of geometry, which isn't necessarily as reliable
    
    
    (cherry picked from commit 42aa42af5311a968c746175af983bfefaff5b54d)
    
    Co-authored-by: Xaver Hugl <xaver.hugl@kde.org>

--- src/utils/xcbutils.h
+++ src/utils/xcbutils.h
@@ -1267,6 +1267,13 @@ public:
         }
         return xcb_randr_get_screen_resources_names(data());
     }
+    inline xcb_randr_output_t *outputs()
+    {
+        if (isNull()) {
+            return nullptr;
+        }
+        return xcb_randr_get_screen_resources_outputs(data());
+    }
 };
 
 XCB_WRAPPER_DATA(CrtcGammaData, xcb_randr_get_crtc_gamma, xcb_randr_crtc_t)
