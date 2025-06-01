248f18ff2e163eb4777a60f24b0ca38db083e575
Jakob Petsovits <jpetso@petsovits.com>
2025-06-01 15:11:01 +0200

    plugins/strokegestures: Add a stroke visualization effect
    For the time being, it simply draws a line while you press down
    the activation button (and the gesture recognizer has marked the
    stroke gesture as "started").

---
+++ src/plugins/strokegestures/main.cpp
@@ -0,0 +1,17 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "strokeeffect.h"
+
+namespace KWin
+{
+
+KWIN_EFFECT_FACTORY(StrokeEffect,
+                    "metadata.json.stripped")
+
+} // namespace KWin
+
+#include "main.moc"
