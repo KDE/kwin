ccc5a285873882a77529c6a534a56c4a873e2e22
Liu Jie <liujie01@kylinos.cn>
2025-07-22 19:28:28 +0800

    scene/borderradius: remove #pragma once

--- src/scene/borderradius.cpp
+++ src/scene/borderradius.cpp
@@ -4,8 +4,6 @@
     SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
 
-#pragma once
-
 #include "scene/borderradius.h"
 
 #include <KDecoration3/Decoration>
