f9b74a0fb8333e4752036485cf047f79eed59307
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-07-30 17:03:39 +0300

    kcms/options: Remove standalone kcm leftovers
    Standalone KCMs are gone.

--- src/kcms/options/mouse.cpp
+++ src/kcms/options/mouse.cpp
@@ -8,8 +8,6 @@
 #include "kwinoptions_settings.h"
 
 #include <KWindowSystem>
-#include <QDBusConnection>
-#include <QDBusMessage>
 
 KWinMouseConfigForm::KWinMouseConfigForm(QWidget *parent)
     : QWidget(parent)
@@ -23,9 +21,8 @@ KWinActionsConfigForm::KWinActionsConfigForm(QWidget *parent)
     setupUi(parent);
 }
 
-KTitleBarActionsConfig::KTitleBarActionsConfig(bool _standAlone, KWinOptionsSettings *settings, QWidget *parent)
+KTitleBarActionsConfig::KTitleBarActionsConfig(KWinOptionsSettings *settings, QWidget *parent)
     : KCModule(parent, KPluginMetaData())
-    , standAlone(_standAlone)
     , m_ui(new KWinMouseConfigForm(widget()))
 {
     if (settings) {
@@ -39,21 +36,8 @@ void KTitleBarActionsConfig::initialize(KWinOptionsSettings *settings)
     addConfig(m_settings, widget());
 }
 
-void KTitleBarActionsConfig::save()
-{
-    KCModule::save();
-
-    if (standAlone) {
-        // Send signal to all kwin instances
-        QDBusMessage message =
-            QDBusMessage::createSignal("/KWin", "org.kde.KWin", "reloadConfig");
-        QDBusConnection::sessionBus().send(message);
-    }
-}
-
-KWindowActionsConfig::KWindowActionsConfig(bool _standAlone, KWinOptionsSettings *settings, QWidget *parent)
+KWindowActionsConfig::KWindowActionsConfig(KWinOptionsSettings *settings, QWidget *parent)
     : KCModule(parent, KPluginMetaData())
-    , standAlone(_standAlone)
     , m_ui(new KWinActionsConfigForm(widget()))
 {
     if (settings) {
@@ -66,18 +50,6 @@ void KWindowActionsConfig::initialize(KWinOptionsSettings *settings)
     m_settings = settings;
     addConfig(m_settings, widget());
     m_ui->info_1->setVisible(KWindowSystem::isPlatformX11());
-}
-
-void KWindowActionsConfig::save()
-{
-    KCModule::save();
-
-    if (standAlone) {
-        // Send signal to all kwin instances
-        QDBusMessage message =
-            QDBusMessage::createSignal("/KWin", "org.kde.KWin", "reloadConfig");
-        QDBusConnection::sessionBus().send(message);
-    }
 }
 
 #include "moc_mouse.cpp"
