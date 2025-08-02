274eef577664c43fc3320f559ca0ec2bf2fb3f35
Joshua Goins <josh@redstrate.com>
2025-08-02 05:46:29 -0400

    backends/libinput: Increase minimum version to 1.28
    This removes some input area code that was gated on 1.27. Ideally we
    want 1.29, but that's unlikely to happen before 6.5.

--- src/backends/libinput/device.cpp
+++ src/backends/libinput/device.cpp
@@ -435,7 +435,6 @@ Device::Device(libinput_device *device, QObject *parent)
     }
 
     if (supportsInputArea() && m_inputArea != defaultInputArea()) {
-#if HAVE_LIBINPUT_INPUT_AREA
         const libinput_config_area_rectangle rect{
             .x1 = m_inputArea.topLeft().x(),
             .y1 = m_inputArea.topLeft().y(),
@@ -443,7 +442,6 @@ Device::Device(libinput_device *device, QObject *parent)
             .y2 = m_inputArea.bottomRight().y(),
         };
         libinput_device_config_area_set_rectangle(m_device, &rect);
-#endif
     }
 
     libinput_device_group *group = libinput_device_get_device_group(device);
@@ -1073,11 +1071,7 @@ double Device::defaultPressureRangeMax() const
 
 bool Device::supportsInputArea() const
 {
-#if HAVE_LIBINPUT_INPUT_AREA
-    return true;
-#else
-    return false;
-#endif
+    return libinput_device_config_area_has_rectangle(m_device);
 }
 
 QRectF Device::inputArea() const
@@ -1090,7 +1084,6 @@ void Device::setInputArea(const QRectF &inputArea)
     if (m_inputArea != inputArea) {
         m_inputArea = inputArea;
 
-#if HAVE_LIBINPUT_INPUT_AREA
         const libinput_config_area_rectangle rect{
             .x1 = m_inputArea.topLeft().x(),
             .y1 = m_inputArea.topLeft().y(),
@@ -1098,7 +1091,6 @@ void Device::setInputArea(const QRectF &inputArea)
             .y2 = m_inputArea.bottomRight().y(),
         };
         libinput_device_config_area_set_rectangle(m_device, &rect);
-#endif
 
         writeEntry(ConfigKey::InputArea, m_inputArea);
         Q_EMIT inputAreaChanged();
