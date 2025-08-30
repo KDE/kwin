fd75e3aba30918191f9e885664ba52999868c0c5
Xaver Hugl <xaver.hugl@kde.org>
2025-08-30 23:40:25 +0200

    backends/wayland: prevent crash when previous color isn't set
    There's an operator== for the shared pointers, there's no need to dereference
    anything.

--- src/backends/wayland/wayland_layer.cpp
+++ src/backends/wayland/wayland_layer.cpp
@@ -106,7 +106,7 @@ void WaylandLayer::commit(PresentationMode presentationMode)
         wp_viewport_set_destination(m_viewport, logicalTarget.width(), logicalTarget.height());
     }
     m_subSurface->setPosition(logicalTarget.topLeft());
-    if (m_colorSurface && *m_color != *m_previousColor) {
+    if (m_colorSurface && m_color != m_previousColor) {
         const auto imageDescription = static_cast<WaylandOutput *>(m_output.get())->backend()->display()->colorManager()->createImageDescription(*m_color);
         wp_color_management_surface_v1_set_image_description(m_colorSurface, imageDescription, WP_COLOR_MANAGER_V1_RENDER_INTENT_PERCEPTUAL);
         wp_image_description_v1_destroy(imageDescription);
