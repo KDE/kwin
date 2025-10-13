3f8fcda16805ffebeacd2c7eb794c9ef80f9b95c
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-10-13 09:27:01 +0300

    x11: Restore "kept" window state
    Effectively reverts c8efb221b361a0100e93797383ffa51e963d0166.
    
    Video games are susceptible to the kept window state. If a video game is
    put behind other windows, it will eventually break and it may not recover
    from the broken state after it is made visible. The likely reason why
    video games break is due to frame callback starvation.
    
    c8efb221b361a0100e93797383ffa51e963d0166 attempted to address that issue
    by unmapping the windows. However, it made X11 windows flash when
    switching between virtual desktops or activities using a touchpad.
    When a window is mapped, Xwayland will immediately destroy and create a
    new wl_surface.
    
    CCBUG: 504321

--- src/layers.cpp
+++ src/layers.cpp
@@ -100,7 +100,8 @@ void Workspace::updateStackingOrder(bool propagate_new_windows)
         return;
     }
     QList<Window *> new_stacking_order = constrainedStackingOrder();
-    bool changed = new_stacking_order != stacking_order;
+    bool changed = (force_restacking || new_stacking_order != stacking_order);
+    force_restacking = false;
     stacking_order = new_stacking_order;
     if (changed || propagate_new_windows) {
 #if KWIN_BUILD_X11
@@ -143,10 +144,23 @@ void Workspace::propagateWindows(bool propagate_new_windows)
 
     for (int i = stacking_order.size() - 1; i >= 0; --i) {
         X11Window *window = qobject_cast<X11Window *>(stacking_order.at(i));
-        if (!window || window->isDeleted() || window->isUnmanaged()) {
+        if (!window || window->isDeleted() || window->isUnmanaged() || window->hiddenPreview()) {
             continue;
         }
 
+        newWindowStack << window->window();
+    }
+
+    newWindowStack << *m_guardWindow;
+
+    // when having hidden previews, stack hidden windows below everything else
+    // (as far as pure X stacking order is concerned), in order to avoid having
+    // these windows that should be unmapped to interfere with other windows
+    for (int i = stacking_order.size() - 1; i >= 0; --i) {
+        X11Window *window = qobject_cast<X11Window *>(stacking_order.at(i));
+        if (!window || window->isDeleted() || window->isUnmanaged() || !window->hiddenPreview()) {
+            continue;
+        }
         newWindowStack << window->window();
     }
 
