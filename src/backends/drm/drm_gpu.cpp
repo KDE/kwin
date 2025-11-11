07c6c5eef3b9723ef8c39f895ca5f44457602f27
Xaver Hugl <xaver.hugl@kde.org>
2025-11-11 14:45:43 +0100

    backends/drm: remove some debug prints
    They haven't been practically useful so far, and they spam the log quite a lot.
    This will become especially bad with automatic brightness adjustments, which
    will apply new output configs all the time.

--- src/backends/drm/drm_gpu.cpp
+++ src/backends/drm/drm_gpu.cpp
@@ -331,10 +331,8 @@ void DrmGpu::removeOutputs()
 
 DrmPipeline::Error DrmGpu::checkCrtcAssignment(QList<DrmConnector *> connectors, const QList<DrmCrtc *> &crtcs)
 {
-    qCDebug(KWIN_DRM) << "Attempting to match" << connectors << "with" << crtcs;
     if (connectors.isEmpty()) {
         const auto result = testPipelines();
-        qCDebug(KWIN_DRM) << "Testing CRTC assignment..." << (result == DrmPipeline::Error::None ? "passed" : "failed");
         return result;
     }
     auto connector = connectors.takeFirst();
@@ -347,12 +345,10 @@ DrmPipeline::Error DrmGpu::checkCrtcAssignment(QList<DrmConnector *> connectors,
     if (!pipeline->enabled() || !connector->isConnected()) {
         // disabled pipelines don't need CRTCs
         pipeline->setCrtc(nullptr);
-        qCDebug(KWIN_DRM) << "Unassigning CRTC from connector" << connector->id();
         return checkCrtcAssignment(connectors, crtcs);
     }
     if (crtcs.isEmpty()) {
         // we have no crtc left to drive this connector
-        qCWarning(KWIN_DRM) << "No matching CRTC for connector" << connector->id();
         return DrmPipeline::Error::NotEnoughCrtcs;
     }
     DrmCrtc *currentCrtc = nullptr;
@@ -367,7 +363,6 @@ DrmPipeline::Error DrmGpu::checkCrtcAssignment(QList<DrmConnector *> connectors,
             auto crtcsLeft = crtcs;
             crtcsLeft.removeOne(currentCrtc);
             pipeline->setCrtc(currentCrtc);
-            qCDebug(KWIN_DRM) << "Assigning CRTC" << currentCrtc->id() << "to connector" << connector->id();
             DrmPipeline::Error err = checkCrtcAssignment(connectors, crtcsLeft);
             if (err == DrmPipeline::Error::None || err == DrmPipeline::Error::NoPermission || err == DrmPipeline::Error::FramePending) {
                 return err;
@@ -379,7 +374,6 @@ DrmPipeline::Error DrmGpu::checkCrtcAssignment(QList<DrmConnector *> connectors,
             auto crtcsLeft = crtcs;
             crtcsLeft.removeOne(crtc);
             pipeline->setCrtc(crtc);
-            qCDebug(KWIN_DRM) << "Assigning CRTC" << crtc->id() << "to connector" << connector->id();
             DrmPipeline::Error err = checkCrtcAssignment(connectors, crtcsLeft);
             if (err == DrmPipeline::Error::None || err == DrmPipeline::Error::NoPermission || err == DrmPipeline::Error::FramePending) {
                 return err;
