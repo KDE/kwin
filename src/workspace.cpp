ede91a8f96467a8c3f0d872514d0499f562a029c
Xaver Hugl <xaver.hugl@kde.org>
2025-11-26 21:15:47 +0100

    add some temporary debug logs

--- src/workspace.cpp
+++ src/workspace.cpp
@@ -1249,6 +1249,7 @@ void Workspace::updateOutputs()
                 && output->tileInfo()->groupId == groupId
                 && output->tileInfo()->tileLocation == QPoint(0, 0);
         });
+        qWarning() << "Tile group" << groupId << "is complete, with a size of" << info.second;
     }
 
     for (BackendOutput *output : availableOutputs) {
