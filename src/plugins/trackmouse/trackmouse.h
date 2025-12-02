637410bacbe2a261f0e82efd846352d14612467d
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-12-02 10:41:53 +0200

    plugins/trackmouse: Fix stuttering
    With the output layer changes in 6.5, repaints for the next frame can't
    be scheduled in the pre paint stage anymore.
    
    On the other hand, the track mouse effect relied on that behavior. As a
    short term solution, this change moves the rotation transform update
    step to the post paint stage.
    
    BUG: 512767

--- src/plugins/trackmouse/trackmouse.h
+++ src/plugins/trackmouse/trackmouse.h
@@ -41,7 +41,7 @@ public:
     TrackMouseEffect();
     ~TrackMouseEffect() override;
 
-    void prePaintScreen(ScreenPrePaintData &data, std::chrono::milliseconds presentTime) override;
+    void postPaintScreen() override;
     void reconfigure(ReconfigureFlags) override;
     bool isActive() const override;
 
