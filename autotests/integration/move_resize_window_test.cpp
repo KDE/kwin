josephcrowell/kwin-x11-improved

window: respect minSize and maxSize in confineInteractiveResize

autotests/integration/move_resize_window_test.cpp
@@ -68,6 +68,8 @@ private Q_SLOTS:
     void testRestrictedMoveMultiMonitor();
     void testRestrictedResizeUp();
     void testRestrictedResizeRight();
+    void testRestrictedResizeMinSize();
+    void testRestrictedResizeMaxSize();
 
 private:
     std::tuple<Window *, std::unique_ptr<KWayland::Client::Surface>, std::unique_ptr<Test::XdgToplevel>> showWindow();
@@ -1253,6 +1255,158 @@ void MoveResizeWindowTest::testRestrictedResizeRight()
     Test::render(surface.get(), toplevelSize, Qt::blue);
     QVERIFY(frameGeometryChangedSpy.wait());
     QCOMPARE(window->frameGeometry().topRight(), QPoint(1060, 50));
+
+    // let's end
+    surface.reset();
+    QVERIFY(Test::waitForWindowClosed(window));
+}
+
+void MoveResizeWindowTest::testRestrictedResizeMinSize()
+{
+    // tests minSize is respected during restrictedResize
+
+    std::vector<std::unique_ptr<KWayland::Client::Surface>> surfaces;
+    std::vector<std::unique_ptr<Test::LayerSurfaceV1>> shellSurfaces;
+    const auto add = [this, &surfaces, &shellSurfaces](const QRect &geometry, int anchor) {
+        auto [surface, shellSurface] = addPanel(geometry, anchor);
+        QVERIFY(surface);
+        QVERIFY(shellSurface);
+        surfaces.push_back(std::move(surface));
+        shellSurfaces.push_back(std::move(shellSurface));
+    };
+    add(QRect(1080, 256, 200, 512), Test::LayerSurfaceV1::anchor_right);
+
+    auto [window, surface, shellSurface] = showWindow();
+    QVERIFY(window);
+    QVERIFY(window->isDecorated());
+    QVERIFY(!window->noBorder());
+    QCOMPARE(window->titlebarPosition(), Qt::TopEdge);
+    QVERIFY(surface);
+
+    shellSurface->set_min_size(200, 300);
+
+    QCOMPARE(workspace()->activeWindow(), window);
+    QRectF decorationLeft, decorationRight, decorationTop, decorationBottom;
+    window->layoutDecorationRects(decorationLeft, decorationTop, decorationRight, decorationBottom);
+    QVERIFY(!decorationTop.isEmpty());
+
+    QSignalSpy frameGeometryChangedSpy(window, &Window::frameGeometryChanged);
+    QSignalSpy toplevelConfigureRequestedSpy(shellSurface.get(), &Test::XdgToplevel::configureRequested);
+    QSignalSpy surfaceConfigureRequestedSpy(shellSurface->xdgSurface(), &Test::XdgSurface::configureRequested);
+    QSignalSpy interactiveMoveResizeStartedSpy(window, &Window::interactiveMoveResizeStarted);
+
+    quint32 timestamp = 1;
+
+    // strut denoted by "*", border by "|" and "-"
+    // --------------------
+    // |                  *
+    // |                  *
+    // --------------------
+
+    // test minSize restriction
+    window->move(QPoint(970, 500));
+    window->resize(QSize(300, 500));
+    QVERIFY(surfaceConfigureRequestedSpy.wait());
+    QCOMPARE(surfaceConfigureRequestedSpy.count(), 2);
+    QCOMPARE(toplevelConfigureRequestedSpy.count(), 2);
+    QSize toplevelSize = toplevelConfigureRequestedSpy.last().at(0).toSize();
+    shellSurface->xdgSurface()->ack_configure(surfaceConfigureRequestedSpy.last().at(0).value<quint32>());
+    Test::render(surface.get(), toplevelSize, Qt::blue);
+    QVERIFY(frameGeometryChangedSpy.wait());
+    QCOMPARE(window->frameGeometry(), QRectF(970, 500, 300, 500));
+
+    MOTION(QPoint(window->frameGeometry().left(), window->frameGeometry().top()));
+    PRESS;
+    QVERIFY(!window->isInteractiveResize());
+    QVERIFY(interactiveMoveResizeStartedSpy.wait());
+    QVERIFY(window->isInteractiveResize());
+    MOTION(QPoint(window->frameGeometry().left() + 100, 768));
+    RELEASE;
+    QVERIFY(surfaceConfigureRequestedSpy.wait());
+    QCOMPARE(surfaceConfigureRequestedSpy.count(), 3);
+    QCOMPARE(toplevelConfigureRequestedSpy.count(), 3);
+    toplevelSize = toplevelConfigureRequestedSpy.last().at(0).toSize();
+    shellSurface->xdgSurface()->ack_configure(surfaceConfigureRequestedSpy.last().at(0).value<quint32>());
+    Test::render(surface.get(), toplevelSize, Qt::blue);
+    QVERIFY(frameGeometryChangedSpy.wait());
+    QCOMPARE(window->frameGeometry(), QRectF(980, 700, 290, 300));
+
+    // let's end
+    surface.reset();
+    QVERIFY(Test::waitForWindowClosed(window));
+}
+
+void MoveResizeWindowTest::testRestrictedResizeMaxSize()
+{
+    // tests minSize is respected during restrictedResize
+
+    std::vector<std::unique_ptr<KWayland::Client::Surface>> surfaces;
+    std::vector<std::unique_ptr<Test::LayerSurfaceV1>> shellSurfaces;
+    const auto add = [this, &surfaces, &shellSurfaces](const QRect &geometry, int anchor) {
+        auto [surface, shellSurface] = addPanel(geometry, anchor);
+        QVERIFY(surface);
+        QVERIFY(shellSurface);
+        surfaces.push_back(std::move(surface));
+        shellSurfaces.push_back(std::move(shellSurface));
+    };
+    add(QRect(1080, 256, 200, 512), Test::LayerSurfaceV1::anchor_right);
+
+    auto [window, surface, shellSurface] = showWindow();
+    QVERIFY(window);
+    QVERIFY(window->isDecorated());
+    QVERIFY(!window->noBorder());
+    QCOMPARE(window->titlebarPosition(), Qt::TopEdge);
+    QVERIFY(surface);
+
+    shellSurface->set_min_size(300, 400);
+    shellSurface->set_max_size(300, 500);
+
+    QCOMPARE(workspace()->activeWindow(), window);
+    QRectF decorationLeft, decorationRight, decorationTop, decorationBottom;
+    window->layoutDecorationRects(decorationLeft, decorationTop, decorationRight, decorationBottom);
+    QVERIFY(!decorationTop.isEmpty());
+
+    QSignalSpy frameGeometryChangedSpy(window, &Window::frameGeometryChanged);
+    QSignalSpy toplevelConfigureRequestedSpy(shellSurface.get(), &Test::XdgToplevel::configureRequested);
+    QSignalSpy surfaceConfigureRequestedSpy(shellSurface->xdgSurface(), &Test::XdgSurface::configureRequested);
+    QSignalSpy interactiveMoveResizeStartedSpy(window, &Window::interactiveMoveResizeStarted);
+
+    quint32 timestamp = 1;
+
+    // strut denoted by "*", border by "|" and "-"
+    // --------------------
+    // |                  *
+    // |                  *
+    // --------------------
+
+    // test minSize width = maxSize width
+
+    window->move(QPoint(970, 500));
+    window->resize(QSize(300, 500));
+    QVERIFY(surfaceConfigureRequestedSpy.wait());
+    QCOMPARE(surfaceConfigureRequestedSpy.count(), 2);
+    QCOMPARE(toplevelConfigureRequestedSpy.count(), 2);
+    QSize toplevelSize = toplevelConfigureRequestedSpy.last().at(0).toSize();
+    shellSurface->xdgSurface()->ack_configure(surfaceConfigureRequestedSpy.last().at(0).value<quint32>());
+    Test::render(surface.get(), toplevelSize, Qt::blue);
+    QVERIFY(frameGeometryChangedSpy.wait());
+    QCOMPARE(window->frameGeometry(), QRectF(970, 500, 300, 500));
+
+    MOTION(QPoint(window->frameGeometry().left(), window->frameGeometry().top()));
+    PRESS;
+    QVERIFY(!window->isInteractiveResize());
+    QVERIFY(interactiveMoveResizeStartedSpy.wait());
+    QVERIFY(window->isInteractiveResize());
+    MOTION(QPoint(window->frameGeometry().left() - 100, 768));
+    RELEASE;
+    QVERIFY(surfaceConfigureRequestedSpy.wait());
+    QCOMPARE(surfaceConfigureRequestedSpy.count(), 3);
+    QCOMPARE(toplevelConfigureRequestedSpy.count(), 3);
+    toplevelSize = toplevelConfigureRequestedSpy.last().at(0).toSize();
+    shellSurface->xdgSurface()->ack_configure(surfaceConfigureRequestedSpy.last().at(0).value<quint32>());
+    Test::render(surface.get(), toplevelSize, Qt::blue);
+    QVERIFY(frameGeometryChangedSpy.wait());
+    QCOMPARE(window->frameGeometry(), QRectF(970, 600, 300, 400));
 
     // let's end
     surface.reset();

src/window.cpp
@@ -1577,7 +1577,8 @@ static std::optional<QPointF> confineInteractiveMove(const QRectF &geometry, int
  *
  * See doc/moveresizerestriction for more details on algorithm.
  */
-static std::optional<QPointF> confineInteractiveResize(const QRectF &geometry, Gravity gravity, int minVisibleWidth, int minVisibleHeight)
+static std::optional<QPointF> confineInteractiveResize(const QRectF &geometry, Gravity gravity, int minVisibleWidth, int minVisibleHeight,
+                                                       const QSizeF &minSize, const QSizeF &maxSize)
 {
     if (gravity == Gravity::Bottom) {
         QPointF candidate = geometry.bottomLeft();
@@ -1639,16 +1640,57 @@ static std::optional<QPointF> confineInteractiveResize(const QRectF &geometry, G
         return QLineF(source, dest).length();
     };
 
+    const auto applyMinSizeMaxSizeConstraints = [gravity, geometry, minSize, maxSize](QRectF rect) {
+        // in applying minSize and maxSize constraints, add slackness of 1 to avoid invalid rectangles when minSize is equal to maxSize
+        switch (gravity) {
+        case Gravity::Top:
+            rect.setBottom(std::min(rect.bottom(), geometry.bottom() - minSize.height()));
+            rect.setTop(std::max(rect.top(), geometry.bottom() - maxSize.height() - 1));
+            break;
+        case Gravity::TopLeft:
+            rect.setRight(std::min(rect.right(), geometry.right() - minSize.width()));
+            rect.setBottom(std::min(rect.bottom(), geometry.bottom() - minSize.height()));
+            rect.setLeft(std::max(rect.left(), geometry.right() - maxSize.width() - 1));
+            rect.setTop(std::max(rect.top(), geometry.bottom() - maxSize.height() - 1));
+            break;
+        case Gravity::TopRight:
+            rect.setLeft(std::max(rect.left(), geometry.left() + minSize.width()));
+            rect.setBottom(std::min(rect.bottom(), geometry.bottom() - minSize.height()));
+            rect.setRight(std::min(rect.right(), geometry.left() + maxSize.width() + 1));
+            rect.setTop(std::max(rect.top(), geometry.bottom() - maxSize.height() - 1));
+            break;
+        case Gravity::Left:
+        case Gravity::BottomLeft:
+            rect.setRight(std::min(rect.right(), geometry.right() - minSize.width()));
+            rect.setLeft(std::max(rect.left(), geometry.right() - maxSize.width() - 1));
+            break;
+        case Gravity::Right:
+        case Gravity::BottomRight:
+            rect.setLeft(std::max(rect.left(), geometry.left() + minSize.width()));
+            rect.setRight(std::min(rect.right(), geometry.left() + maxSize.width() + 1));
+            break;
+        default:
+            Q_UNREACHABLE();
+        }
+        return rect;
+    };
+
     std::optional<QPointF> candidate;
     qreal bestScore;
     const auto availableArea = workspace()->clientArea(FullArea, workspace()->activeOutput(), VirtualDesktopManager::self()->currentDesktop());
 
     switch (gravity) {
+        // QRectF is used because QRect::right() returns left() + width() - 1; QRectF::right() returns left() + width()
     case Gravity::Top:
         // resizing from the top is handled like moving the window to avoid zero width rectangles when window width is equal to minVisibleWidth
-        for (QRect rect : visibleSubrectRegion) {
+        for (QRectF rect : visibleSubrectRegion) {
             // convert top-left of visible titlebar subrect to top-left of the window
             rect.setLeft(rect.left() - geometry.width() + minVisibleWidth);
+
+            rect = applyMinSizeMaxSizeConstraints(rect);
+            if (!rect.isValid()) {
+                continue;
+            }
 
             const QPointF closest = clampPoint(rect, anchor);
             const qreal score = calcDistance(anchor, closest);
@@ -1661,9 +1703,14 @@ static std::optional<QPointF> confineInteractiveResize(const QRectF &geometry, G
     case (Gravity::Left):
     case (Gravity::TopLeft):
     case (Gravity::BottomLeft):
-        for (QRect rect : visibleSubrectRegion) {
+        for (QRectF rect : visibleSubrectRegion) {
             // convert top-left of visible titlebar subrect to top-left of the window
             rect.setLeft(availableArea.left());
+
+            rect = applyMinSizeMaxSizeConstraints(rect);
+            if (!rect.isValid()) {
+                continue;
+            }
 
             const QPointF closest = clampPoint(rect, anchor);
             const qreal score = calcDistance(anchor, closest);
@@ -1676,10 +1723,14 @@ static std::optional<QPointF> confineInteractiveResize(const QRectF &geometry, G
     case (Gravity::Right):
     case (Gravity::TopRight):
     case (Gravity::BottomRight):
-        // QRectF is used because QRect::right() returns left() + width() - 1; QRectF::right() returns left() + width()
         for (QRectF rect : visibleSubrectRegion) {
             // convert top-right of visible titlebar subrect to top-right of the window
             rect.setRight(availableArea.left() + availableArea.width());
+
+            rect = applyMinSizeMaxSizeConstraints(rect);
+            if (!rect.isValid()) {
+                continue;
+            }
 
             const QPointF closest = clampPoint(rect, anchor);
             const qreal score = calcDistance(anchor, closest);
@@ -1756,7 +1807,8 @@ QRectF Window::nextInteractiveResizeGeometry(const QPointF &global) const
     nextMoveResizeGeom = workspace()->adjustWindowSize(this, nextMoveResizeGeom, gravity);
 
     if (!isUnrestrictedInteractiveMoveResize()) {
-        if (const auto anchor = confineInteractiveResize(nextMoveResizeGeom, gravity, 100, titlebarThickness())) {
+        if (const auto anchor = confineInteractiveResize(nextMoveResizeGeom, gravity, 100, titlebarThickness(),
+                                                         nextClientSizeToFrameSize(minSize()), nextClientSizeToFrameSize(maxSize()))) {
             switch (gravity) {
             case Gravity::TopLeft:
                 nextMoveResizeGeom.setTopLeft(*anchor);

KDE/kwin

autotests/integration/move_resize_window_test.cpp
@@ -65,6 +65,8 @@ private Q_SLOTS:
     void testRestrictedMoveMultiMonitor();
     void testRestrictedResizeUp();
     void testRestrictedResizeRight();
+    void testRestrictedResizeMinSize();
+    void testRestrictedResizeMaxSize();
 
 private:
     std::tuple<Window *, std::unique_ptr<KWayland::Client::Surface>, std::unique_ptr<Test::XdgToplevel>> showWindow();
@@ -1250,6 +1252,158 @@ void MoveResizeWindowTest::testRestrictedResizeRight()
     Test::render(surface.get(), toplevelSize, Qt::blue);
     QVERIFY(frameGeometryChangedSpy.wait());
     QCOMPARE(window->frameGeometry().topRight(), QPoint(1060, 50));
+
+    // let's end
+    surface.reset();
+    QVERIFY(Test::waitForWindowClosed(window));
+}
+
+void MoveResizeWindowTest::testRestrictedResizeMinSize()
+{
+    // tests minSize is respected during restrictedResize
+
+    std::vector<std::unique_ptr<KWayland::Client::Surface>> surfaces;
+    std::vector<std::unique_ptr<Test::LayerSurfaceV1>> shellSurfaces;
+    const auto add = [this, &surfaces, &shellSurfaces](const QRect &geometry, int anchor) {
+        auto [surface, shellSurface] = addPanel(geometry, anchor);
+        QVERIFY(surface);
+        QVERIFY(shellSurface);
+        surfaces.push_back(std::move(surface));
+        shellSurfaces.push_back(std::move(shellSurface));
+    };
+    add(QRect(1080, 256, 200, 512), Test::LayerSurfaceV1::anchor_right);
+
+    auto [window, surface, shellSurface] = showWindow();
+    QVERIFY(window);
+    QVERIFY(window->isDecorated());
+    QVERIFY(!window->noBorder());
+    QCOMPARE(window->titlebarPosition(), Qt::TopEdge);
+    QVERIFY(surface);
+
+    shellSurface->set_min_size(200, 300);
+
+    QCOMPARE(workspace()->activeWindow(), window);
+    RectF decorationLeft, decorationRight, decorationTop, decorationBottom;
+    window->layoutDecorationRects(decorationLeft, decorationTop, decorationRight, decorationBottom);
+    QVERIFY(!decorationTop.isEmpty());
+
+    QSignalSpy frameGeometryChangedSpy(window, &Window::frameGeometryChanged);
+    QSignalSpy toplevelConfigureRequestedSpy(shellSurface.get(), &Test::XdgToplevel::configureRequested);
+    QSignalSpy surfaceConfigureRequestedSpy(shellSurface->xdgSurface(), &Test::XdgSurface::configureRequested);
+    QSignalSpy interactiveMoveResizeStartedSpy(window, &Window::interactiveMoveResizeStarted);
+
+    quint32 timestamp = 1;
+
+    // strut denoted by "*", border by "|" and "-"
+    // --------------------
+    // |                  *
+    // |                  *
+    // --------------------
+
+    // test minSize restriction
+    window->move(QPoint(970, 500));
+    window->resize(QSize(300, 500));
+    QVERIFY(surfaceConfigureRequestedSpy.wait());
+    QCOMPARE(surfaceConfigureRequestedSpy.count(), 2);
+    QCOMPARE(toplevelConfigureRequestedSpy.count(), 2);
+    QSize toplevelSize = toplevelConfigureRequestedSpy.last().at(0).toSize();
+    shellSurface->xdgSurface()->ack_configure(surfaceConfigureRequestedSpy.last().at(0).value<quint32>());
+    Test::render(surface.get(), toplevelSize, Qt::blue);
+    QVERIFY(frameGeometryChangedSpy.wait());
+    QCOMPARE(window->frameGeometry(), QRectF(970, 500, 300, 500));
+
+    MOTION(QPoint(window->frameGeometry().left(), window->frameGeometry().top()));
+    PRESS;
+    QVERIFY(!window->isInteractiveResize());
+    QVERIFY(interactiveMoveResizeStartedSpy.wait());
+    QVERIFY(window->isInteractiveResize());
+    MOTION(QPoint(window->frameGeometry().left() + 100, 768));
+    RELEASE;
+    QVERIFY(surfaceConfigureRequestedSpy.wait());
+    QCOMPARE(surfaceConfigureRequestedSpy.count(), 3);
+    QCOMPARE(toplevelConfigureRequestedSpy.count(), 3);
+    toplevelSize = toplevelConfigureRequestedSpy.last().at(0).toSize();
+    shellSurface->xdgSurface()->ack_configure(surfaceConfigureRequestedSpy.last().at(0).value<quint32>());
+    Test::render(surface.get(), toplevelSize, Qt::blue);
+    QVERIFY(frameGeometryChangedSpy.wait());
+    QCOMPARE(window->frameGeometry(), QRectF(980, 700, 290, 300));
+
+    // let's end
+    surface.reset();
+    QVERIFY(Test::waitForWindowClosed(window));
+}
+
+void MoveResizeWindowTest::testRestrictedResizeMaxSize()
+{
+    // tests minSize is respected during restrictedResize
+
+    std::vector<std::unique_ptr<KWayland::Client::Surface>> surfaces;
+    std::vector<std::unique_ptr<Test::LayerSurfaceV1>> shellSurfaces;
+    const auto add = [this, &surfaces, &shellSurfaces](const QRect &geometry, int anchor) {
+        auto [surface, shellSurface] = addPanel(geometry, anchor);
+        QVERIFY(surface);
+        QVERIFY(shellSurface);
+        surfaces.push_back(std::move(surface));
+        shellSurfaces.push_back(std::move(shellSurface));
+    };
+    add(QRect(1080, 256, 200, 512), Test::LayerSurfaceV1::anchor_right);
+
+    auto [window, surface, shellSurface] = showWindow();
+    QVERIFY(window);
+    QVERIFY(window->isDecorated());
+    QVERIFY(!window->noBorder());
+    QCOMPARE(window->titlebarPosition(), Qt::TopEdge);
+    QVERIFY(surface);
+
+    shellSurface->set_min_size(300, 400);
+    shellSurface->set_max_size(300, 500);
+
+    QCOMPARE(workspace()->activeWindow(), window);
+    RectF decorationLeft, decorationRight, decorationTop, decorationBottom;
+    window->layoutDecorationRects(decorationLeft, decorationTop, decorationRight, decorationBottom);
+    QVERIFY(!decorationTop.isEmpty());
+
+    QSignalSpy frameGeometryChangedSpy(window, &Window::frameGeometryChanged);
+    QSignalSpy toplevelConfigureRequestedSpy(shellSurface.get(), &Test::XdgToplevel::configureRequested);
+    QSignalSpy surfaceConfigureRequestedSpy(shellSurface->xdgSurface(), &Test::XdgSurface::configureRequested);
+    QSignalSpy interactiveMoveResizeStartedSpy(window, &Window::interactiveMoveResizeStarted);
+
+    quint32 timestamp = 1;
+
+    // strut denoted by "*", border by "|" and "-"
+    // --------------------
+    // |                  *
+    // |                  *
+    // --------------------
+
+    // test minSize width = maxSize width
+
+    window->move(QPoint(970, 500));
+    window->resize(QSize(300, 500));
+    QVERIFY(surfaceConfigureRequestedSpy.wait());
+    QCOMPARE(surfaceConfigureRequestedSpy.count(), 2);
+    QCOMPARE(toplevelConfigureRequestedSpy.count(), 2);
+    QSize toplevelSize = toplevelConfigureRequestedSpy.last().at(0).toSize();
+    shellSurface->xdgSurface()->ack_configure(surfaceConfigureRequestedSpy.last().at(0).value<quint32>());
+    Test::render(surface.get(), toplevelSize, Qt::blue);
+    QVERIFY(frameGeometryChangedSpy.wait());
+    QCOMPARE(window->frameGeometry(), QRectF(970, 500, 300, 500));
+
+    MOTION(QPoint(window->frameGeometry().left(), window->frameGeometry().top()));
+    PRESS;
+    QVERIFY(!window->isInteractiveResize());
+    QVERIFY(interactiveMoveResizeStartedSpy.wait());
+    QVERIFY(window->isInteractiveResize());
+    MOTION(QPoint(window->frameGeometry().left() - 100, 768));
+    RELEASE;
+    QVERIFY(surfaceConfigureRequestedSpy.wait());
+    QCOMPARE(surfaceConfigureRequestedSpy.count(), 3);
+    QCOMPARE(toplevelConfigureRequestedSpy.count(), 3);
+    toplevelSize = toplevelConfigureRequestedSpy.last().at(0).toSize();
+    shellSurface->xdgSurface()->ack_configure(surfaceConfigureRequestedSpy.last().at(0).value<quint32>());
+    Test::render(surface.get(), toplevelSize, Qt::blue);
+    QVERIFY(frameGeometryChangedSpy.wait());
+    QCOMPARE(window->frameGeometry(), QRectF(970, 600, 300, 400));
 
     // let's end
     surface.reset();

src/window.cpp
@@ -1445,7 +1445,8 @@ static std::optional<QPointF> confineInteractiveMove(const RectF &geometry, int 
  *
  * See doc/moveresizerestriction for more details on algorithm.
  */
-static std::optional<QPointF> confineInteractiveResize(const RectF &geometry, Gravity gravity, int minVisibleWidth, int minVisibleHeight)
+static std::optional<QPointF> confineInteractiveResize(const RectF &geometry, Gravity gravity, int minVisibleWidth, int minVisibleHeight,
+                                                       const QSizeF &minSize, const QSizeF &maxSize)
 {
     if (gravity == Gravity::Bottom) {
         QPointF candidate = geometry.bottomLeft();
@@ -1507,16 +1508,57 @@ static std::optional<QPointF> confineInteractiveResize(const RectF &geometry, Gr
         return QLineF(source, dest).length();
     };
 
+    const auto applyMinSizeMaxSizeConstraints = [gravity, geometry, minSize, maxSize](QRectF rect) {
+        // in applying minSize and maxSize constraints, add slackness of 1 to avoid invalid rectangles when minSize is equal to maxSize
+        switch (gravity) {
+        case Gravity::Top:
+            rect.setBottom(std::min(rect.bottom(), geometry.bottom() - minSize.height()));
+            rect.setTop(std::max(rect.top(), geometry.bottom() - maxSize.height() - 1));
+            break;
+        case Gravity::TopLeft:
+            rect.setRight(std::min(rect.right(), geometry.right() - minSize.width()));
+            rect.setBottom(std::min(rect.bottom(), geometry.bottom() - minSize.height()));
+            rect.setLeft(std::max(rect.left(), geometry.right() - maxSize.width() - 1));
+            rect.setTop(std::max(rect.top(), geometry.bottom() - maxSize.height() - 1));
+            break;
+        case Gravity::TopRight:
+            rect.setLeft(std::max(rect.left(), geometry.left() + minSize.width()));
+            rect.setBottom(std::min(rect.bottom(), geometry.bottom() - minSize.height()));
+            rect.setRight(std::min(rect.right(), geometry.left() + maxSize.width() + 1));
+            rect.setTop(std::max(rect.top(), geometry.bottom() - maxSize.height() - 1));
+            break;
+        case Gravity::Left:
+        case Gravity::BottomLeft:
+            rect.setRight(std::min(rect.right(), geometry.right() - minSize.width()));
+            rect.setLeft(std::max(rect.left(), geometry.right() - maxSize.width() - 1));
+            break;
+        case Gravity::Right:
+        case Gravity::BottomRight:
+            rect.setLeft(std::max(rect.left(), geometry.left() + minSize.width()));
+            rect.setRight(std::min(rect.right(), geometry.left() + maxSize.width() + 1));
+            break;
+        default:
+            Q_UNREACHABLE();
+        }
+        return rect;
+    };
+
     std::optional<QPointF> candidate;
     qreal bestScore;
     const auto availableArea = workspace()->clientArea(FullArea, workspace()->activeOutput(), VirtualDesktopManager::self()->currentDesktop());
 
     switch (gravity) {
+    // QRectF is used because QRect::right() returns left() + width() - 1; QRectF::right() returns left() + width()
     case Gravity::Top:
         // resizing from the top is handled like moving the window to avoid zero width rectangles when window width is equal to minVisibleWidth
-        for (Rect rect : visibleSubrectRegion.rects()) {
+        for (RectF rect : visibleSubrectRegion.rects()) {
             // convert top-left of visible titlebar subrect to top-left of the window
             rect.setLeft(rect.left() - geometry.width() + minVisibleWidth);
+
+            rect = applyMinSizeMaxSizeConstraints(rect);
+            if (!rect.isValid()) {
+                continue;
+            }
 
             const QPointF closest = clampPoint(rect, anchor);
             const qreal score = calcDistance(anchor, closest);
@@ -1529,9 +1571,14 @@ static std::optional<QPointF> confineInteractiveResize(const RectF &geometry, Gr
     case Gravity::Left:
     case Gravity::TopLeft:
     case Gravity::BottomLeft:
-        for (Rect rect : visibleSubrectRegion.rects()) {
+        for (RectF rect : visibleSubrectRegion.rects()) {
             // convert top-left of visible titlebar subrect to top-left of the window
             rect.setLeft(availableArea.left());
+
+            rect = applyMinSizeMaxSizeConstraints(rect);
+            if (!rect.isValid()) {
+                continue;
+            }
 
             const QPointF closest = clampPoint(rect, anchor);
             const qreal score = calcDistance(anchor, closest);
@@ -1547,6 +1594,11 @@ static std::optional<QPointF> confineInteractiveResize(const RectF &geometry, Gr
         for (RectF rect : visibleSubrectRegion.rects()) {
             // convert top-right of visible titlebar subrect to top-right of the window
             rect.setRight(availableArea.left() + availableArea.width());
+
+            rect = applyMinSizeMaxSizeConstraints(rect);
+            if (!rect.isValid()) {
+                continue;
+            }
 
             const QPointF closest = clampPoint(rect, anchor);
             const qreal score = calcDistance(anchor, closest);
@@ -1623,7 +1675,8 @@ RectF Window::nextInteractiveResizeGeometry(const QPointF &global) const
     nextMoveResizeGeom = workspace()->adjustWindowSize(this, nextMoveResizeGeom, gravity);
 
     if (!isUnrestrictedInteractiveMoveResize()) {
-        if (const auto anchor = confineInteractiveResize(nextMoveResizeGeom, gravity, 100, titlebarThickness())) {
+        if (const auto anchor = confineInteractiveResize(nextMoveResizeGeom, gravity, 100, titlebarThickness(),
+                                                         nextClientSizeToFrameSize(minSize()), nextClientSizeToFrameSize(maxSize()))) {
             switch (gravity) {
             case Gravity::TopLeft:
                 nextMoveResizeGeom.setTopLeft(*anchor);