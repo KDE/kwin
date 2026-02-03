114189ac3cdda59cf72ee5a67a1462bd58730481
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2026-02-03 14:17:19 +0200

    foo

--- src/main.cpp
+++ src/main.cpp
@@ -340,7 +340,7 @@ void Application::applyXwaylandScale()
 #if KWIN_BUILD_X11
     if (x11Connection()) {
         // rerun the fonts kcm init that does the appropriate xrdb call with the new settings
-        QProcess::startDetached("kcminit", {"kcm_fonts_init", "kcm_style_init"});
+        // QProcess::startDetached("kcminit", {"kcm_fonts_init", "kcm_style_init"});
     }
 #endif
 }
