32e758124896fa773e7c84ae30ab3183018e762a
Nicolas Fella <nicolas.fella@gmx.de>
2026-02-14 00:32:49 +0100

    Document Window(Filter)Model in QML scripting API

--- src/scripting/windowmodel.h
+++ src/scripting/windowmodel.h
@@ -19,6 +19,22 @@ namespace KWin
 class Window;
 class LogicalOutput;
 
+/*!
+ * \qmltype WindowModel
+ * \inqmlmodule org.kde.kwin
+ * \brief Window Model.
+ *
+ * This model provides the following roles:
+ * \list
+ * \li display (string)
+ * \li window (Window)
+ * \li output (LogicalOutput)
+ * \li desktop (list<VirtualDesktop>)
+ * \li activity (list<string>)
+ * \endlist
+ *
+ * \sa WindowFilterModel
+ */
 class WindowModel : public QAbstractListModel
 {
     Q_OBJECT
@@ -47,18 +63,61 @@ private:
     QList<Window *> m_windows;
 };
 
+/*!
+ * \qmltype WindowFilterModel
+ * \inqmlmodule org.kde.kwin
+ *
+ * \brief Filter a WindowModel
+ */
 class WindowFilterModel : public QSortFilterProxyModel
 {
     Q_OBJECT
+
+    /*!
+     * \qmlproperty WindowModel WindowFilterModel::windowModel
+     */
     Q_PROPERTY(WindowModel *windowModel READ windowModel WRITE setWindowModel NOTIFY windowModelChanged)
+
+    /*!
+     * \qmlproperty string WindowFilterModel::activity
+     */
     Q_PROPERTY(QString activity READ activity WRITE setActivity RESET resetActivity NOTIFY activityChanged)
+
+    /*!
+     * \qmlproperty VirtualDesktop WindowFilterModel::desktop
+     */
     Q_PROPERTY(KWin::VirtualDesktop *desktop READ desktop WRITE setDesktop RESET resetDesktop NOTIFY desktopChanged)
+
+    /*!
+     * \qmlproperty string WindowFilterModel::filter
+     */
     Q_PROPERTY(QString filter READ filter WRITE setFilter NOTIFY filterChanged)
+
+    /*!
+     * \qmlproperty string WindowFilterModel::screenName
+     */
     Q_PROPERTY(QString screenName READ screenName WRITE setScreenName RESET resetScreenName NOTIFY screenNameChanged)
+
+    /*!
+     * \qmlproperty enumeration WindowFilterModel::windowType
+     * \qmlenumeratorsfrom KWin::WindowTypes
+     */
     Q_PROPERTY(WindowTypes windowType READ windowType WRITE setWindowType RESET resetWindowType NOTIFY windowTypeChanged)
+
+    /*!
+     * \qmlproperty bool WindowFilterModel::minimizedWindows
+     */
     Q_PROPERTY(bool minimizedWindows READ minimizedWindows WRITE setMinimizedWindows NOTIFY minimizedWindowsChanged)
 
 public:
+    /*!
+     * \value Normal
+     * \value Dialog
+     * \value Dock
+     * \value Desktop
+     * \value Notification
+     * \value CriticalNotification
+     */
     enum WindowType {
         Normal = 0x1,
         Dialog = 0x2,
