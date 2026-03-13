505d98a7a06cb0132b67ed9d5c43814f77126673
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2026-03-13 19:19:12 +0200

    wayland: Implement wl_fixes v2

--- src/wayland/fixes.cpp
+++ src/wayland/fixes.cpp
@@ -11,20 +11,24 @@
 namespace KWin
 {
 
-static constexpr int s_version = 1;
+static constexpr int s_version = 2;
 
 class FixesInterfacePrivate : public QtWaylandServer::wl_fixes
 {
 public:
     FixesInterfacePrivate(Display *display);
 
+    Display *display;
+
 protected:
     void fixes_destroy(Resource *resource) override;
     void fixes_destroy_registry(Resource *resource, struct ::wl_resource *registry) override;
+    void fixes_ack_global_remove(Resource *resource, struct ::wl_resource *registry, uint32_t name) override;
 };
 
 FixesInterfacePrivate::FixesInterfacePrivate(Display *display)
     : QtWaylandServer::wl_fixes(*display, s_version)
+    , display(display)
 {
 }
 
@@ -36,6 +40,11 @@ void FixesInterfacePrivate::fixes_destroy(Resource *resource)
 void FixesInterfacePrivate::fixes_destroy_registry(Resource *resource, struct ::wl_resource *registry)
 {
     wl_resource_destroy(registry);
+}
+
+void FixesInterfacePrivate::fixes_ack_global_remove(Resource *resource, struct ::wl_resource *registry, uint32_t name)
+{
+    wl_fixes_handle_ack_global_remove(resource->handle, registry, name);
 }
 
 FixesInterface::FixesInterface(Display *display, QObject *parent)
