70277cd98211d8ffa3444176205cc4f277d705ed
David Edmundson <kde@davidedmundson.co.uk>
2026-04-24 17:16:22 +0100

    Fix passing fullscreen to the X11 backend

--- src/main_wayland.cpp
+++ src/main_wayland.cpp
@@ -544,6 +544,7 @@ int main(int argc, char *argv[])
             .outputCount = outputCount,
             .outputScale = outputScale,
             .outputSize = initialWindowSize,
+            .fullscreen = fullscreen,
         }));
         break;
     }
