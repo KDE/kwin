7f002b450110810dd1a9bf063a3d8490edacbe27
Xaver Hugl <xaver.hugl@kde.org>
2026-06-06 15:49:09 +0200

    plugins/zoom: render screen contents into a separate SceneView
    To make that possible, this adds infrastructure to allow any effect to render
    its own SceneView in preparation to another SceneView being rendered.
    
    This improves the zoom effect by only rendering parts of the scene that are
    actually relevant, which should improve performance.
    Doing this in a SceneView specifically also helps prepare for porting
    the effect to use an item for presentation, and for future multi GPU changes.

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -646,6 +646,13 @@ void Scene::addRepaintFull()
     }
 }
 
+void Scene::scheduleFrame()
+{
+    for (const auto &view : std::as_const(m_views)) {
+        view->scheduleRepaint(nullptr);
+    }
+}
+
 void Scene::addLogicalRepaint(int x, int y, int width, int height)
 {
     addLogicalRepaint(Region(x, y, width, height));
