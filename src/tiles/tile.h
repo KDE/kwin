418e7f64a74a5a511af26f02c3275ccc12672823
Nicolas Fella <nicolas.fella@gmx.de>
2026-07-05 23:54:11 +0200

    Add QML documentation for Tile

--- src/tiles/tile.h
+++ src/tiles/tile.h
@@ -24,19 +24,68 @@ class TileManager;
 class VirtualDesktop;
 class Window;
 
+/*!
+ * \qmluncreatabletype Tile
+ * \inqmlmodule org.kde.kwin
+ *
+ */
 class KWIN_EXPORT Tile : public QObject
 {
     Q_OBJECT
+
+    /*!
+     * \qmlproperty RectF Tile::relativeGeometry
+     */
     Q_PROPERTY(KWin::RectF relativeGeometry READ relativeGeometry WRITE setRelativeGeometry NOTIFY relativeGeometryChanged)
+
+    /*!
+     * \qmlproperty RectF Tile::absoluteGeometry
+     */
     Q_PROPERTY(KWin::RectF absoluteGeometry READ absoluteGeometry NOTIFY absoluteGeometryChanged)
+
+    /*!
+     * \qmlproperty RectF Tile::absoluteGeometryInScreen
+     */
     Q_PROPERTY(KWin::RectF absoluteGeometryInScreen READ absoluteGeometryInScreen NOTIFY absoluteGeometryChanged)
+
+    /*!
+     * \qmlproperty real Tile::padding
+     */
     Q_PROPERTY(qreal padding READ padding WRITE setPadding NOTIFY paddingChanged)
+
+    /*!
+     * \qmlproperty size Tile::minimumSize
+     */
     Q_PROPERTY(QSizeF minimumSize READ minimumSize WRITE setMinimumSize NOTIFY minimumSizeChanged)
+
+    /*!
+     * \qmlproperty int Tile::positionInLayout
+     */
     Q_PROPERTY(int positionInLayout READ row NOTIFY rowChanged)
+
+    /*!
+     * \qmlproperty Tile Tile::parent
+     */
     Q_PROPERTY(Tile *parent READ parentTile CONSTANT)
+
+    /*!
+     * \qmlproperty list<Tile> Tile::tiles
+     */
     Q_PROPERTY(QList<KWin::Tile *> tiles READ childTiles NOTIFY childTilesChanged)
+
+    /*!
+     * \qmlproperty list<Window> Tile::windows
+     */
     Q_PROPERTY(QList<KWin::Window *> windows READ windows NOTIFY windowsChanged)
+
+    /*!
+     * \qmlproperty bool Tile::isLayout
+     */
     Q_PROPERTY(bool isLayout READ isLayout NOTIFY isLayoutChanged)
+
+    /*!
+     * \qmlproperty bool Tile::canBeRemoved
+     */
     Q_PROPERTY(bool canBeRemoved READ canBeRemoved CONSTANT)
 
 public:
@@ -59,33 +108,33 @@ public:
 
     virtual bool supportsResizeGravity(Gravity gravity);
 
-    /**
+    /*!
      * Geometry of the tile in units between 0 and 1 relative to the screen geometry
      */
     RectF relativeGeometry() const;
 
-    /**
+    /*!
      * Geometry of the tile in absolute coordinates
      */
     RectF absoluteGeometry() const;
 
-    /**
+    /*!
      * Geometry of the tile in absolute coordinates, but in screen coordinates,
      * ie the top left corner of rootTile always at 0,0
      */
     RectF absoluteGeometryInScreen() const;
 
-    /**
+    /*!
      * Absolute geometry minus the padding and reserved areas such as panels
      */
     RectF windowGeometry() const;
 
-    /**
+    /*!
      * Absolute geometry minus the padding and reserved areas such as panels
      */
     RectF maximizedWindowGeometry() const;
 
-    /**
+    /*!
      * Which edges of the tile touches an edge of the screen
      */
     Qt::Edges anchors() const;
@@ -105,26 +154,36 @@ public:
 
     Tile *rootTile() const;
 
-    /**
+    /*!
      * All tiles directly children of this tile
      */
     QList<Tile *> childTiles() const;
 
-    /**
+    /*!
      * All tiles descendant of this tile, recursive
      */
     QList<Tile *> descendants() const;
 
-    /**
+    /*!
      * Visit all tiles descendant of this tile, recursive
      */
     void visitDescendants(std::function<void(Tile *child)> callback);
 
     void resizeFromGravity(Gravity gravity, int x_root, int y_root);
 
+    /*!
+     * \qmlmethod void Tile::resizeByPixels(real delta, enumeration edge)
+     */
     Q_INVOKABLE void resizeByPixels(qreal delta, Qt::Edge edge);
 
+    /*!
+     * \qmlmethod void Tile::manage(Window window)
+     */
     Q_INVOKABLE bool manage(Window *window);
+
+    /*!
+     * \qmlmethod void Tile::unmanage(Window window)
+     */
     Q_INVOKABLE bool unmanage(Window *window);
     void forget(Window *window);
     QList<KWin::Window *> windows() const;
@@ -158,9 +217,17 @@ Q_SIGNALS:
     void rowChanged(int row);
     void isLayoutChanged(bool isLayout);
     void childTilesChanged();
-    void windowAdded(Window *window);
-    void windowRemoved(Window *window);
     void windowsChanged();
+
+    /*!
+     * \qmlsignal windowAdded(Window window)
+     */
+    void windowAdded(Window *window);
+
+    /*!
+     * \qmlsignal windowRemoved(Window window)
+     */
+    void windowRemoved(Window *window);
 
 protected:
     void insertChild(int position, Tile *item);
