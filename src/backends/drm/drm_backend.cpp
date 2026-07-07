c60460e014c0759e22dd89a7b390b441124c9e74
Xaver Hugl <xaver.hugl@kde.org>
2026-07-07 14:49:42 +0200

    autotests/integration/drm_test: filter out broken VM drivers
    This avoids accidentally running tests on the broken QXL driver in CI, VKMS
    should be used instead.
    If the test should be run in a virtual machine manually anyways, the
    KWIN_DRM_DEVICES env var can still be used to force the driver to be used.
    
    (cherry picked from commit 131ca37f8ba637c6905a3fa6bd3b082605dc1607)

--- src/backends/drm/drm_backend.cpp
+++ src/backends/drm/drm_backend.cpp
@@ -78,7 +78,6 @@ DrmBackend::DrmBackend(Session *session, QObject *parent)
     , m_udev(std::make_unique<Udev>())
     , m_udevMonitor(m_udev->createMonitor())
     , m_session(session)
-    , m_explicitGpus(splitPathList(qEnvironmentVariable("KWIN_DRM_DEVICES"), ':'))
 {
 }
 
@@ -96,6 +95,8 @@ QList<BackendOutput *> DrmBackend::outputs() const
 
 bool DrmBackend::initialize()
 {
+    m_explicitGpus = splitPathList(qEnvironmentVariable("KWIN_DRM_DEVICES"), ':');
+
     connect(m_session, &Session::devicePaused, this, [this](dev_t deviceId) {
         if (const auto gpu = findGpu(deviceId)) {
             gpu->setActive(false);
