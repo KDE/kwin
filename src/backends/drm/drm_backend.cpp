3e8bf67213fe283c31af6e3c1dcd7037d99a152e
Xaver Hugl <xaver.hugl@kde.org>
2026-07-07 16:26:22 +0200

    backends/drm: allow starting without any KMS nodes
    Since we don't need any "primary" GPU anymore, we can just allow this. Render
    nodes are still required to do compositing though.

--- src/backends/drm/drm_backend.cpp
+++ src/backends/drm/drm_backend.cpp
@@ -134,11 +134,6 @@ bool DrmBackend::initialize()
         }
     }
 
-    if (m_gpus.empty()) {
-        qCWarning(KWIN_DRM) << "No suitable DRM devices have been found";
-        return false;
-    }
-
     // setup udevMonitor
     if (m_udevMonitor) {
         m_udevMonitor->filterSubsystemDevType("drm");
