e9a56cc56dfc835e0287610b1fbba7ec148d4e10
Erhan Bilgili <erhan.bilgili@gmail.com>
2026-07-31 11:49:53 +0200

    utils: Report realtime scheduling errors correctly
    pthread_setschedparam returns an error number directly and does not have
    to set errno. Use that return value so failed realtime setup reports the
    actual cause.

--- src/utils/realtime.cpp
+++ src/utils/realtime.cpp
@@ -20,8 +20,9 @@ void gainRealTime()
     const int minPriority = sched_get_priority_min(SCHED_RR);
     sched_param sp;
     sp.sched_priority = minPriority;
-    if (pthread_setschedparam(pthread_self(), SCHED_RR | SCHED_RESET_ON_FORK, &sp) != 0) {
-        qWarning("Failed to gain real time thread priority (See CAP_SYS_NICE in the capabilities(7) man page). error: %s", strerror(errno));
+    const int error = pthread_setschedparam(pthread_self(), SCHED_RR | SCHED_RESET_ON_FORK, &sp);
+    if (error) {
+        qWarning("Failed to gain real time thread priority (See CAP_SYS_NICE in the capabilities(7) man page). error: %s", strerror(error));
     }
 #endif
 }
