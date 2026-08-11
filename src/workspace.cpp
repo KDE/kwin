1ca9e62b3aff7dd5a566baaf56112cd09d6eecf2
Ramil Nurmanov <ramil2004nur@gmail.com>
2026-08-12 03:20:56 +0400

    workspace: Remove redundant priority change handling

--- src/workspace.cpp
+++ src/workspace.cpp
@@ -359,11 +359,9 @@ void Workspace::init()
     const auto outputs = kwinApp()->outputBackend()->outputs();
     for (BackendOutput *output : outputs) {
         connect(output, &BackendOutput::dpmsModeChanged, this, &Workspace::maybeUpdateDpmsState);
-        connect(output, &BackendOutput::priorityChanged, this, &Workspace::updateOutputOrder);
     }
     connect(kwinApp()->outputBackend(), &OutputBackend::outputAdded, this, [this](BackendOutput *output) {
         connect(output, &BackendOutput::dpmsModeChanged, this, &Workspace::maybeUpdateDpmsState);
-        connect(output, &BackendOutput::priorityChanged, this, &Workspace::updateOutputOrder);
     });
 }
 
