0a3dab8821fc4861fd2a55fd6eebac74b63f9226
Xaver Hugl <xaver.hugl@kde.org>
2026-08-14 19:58:25 +0200

    vulkan: split VulkanQueue into its own file

---
+++ src/vulkan/vulkan_queue.h
@@ -0,0 +1,55 @@
+/*
+    KWin - the KDE window manager
+    This file is part of the KDE project.
+
+    SPDX-FileCopyrightText: 2023-2026 Xaver Hugl <xaver.hugl@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include "kwin_export.h"
+#include "utils/filedescriptor.h"
+
+#include <deque>
+#include <vulkan/vulkan_raii.hpp>
+
+namespace KWin
+{
+
+class VulkanDevice;
+
+class KWIN_EXPORT VulkanQueue
+{
+public:
+    explicit VulkanQueue(VulkanDevice *device, uint32_t familyIndex, vk::raii::Queue &&handle, vk::raii::CommandPool &&commandPool);
+    ~VulkanQueue();
+
+    uint32_t familyIndex() const;
+    const vk::raii::Queue &handle() const;
+
+    vk::raii::CommandBuffer createCommandBuffer();
+    std::optional<FileDescriptor> submit(vk::raii::CommandBuffer &&buffer, FileDescriptor &&syncFd);
+
+    /**
+     * NOTE avoid using this if at all possible, it's obviously terrible for performance!
+     */
+    void waitIdle();
+
+    static std::unique_ptr<VulkanQueue> create(VulkanDevice *device, uint32_t familyIndex);
+
+private:
+    struct SubmittedCommand
+    {
+        vk::raii::Semaphore waitSemaphore;
+        vk::raii::CommandBuffer buffer;
+        FileDescriptor completionSyncFd;
+    };
+
+    VulkanDevice *const m_device;
+    const uint32_t m_familyIndex;
+    const vk::raii::Queue m_handle;
+    const vk::raii::CommandPool m_commandPool;
+    std::deque<SubmittedCommand> m_submittedCommandBuffers;
+};
+
+}
