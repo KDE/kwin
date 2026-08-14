2cd8fa3e2af352024c4e0c40634024764008d07a
Xaver Hugl <xaver.hugl@kde.org>
2026-08-14 19:58:25 +0200

    vulkan: split VulkanQueue into its own file

--- src/vulkan/vulkan_device.h
+++ src/vulkan/vulkan_device.h
@@ -10,14 +10,13 @@
 #include "core/drm_formats.h"
 #include "kwin_export.h"
 #include "utils/filedescriptor.h"
+#include "vulkan_queue.h"
 
 #include <QHash>
 #include <QObject>
 #include <QVector>
-#include <deque>
 #include <memory>
 #include <optional>
-#include <vulkan/vulkan_raii.hpp>
 
 namespace KWin
 {
@@ -26,41 +25,6 @@ class VulkanTexture;
 class GraphicsBuffer;
 struct DmaBufAttributes;
 class RenderDevice;
-class VulkanDevice;
-
-class KWIN_EXPORT VulkanQueue
-{
-public:
-    explicit VulkanQueue(VulkanDevice *device, uint32_t familyIndex, vk::raii::Queue &&handle, vk::raii::CommandPool &&commandPool);
-    ~VulkanQueue();
-
-    uint32_t familyIndex() const;
-    const vk::raii::Queue &handle() const;
-
-    vk::raii::CommandBuffer createCommandBuffer();
-    std::optional<FileDescriptor> submit(vk::raii::CommandBuffer &&buffer, FileDescriptor &&syncFd);
-
-    /**
-     * NOTE avoid using this if at all possible, it's obviously terrible for performance!
-     */
-    void waitIdle();
-
-    static std::unique_ptr<VulkanQueue> create(VulkanDevice *device, uint32_t familyIndex);
-
-private:
-    struct SubmittedCommand
-    {
-        vk::raii::Semaphore waitSemaphore;
-        vk::raii::CommandBuffer buffer;
-        FileDescriptor completionSyncFd;
-    };
-
-    VulkanDevice *const m_device;
-    const uint32_t m_familyIndex;
-    const vk::raii::Queue m_handle;
-    const vk::raii::CommandPool m_commandPool;
-    std::deque<SubmittedCommand> m_submittedCommandBuffers;
-};
 
 class KWIN_EXPORT VulkanDevice : public QObject
 {
