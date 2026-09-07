8c46d1666aed72dda8da383dbe398f2b288157b3
Xaver Hugl <xaver.hugl@kde.org>
2026-09-07 23:47:21 +0200

    effect/offscreenquickview: fix the render time query
    begin() was missing, so it just resulted in nonsense

--- src/effect/offscreenquickview.cpp
+++ src/effect/offscreenquickview.cpp
@@ -311,6 +311,7 @@ void OffscreenQuickView::update(OutputFrame *frame)
         }
         if (frame) {
             renderTime = std::make_unique<GLRenderTimeQuery>();
+            renderTime->begin();
         }
 
         qreal dpr = d->m_view->screen() ? d->m_view->screen()->devicePixelRatio() : 1.0;
