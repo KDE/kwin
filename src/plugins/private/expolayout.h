d6b281f4a0ed9fa05fc5abb401e2b74528e660ac
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2026-09-16 14:33:07 +0300

    plugins: Use QSizeF to represent sizes
    QSizeF is more suitable than RectF.
    
    (cherry picked from commit 98bcbdde8482c0c04665f7a2b7ae491365882a19)

--- src/plugins/private/expolayout.h
+++ src/plugins/private/expolayout.h
@@ -127,7 +127,7 @@ protected:
      * @param maxSize. Then add @param margins to each window size, and @return
      * the adjusted window sizes.
      */
-    QList<RectF> adjustSizes(const RectF &minSize, const RectF &maxSize, const QMarginsF &margins, const QList<RectF> &windowSizes);
+    QList<RectF> adjustSizes(const QSizeF &minSize, const QSizeF &maxSize, const QMarginsF &margins, const QList<RectF> &windowSizes);
 
     /**
      * @brief Use binary search to find a good packing of the @param windowSizes
